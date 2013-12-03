af7064f0f307d18760f74d0dc56ccea576d1ee13
Jeremy McCormick <jermccormick@gmail.com>
2013-12-03 21:34:34 +0000

    remove conditions packages that are moving to separate module

--- hps-java/src/main/java/org/lcsim/hps/conditions/ecal/EcalBadChannelCollection.java
+++
@@ -1,9 +0,0 @@
-package org.lcsim.hps.conditions.ecal;
-
-import org.lcsim.hps.conditions.ChannelCollection;
-
-/**
- * A collection of bad channel IDs in the ECAL.
- */
-public class EcalBadChannelCollection extends ChannelCollection {
-}
