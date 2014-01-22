b42c2dc60b6e6bc10369556bc700138acc281ddc
Jeremy McCormick <jermccormick@gmail.com>
2014-01-22 04:40:07 +0000

    fix run script

--- src/main/scripts/start_monitoring_application.sh
+++ src/main/scripts/start_monitoring_application.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-classpath=${project.basedir}/target/hps-java-${project.version}-bin.jar
-prod="java -Xmx1024m -classpath "$classpath" org.lcsim.hps.monitoring.MonitoringApplication $@"
+classpath=${project.basedir}/target/${project.artifactId}-${project.version}-bin.jar
+prod="java -Xmx1024m -classpath "$classpath" org.hps.monitoring.MonitoringApplication $@"
 # -c 100"
 echo $prod
 exec $prod
