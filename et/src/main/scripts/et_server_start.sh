885f0b0ab9cd9885726bb6a22436977a15887163
Jeremy McCormick <jermccormick@gmail.com>
2014-01-22 04:57:30 +0000

    fix scripts

--- et/src/main/scripts/et_server_start.sh
+++ et/src/main/scripts/et_server_start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-et=${project.basedir}/target/hps-et-java-${project.version}.jar
+et=${project.basedir}/target/${project.artifactId}-${project.version}.jar
 server="java -Xmx1024m -classpath $et org.jlab.coda.et.apps.StartEt $@" 
 
 echo "$server"
--- et/src/main/scripts/et_server_test.sh
+++ et/src/main/scripts/et_server_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Setup server for test EVIO events. 
-et=${project.basedir}/target/hps-et-java-${project.version}.jar
+et=${project.basedir}/target/${project.artifactId}-${project.version}.jar
 #server="java -classpath $et org.jlab.coda.et.apps.StartEt -p 11111 -f ETBuffer -n 100 -s 1024 -v -d" 
 server="java -Xmx1024m -classpath $et org.jlab.coda.et.apps.StartEt -f ETBuffer" 
 server=$server" -s 10000 -v"
