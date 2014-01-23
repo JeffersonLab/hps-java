65edab126c924ee104aa5f692a55953e4580fcbf
Norman Graf <ngraf@slac.stanford.edu>
2014-01-23 22:07:51 +0000

    Unit test for MaterialSupervisor. Work in progress.

---
+++ hps-java/src/test/java/org/lcsim/hps/recon/tracking/MaterialSupervisorTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.lcsim.hps.recon.tracking;
+
+import junit.framework.TestCase;
+import org.lcsim.conditions.ConditionsManager;
+import org.lcsim.conditions.ConditionsManager.ConditionsNotFoundException;
+import org.lcsim.geometry.Detector;
+import org.lcsim.geometry.util.DetectorLocator;
+import org.lcsim.util.loop.LCSimConditionsManagerImplementation;
+
+/**
+ *
+ * @author ngraf
+ */
+public class MaterialSupervisorTest extends TestCase
+{
+
+    public void testMaterialSupervisor()
+    {
+        String detectorName = "HPS-Proposal2014-v5-2pt2";
+        Detector det = DetectorLocator.findDetector(detectorName);
+        System.out.println(det.getName());
+        
+        boolean debug = true;
+        boolean includeMS=true;
+        MaterialSupervisor instance = new MaterialSupervisor(includeMS);
+        instance.setDebug(debug);
+        // following call crashes.
+        // evidently something else needs to be set up
+     //   instance.buildModel(det);
+
+    }
+}
