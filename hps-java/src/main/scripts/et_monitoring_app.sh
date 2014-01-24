71cddf68e91c90a46c5d659a37934f3f50dbd635
Jeremy McCormick <jermccormick@gmail.com>
2014-01-24 00:59:59 +0000

    remove old monitoring run script

--- hps-java/src/main/scripts/et_monitoring_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-classpath=${project.basedir}/target/${project.artifactId}-${project.version}-bin.jar
-prod="java -Xmx1024m -classpath "$classpath" org.lcsim.hps.monitoring.MonitoringApplication $@"
-# -c 100"
-echo $prod
-exec $prod
