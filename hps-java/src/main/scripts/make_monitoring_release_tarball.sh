0f140d82a8dfd70341f18a75f350e8f36df0a29b
Jeremy McCormick <jermccormick@gmail.com>
2014-01-24 00:58:54 +0000

    remove old script

--- hps-java/src/main/scripts/make_monitoring_release_tarball.sh
+++
@@ -1,1 +0,0 @@
-tar -zcvf hps_monitoring_release.tar.gz ./hps-et-java/lib/*/*.so ./hps-et-java/lib/*/*.dylib ./hps-et-java/target/*.jar ./hps-java/target/*bin.jar ./hps-et-java/scripts/*.sh ./hps-java/scripts/*.sh ./hps-java/src/main/resources/org/lcsim/hps/steering/*.lcsim
