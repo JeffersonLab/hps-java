9320703606cc184451e6e43c67e10213ab054aa0
Norman Graf <ngraf@slac.stanford.edu>
2014-01-30 23:23:09 +0000

    Made Driver more generic by adding setter methods for collections, etc. Removed any reference to SimCalorimeterHit from Driver. Added Driver for SimCalorimeterHit to CalorimeterHit conversion in test. Added simple analysis Driver to test. Work in progress.

--- users/src/main/java/org/lcsim/hps/users/ngraf/NearestNeighborClusterDriver.java
+++ users/src/main/java/org/lcsim/hps/users/ngraf/NearestNeighborClusterDriver.java
@@ -18,64 +18,71 @@ import org.lcsim.util.aida.AIDA;
 
 /**
  * This is a test
+ *
  * @author Norman A Graf
  *
  * @version $Id:
  */
 public class NearestNeighborClusterDriver extends Driver
 {
-    // Histogram manager
-    private AIDA aida;
     // Minimum E for cluster seed.
     double seedEMin = .00 * ECalUtils.GeV;
     // Minimum E to add hit to cluster.
     double addEMin = .00 * ECalUtils.GeV;
     // Map of crystals to their neighbors.
     HPSEcal3.NeighborMap _neighborMap = null;
+    String ecalCollectionName;
+    String ecalName = "Ecal";
+    String clusterCollectionName = "NearestNeighborEcalClusters";
+
+    public void setEcalName(String ecalName)
+    {
+        this.ecalName = ecalName;
+    }
+
+    public void setEcalCollectionName(String ecalCollectionName)
+    {
+        this.ecalCollectionName = ecalCollectionName;
+    }
+
+    public void setClusterCollectionName(String clusterCollectionName)
+    {
+        this.clusterCollectionName = clusterCollectionName;
+    }
+
+    public void setSeedEMin(double seedEMin)
+    {
+        this.seedEMin = seedEMin;
+    }
+
+    public void setAddEMin(double addEMin)
+    {
+        this.addEMin = addEMin;
+        if (seedEMin < addEMin) {
+            seedEMin = addEMin;
+        }
+    }
 
     @Override
-    public void processChildren(EventHeader event)
+    protected void process(EventHeader event)
     {
-        boolean debug = true;
-        // This is example code to run over a simple lcio file output from slic
-        // on which no calorimeter digitzation has been done.
-        // Therefore need to convert from SimCalorimeterHit to CalorimeterHit
-        // This would normally be done as a part of the reconstruction
-        if (event.hasCollection(SimCalorimeterHit.class, "EcalHits")) {
-            // Get the list of sim ECal hits.
-            List<SimCalorimeterHit> hits = event.get(SimCalorimeterHit.class, "EcalHits");
+        // Fetch the list of CalorimeterHits
+        if (event.hasCollection(CalorimeterHit.class, ecalCollectionName)) {
+            
+            List<CalorimeterHit> hits = event.get(CalorimeterHit.class, ecalCollectionName);
 
-            // create a list of CalorimeterHits
             // Make a hit map for quick lookup by ID.
             Map<Long, CalorimeterHit> hitMap = new HashMap<Long, CalorimeterHit>();
-            for (SimCalorimeterHit hit : hits) {
+            for (CalorimeterHit hit : hits) {
                 hitMap.put(hit.getCellID(), hit);
             }
 
             // Cluster the hits using a nearest neighbor algorithm
-            int flag = 1 << LCIOConstants.CLBIT_HITS;
             List<NearestNeighborCluster> clusters = createNearestNeighborClusters(hitMap);
-            // quick analysis example
-            if(debug) System.out.println("found " + clusters.size() + " clusters");
-            double eTop = 0.;
-            double eBottom = 0.;
-            for (Cluster clus : clusters) {
-                if(debug)System.out.println("x: " + clus.getPosition()[0] + " y: " + clus.getPosition()[1] + " iPhi: " + clus.getIPhi() + " iTheta " + clus.getITheta());
-                if(clus.getPosition()[1]>0)
-                {
-                    eTop+=clus.getEnergy();
-                }
-                else
-                {
-                    eBottom += clus.getEnergy();
-                }
-            }
-            //fill the histograms
-            aida.cloud1D("Top energy sum").fill(eTop);
-            aida.cloud1D("Bottom energy sum").fill(eBottom);
-            aida.cloud1D("energy diff").fill(eTop-eBottom);
+            
             //add the list of clusters to the event
-            event.put("NearestNeighborClusters", clusters, NearestNeighborCluster.class, flag);
+            int flag = 1 << LCIOConstants.CLBIT_HITS;
+            event.put(clusterCollectionName, clusters, NearestNeighborCluster.class, flag);
         }
     }
 
@@ -83,17 +90,16 @@ public class NearestNeighborClusterDriver extends Driver
     protected void detectorChanged(Detector detector)
     {
         // Get the Subdetector.
-        HPSEcal3 ecal = (HPSEcal3) detector.getSubdetector("Ecal");
+        HPSEcal3 ecal = (HPSEcal3) detector.getSubdetector(ecalName);
         // Cache the neighbor map for use by the nearest neighbor clustering algorithm
         _neighborMap = ecal.getNeighborMap();
-        // set up some plotting infrastructure
-        aida = AIDA.defaultInstance();
-        aida.tree().cd("/");
     }
 
     /**
      * Run the clustering algorithm over the list of hit crystals
-     * @param map Map of CalorimeterHit keyed on CellID. Note that this map is modified
+     *
+     * @param map Map of CalorimeterHit keyed on CellID. Note that this map is
+     * modified
      * @return The list of found clusters
      */
     public List<NearestNeighborCluster> createNearestNeighborClusters(Map<Long, CalorimeterHit> map)
