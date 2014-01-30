8b66b74fc194f6e800cade44ffa04490772f0316
Kyle McCarty <mccaky@gmail.com>
2014-01-30 23:04:02 +0000

    No code changed; added documentation to CTPEcalClusterer.

--- hps-java/src/main/java/org/lcsim/hps/recon/ecal/CTPEcalClusterer.java
+++ hps-java/src/main/java/org/lcsim/hps/recon/ecal/CTPEcalClusterer.java
@@ -11,7 +11,6 @@ import java.util.PriorityQueue;
 import java.util.Set;
 
 import org.lcsim.event.CalorimeterHit;
-//import org.lcsim.event.Cluster;
 import org.lcsim.event.EventHeader;
 import org.lcsim.geometry.Detector;
 import org.lcsim.geometry.IDDecoder;
@@ -32,67 +31,75 @@ import org.lcsim.lcio.LCIOConstants;
  * @version $Id: CTPEcalClusterer.java,v 1.1 2013/02/25 22:39:24 meeg Exp $
  */
 public class CTPEcalClusterer extends Driver {
-
+    // The calorimeter object.
     HPSEcal3 ecal;
     IDDecoder dec;
+    // The identification name for getting the calorimeter object.
     String ecalName;
+    // The collection name for the calorimeter hits.
     String ecalCollectionName;
+    // The collection name in which to store clusters.
     String clusterCollectionName = "EcalClusters";
     Set<Long> clusterCenters = null;
     Map<Long, Double> hitSums = null;
     Map<Long, CalorimeterHit> hitMap = null;
     // Map of crystals to their neighbors.
     NeighborMap neighborMap = null;
+    // The time period in which clusters may be formed. A negative value means that all hits
+    // will always be sued in clsuter finding, regardless of the time difference between them.
     double clusterWindow = -1;
+    // The minimum energy needed for a hit to be considered.
     double addEMin = 0;
-
+    
     public CTPEcalClusterer() {
     }
-
+    
     public void setAddEMin(double addEMin) {
         this.addEMin = addEMin;
     }
-
+    
     public void setClusterWindow(double clusterWindow) {
         this.clusterWindow = clusterWindow;
     }
-
+    
     public void setClusterCollectionName(String clusterCollectionName) {
         this.clusterCollectionName = clusterCollectionName;
     }
-
+    
     public void setEcalCollectionName(String ecalCollectionName) {
         this.ecalCollectionName = ecalCollectionName;
     }
-
+    
     public void setEcalName(String ecalName) {
         this.ecalName = ecalName;
     }
-
-    @Override
+    
     public void startOfData() {
+        // Make sure that there is a cluster collection name into which clusters may be placed.
         if (ecalCollectionName == null) {
             throw new RuntimeException("The parameter ecalCollectionName was not set!");
         }
-
+        
+        // Make sure that there is a calorimeter detector.
         if (ecalName == null) {
             throw new RuntimeException("The parameter ecalName was not set!");
         }
     }
-
+    
     @Override
     public void detectorChanged(Detector detector) {
         // Get the Subdetector.
         ecal = (HPSEcal3) detector.getSubdetector(ecalName);
-
+        
         // Get the decoder for the ECal IDs.
         dec = ecal.getIDDecoder();
-
+        
         // Cache ref to neighbor map.
         neighborMap = ecal.getNeighborMap();
-
+        
         clusterCenters = new HashSet<Long>();
         //Make set of valid cluster centers.
+        /// Exclude edge crystals as good cluster centers.
         for (Long cellID : neighborMap.keySet()) {
             boolean isValidCenter = true;
             Set<Long> neighbors = neighborMap.get(cellID);
@@ -100,7 +107,7 @@ public class CTPEcalClusterer extends Driver {
                 Set<Long> neighborneighbors = new HashSet<Long>();
                 neighborneighbors.addAll(neighborMap.get(neighborID));
                 neighborneighbors.add(neighborID);
-
+                
                 if (neighborneighbors.containsAll(neighbors)) {
                     isValidCenter = false;
                     break;
@@ -110,168 +117,223 @@ public class CTPEcalClusterer extends Driver {
                 clusterCenters.add(cellID);
             }
         }
-
-        //System.out.println(ecal.getName());
-        //System.out.println("  nx="+ecal.nx());
-        //System.out.println("  ny="+ecal.ny());
-        //System.out.println("  beamgap="+ecal.beamGap());
-        //System.out.println("  dface="+ecal.distanceToFace());
-
-        //System.out.println(neighborMap.toString());
     }
-
-    @Override
+    
     public void process(EventHeader event) {
-        //System.out.println(this.getClass().getCanonicalName() + " - process");
-
+        // Make sure that this event has calorimeter hits.
         if (event.hasCollection(CalorimeterHit.class, ecalCollectionName)) {
-            // Get the list of raw ECal hits.
+            // Get the list of calorimeter hits from the event.
             List<CalorimeterHit> hits = event.get(CalorimeterHit.class, ecalCollectionName);
-
+            
+            // Define a list of clusters to be filled.
             List<HPSEcalCluster> clusters;
-
+            
+            // If there is a cluster window, run the clsuter window code. A cluster window is a time
+            // period in nanoseconds during which hits can be applied to the same cluster.
             if (clusterWindow >= 0) {
+                // Create priority queues. These are sorted by the time variable associated with each hit.
                 PriorityQueue<CalorimeterHit> futureHits = new PriorityQueue<CalorimeterHit>(10, new TimeComparator());
                 PriorityQueue<CalorimeterHit> pastHits = new PriorityQueue<CalorimeterHit>(10, new TimeComparator());
+                
+                // Initialize the cluster list variable.
                 clusters = new ArrayList<HPSEcalCluster>();
-
+                
+                // Populate the list of unprocessed hits with the calorimeter hits. These will then be sorted
+                // by time, from first to last, automatically by the priority queue.
                 for (CalorimeterHit hit : hits) {
                     if (hit.getRawEnergy() > addEMin) {
                         futureHits.add(hit);
                     }
                 }
-
+                
+                // We process the unprocessed hits...
                 while (!futureHits.isEmpty()) {
+                    // Move the first occurring hit from the unprocessed list to the processed list.
                     CalorimeterHit nextHit = futureHits.poll();
                     pastHits.add(nextHit);
+                    
+                    // Add any hits that occurred at the same time as the hit we just added to the processed list.
                     while (!futureHits.isEmpty() && futureHits.peek().getTime() == nextHit.getTime()) {
                         pastHits.add(futureHits.poll());
                     }
+                    
+                    // Remove hits that happened earlier than the cluster window period.
                     while (pastHits.peek().getTime() < nextHit.getTime() - clusterWindow) {
                         pastHits.poll();
                     }
+                    
+                    // Calculate the cluster energy for each crystal. This should be the
+                    // total energy for the 3x3 crystal collection sorrounding the center
+                    // crystal.
                     sumHits(pastHits);
+                    
+                    // Choose which crystal is the appropriate cluster crystal.
                     clusters.addAll(createClusters());
                 }
+            // If there is no cluster window, then all the hits in the event are visible simultaneously.
             } else {
+                // Calculate the cluster energy of each crystal.
                 sumHits(hits);
+                // Generate the clusters.
                 clusters = createClusters();
             }
-
-            // Put Cluster collection into event.
+            
+            // Put the cluster collection into the event.
             int flag = 1 << LCIOConstants.CLBIT_HITS;
             event.put(clusterCollectionName, clusters, HPSEcalCluster.class, flag);
         }
     }
-
+    
     public void sumHits(Collection<CalorimeterHit> hits) {
-        // Hit map.
+        // Store the latest hit on each crystal in a map for later reference in
+        // the clustering algorithm.
         hitMap = new HashMap<Long, CalorimeterHit>();
-
+        // Store the cluster energy for each crystal. Cluster energy represents
+        // the total energy of the 3x3 crystal set.
         hitSums = new HashMap<Long, Double>();
-        // Loop over ECal hits to compute energy sums
+        
+        // Loop over the active calorimeter hits to compute the cluster energies.
         for (CalorimeterHit hit : hits) {
-//                System.out.format("hit: %f\n",hit.getRawEnergy());
             // Make a hit map for quick lookup by ID.
             hitMap.put(hit.getCellID(), hit);
-
-            // Get neighbor crystal IDs. 
+            
+            // Get the cell ID for the current crystal's neighbors.
             Set<Long> neighbors = neighborMap.get(hit.getCellID());
-
+            
+            // If there are no neighbors, something is rather wrong.
             if (neighbors == null) {
                 throw new RuntimeException("Oops!  Set of neighbors is null!");
             }
-
+            
+            // Store the energy of the current calorimeter hit.
             Double hitSum;
-
+            
+            // We are only interested in this crystal's cluster energy if it is
+            // a valid cluster crystal. Edge crystals are not allowed to be clusters,
+            // so these are ignored.
             if (clusterCenters.contains(hit.getCellID())) {
+                // Check if an energy has been assigned to the crystal.
                 hitSum = hitSums.get(hit.getCellID());
+                
+                // If not, then the crystal's cluster energy is equal to this hit's energy.
                 if (hitSum == null) {
                     hitSums.put(hit.getCellID(), hit.getRawEnergy());
-                } else {
+                }
+                // Otherwise, add the energy of this hit to the total crystal cluster energy.
+                else {
                     hitSums.put(hit.getCellID(), hitSum + hit.getRawEnergy());
                 }
             }
-
-            // Loop over neighbors to make hit list for cluster.
+            
+            // Loop over neighbors to add the current hit's energy to the neighbor's
+            // cluster energy.
             for (Long neighborId : neighbors) {
+                // If the crystal is not an edge crystal, ignore its hit energy.
                 if (!clusterCenters.contains(neighborId)) {
                     continue;
                 }
+                
+                // Get the cluster energy of the neighboring crystals.
                 hitSum = hitSums.get(neighborId);
+                
+                // If the neighbor crystal has no cluster energy, then set the
+                // cluster energy to the current hit's energy.
                 if (hitSum == null) {
                     hitSums.put(neighborId, hit.getRawEnergy());
-                } else {
+                }
+                // Otherwise, add the hit's energy to the neighbor's cluster energy.
+                else {
                     hitSums.put(neighborId, hitSum + hit.getRawEnergy());
                 }
             }
         }
     }
-
+    
     public List<HPSEcalCluster> createClusters() {
-//		boolean printClusters;
-        // New Cluster list to be added to event.
+        // Create a list of clusters to be added to the event,
         List<HPSEcalCluster> clusters = new ArrayList<HPSEcalCluster>();
-        //System.out.println("New event");
-        //for each crystal with a nonzero hit count, test for cluster
-        for (Long possibleCluster : hitSums.keySet()) {
+        
+        // We examine each crystal with a non-zero cluster energy.
+        for(Long possibleCluster : hitSums.keySet()) {
+            // Get the luster energy for the crystal this hit is assocaite with.
             Double thisSum = hitSums.get(possibleCluster);
-
-            // Get neighbor crystal IDs.
+            
+            // Get neighboring crystals' IDs.
             Set<Long> neighbors = neighborMap.get(possibleCluster);
-
+            
+            // If there are no neighbors, throw an error.
             if (neighbors == null) {
                 throw new RuntimeException("Oops!  Set of neighbors is null!");
             }
-
-            //Apply peak detector scheme.
-            // Set the ID.
+            
+            // Get the x/y position of the hit's associated crystal.
             dec.setID(possibleCluster);
             int x1 = dec.getValue("ix");
             int y1 = dec.getValue("iy");
-//			System.out.printf("\nThis cluster: E= %f, ID=%d, x=%d, y=%d, neighbors=%d\n", thisSum, possibleCluster, x1, y1, neighbors.size());
+            
+            // Store whether it is a valid cluster or not.
             boolean isCluster = true;
+            
+            // Check to see if any of the crystal's neighbors preclude the current crystal
+            // from being a proper cluster. The cluster crystal should have the highest
+            // energy among its neighbors
             for (Long neighborId : neighbors) {
-                // Set the ID.
+                // Get the x/y position of the neighbor's associated crystal.
                 dec.setID(neighborId);
                 int x2 = dec.getValue("ix");
                 int y2 = dec.getValue("iy");
-
+                
+                // If the neighbor's energy value does not exist, we don't need to perform
+                // any additional checks for this neighbor. A crystal with no energy can
+                // not be the center of a cluster.
                 Double neighborSum = hitSums.get(neighborId);
                 if (neighborSum == null) {
                     continue;
                 }
-
-//				System.out.printf("Neighbor cluster: E= %f, ID=%d, x=%d, y=%d, neighbors=%d\n", neighborSum, neighborId, x2, y2, neighborMap.get(neighborId).size());
-
+                
+                // If the neighbor's energy value is greater than this crystal's value,
+                // then this crystal is not the cluster and we may terminate the check.
                 if (neighborSum > thisSum) {
-//					System.out.println("Reject cluster: sum cut");
                     isCluster = false;
                     break;
-                } //								else if (false) { //ctp
-                //								else if (neighborSum.equals(thisSum) && neighborId > possibleCluster) { //id
-                //								else if (neighborSum.equals(thisSum) && (x1<x2 || (x1==x2 && Math.abs(y1)>Math.abs(y2)))) { //right_in
-                //								else if (neighborSum.equals(thisSum) && (x1<x2 || (x1==x2 && Math.abs(y1)<Math.abs(y2)))) { //right_out
-                //								else if (neighborSum.equals(thisSum) && (x1>x2 || (x1==x2 && Math.abs(y1)>Math.abs(y2)))) { //left_in
-                else if (neighborSum.equals(thisSum) && (x1 > x2 || (x1 == x2 && Math.abs(y1) < Math.abs(y2)))) { //left_out
-//								else if (neighborSum.equals(thisSum) && (x1<x2 || (x1==x2 && y1<y2))) { //right_up
-//								else if (neighborSum.equals(thisSum) && (x1>x2 || (x1==x2 && y1<y2))) { //left_up
-//					System.out.println("Reject cluster: tie-breaker cut");
+                }
+                // If the crystals each have the same energy, we choose the crystal that
+                // is closest to the electron side of the detector. If both crystals are
+                // equally close, we choose the crystal closest to the beam gap. If the
+                // neighbor fits these parameters better, this is not a crystal and we
+                // may skip any further checks.
+                else if (neighborSum.equals(thisSum) && (x1 > x2 || (x1 == x2 && Math.abs(y1) < Math.abs(y2)))) {
                     isCluster = false;
                     break;
                 }
             }
-
+            
+            // If the crystal was not invalidated by the any of the neighboring crystals,
+            // then it is a cluster crystal and should be processed.
             if (isCluster) {
+                // Make a list to store the hits that are part of this cluster.
                 List<CalorimeterHit> hits = new ArrayList<CalorimeterHit>();
+                
+                // Store the time at which the cluster occurred.
                 double clusterTime = Double.NEGATIVE_INFINITY;
+                
+                // Get the last hit on this crystal.
                 CalorimeterHit hit = hitMap.get(possibleCluster);
+                
+                // If the hit exists, add it to the list of associated hits.
                 if (hit != null) {
                     hits.add(hit);
+                    
+                    // If the latest hit's time is later than the current cluster time,
+                    // set the cluster time to the latest hit's time.
                     if (hit.getTime() > clusterTime) {
                         clusterTime = hit.getTime();
                     }
                 }
+                
+                // Add all of the neighboring crystals to the cluster, if they have a
+                // hit associated with them. Crystals with no hits are not actually part
+                // of a cluster.
                 for (Long neighborId : neighbors) {
                     hit = hitMap.get(neighborId);
                     if (hit != null) {
@@ -281,22 +343,30 @@ public class CTPEcalClusterer extends Driver {
                         }
                     }
                 }
+                
+                // Generate a new cluster seed hit from the above results.
                 CalorimeterHit seedHit = new HPSCalorimeterHit(0.0, clusterTime, possibleCluster, hits.get(0).getType());
                 seedHit.setMetaData(hits.get(0).getMetaData());
+                
+                // Generate a new cluster from the seed hit.
                 HPSEcalCluster cluster = new HPSEcalCluster(seedHit);
+                
+                // Populate the cluster with each of the chosen neighbors.
                 for (CalorimeterHit clusterHit : hits) {
                     cluster.addHit(clusterHit);
                 }
+                
+                // Add the cluster to the cluster list.
                 clusters.add(cluster);
-//                System.out.println(cluster.getEnergy());
             }
         }
+        
+        // Return the list of clusters.
         return clusters;
     }
-
+    
     static class TimeComparator implements Comparator<CalorimeterHit> {
-
-        @Override
+        // Compare by time with the earlier coming before the later.
         public int compare(CalorimeterHit o1, CalorimeterHit o2) {
             if (o1.getTime() == o2.getTime()) {
                 return 0;
