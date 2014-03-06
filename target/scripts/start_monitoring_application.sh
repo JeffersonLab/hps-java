5dd54277f9217bda58d40737608cf9f9888825df
Jeremy McCormick <jermccormick@gmail.com>
2014-03-06 03:39:44 +0000

    Remove files committed in error by someone.

--- target/scripts/start_monitoring_application.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-classpath=/local/celentano/eclipse_workspace/hps/monitoring-app/target/hps-monitoring-app-3.0.2-SNAPSHOT-bin.jar
-prod="java -Xmx1024m -classpath "$classpath" org.hps.monitoring.MonitoringApplication $@"
-# -c 100"
-echo $prod
-exec $prod
