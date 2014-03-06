a092fe8ed56aea85d4a5643edf9eaf06a36f151e
Jeremy McCormick <jermccormick@gmail.com>
2014-03-06 03:39:44 +0000

    Remove files committed in error by someone.

--- et/target/scripts/et_server_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-et=/local/celentano/eclipse_workspace/hps/et/target/hps-et-3.0.2-SNAPSHOT.jar
-server="java -Xmx1024m -classpath $et org.jlab.coda.et.apps.StartEt $@" 
-
-echo "$server"
-exec $server
--- et/target/scripts/et_start_c_server_defaults.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/local/celentano/eclipse_workspace/hps/et/target/scripts/et_start_c_server.sh -d -f ETBuffer -s 10000 -nd
