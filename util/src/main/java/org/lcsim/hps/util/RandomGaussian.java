6ae5c5f5ba3963a948faf8ff0ece1f3d5b7b7efc
Jeremy McCormick <jermccormick@gmail.com>
2014-03-26 00:31:32 +0000

    Adding some classes to util temporarily while I sort out some module dependencies.

---
+++ util/src/main/java/org/lcsim/hps/util/RandomGaussian.java
@@ -0,0 +1,32 @@
+package org.lcsim.hps.util;
+
+//--- java ---//
+import java.util.Random;
+
+/**
+ * 
+ * @author Omar Moreno
+ * @version $Id: RandomGaussian.java,v 1.1 2012/08/27 19:01:36 omoreno Exp $
+ */
+public class RandomGaussian {
+
+    private static Random randNumberGenerator;
+        
+    /**
+     * Class shouldn't be instantiated by anyone 
+     */
+    private RandomGaussian(){
+    }
+
+    /**
+     * Generates a Gaussian distributed number with given mean and standard deviation
+     *
+     * @param mean : Mean of the distribution
+     * @param sigma : Standard deviation of the distribution
+     * @return Gaussian distributed number
+     */
+    public static double getGaussian(double mean, double sigma){
+            if(randNumberGenerator == null) randNumberGenerator = new Random();
+            return mean + randNumberGenerator.nextGaussian()*sigma;
+    }
+}
---
+++ util/src/main/java/org/lcsim/hps/util/Redrawable.java
@@ -0,0 +1,13 @@
+package org.lcsim.hps.util;
+
+/**
+ * Drivers that will be attached to monitoring system should implement 
+ * this if they will only redraw plots every N events, or when redraw() is called.
+ * Setting eventRefreshRate = 0 should disable automatic redrawing.
+ * @deprecated Individual Drivers should implement this behavior.  No interface should be necessary.
+ */
+@Deprecated
+public interface Redrawable {
+    public void redraw();
+    public void setEventRefreshRate(int eventRefreshRate);
+}
