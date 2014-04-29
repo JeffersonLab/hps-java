111c6bcf61593ed6358f1442f37d1426d98a01b0
Jeremy McCormick <jermccormick@gmail.com>
2014-04-29 16:06:39 +0000

    A very simple test

---
+++ users/src/main/java/org/hps/users/sfegan/HPSECalTestFegan.java
@@ -0,0 +1,14 @@
+package main.java.org.hps.users.sfegan;
+
+
+/**
+ * This Driver is a preliminary exploration of ECal stuff, it is hoped to
+ * form the basis of many new classes...
+ *
+ *
+ * @author Stuart Fegan <fegan@ge.infn.it>
+ */
+
+public class HPSECalTestFegan {
+
+}
