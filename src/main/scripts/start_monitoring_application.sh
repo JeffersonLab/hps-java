51e26546165f94dcfe557f55080d113f72d9e9bd
Jeremy McCormick <jermccormick@gmail.com>
2014-05-22 18:56:29 +0000

    Consolidate all scripts for running and testing the monitoring application into the monitoring-app module.  Fix for HPSJAVA-141 to set load library path automatically in run scripts.  (JM)

--- src/main/scripts/start_monitoring_application.sh
+++ src/main/scripts/start_monitoring_application.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-classpath=${project.basedir}/target/${project.artifactId}-${project.version}-bin.jar
-prod="java -Xmx1024m -classpath "$classpath" org.hps.monitoring.MonitoringApplication $@"
-# -c 100"
+# Set the load library path.
+. ${project.build.directory}/scripts/ldpath.sh
+
+# Setup the classpath variable.
+classpath=${project.build.directory}/${project.artifactId}-${project.version}-bin.jar
+
+# Start the monitoring application, sending any script arguments to the end of the command.
+prod="java -Xmx1024m -classpath $classpath org.hps.monitoring.MonitoringApplication $@"
 echo $prod
 exec $prod
