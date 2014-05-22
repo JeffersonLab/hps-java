588a25f2e472177d1a540334d9bc955f7c0aac90
Jeremy McCormick <jermccormick@gmail.com>
2014-05-22 18:56:29 +0000

    Consolidate all scripts for running and testing the monitoring application into the monitoring-app module.  Fix for HPSJAVA-141 to set load library path automatically in run scripts.  (JM)

---
+++ monitoring-app/src/main/scripts/evio_file_producer.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Set the load library path.
+. ${project.build.directory}/scripts/ldpath.sh
+
+# First argument, with no command switch, is name of EVIO file.
+eviofile=$1
+
+# Subsequent arguments are appended to the end of the command.
+shift
+
+# Classpath pointing to the hps-evio jar.
+classpath=${project.build.directory}/${project.artifactId}-${project.version}-bin.jar
+
+# Run the file producer, sending any additional arguments to the command.
+#prod="java -classpath $classpath org.hps.evio.EvioFileProducer -e ${eviofile} -f ETBuffer -host localhost -s 10000 -d 100 $@"
+prod="java -classpath $classpath org.hps.evio.EvioFileProducer -e ${eviofile} -f ETBuffer -host localhost -s 10000 $@"
+echo $prod
+exec $prod
