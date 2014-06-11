797aa1604aa081c25b3fa85570b6cad852d27c86
Jeremy McCormick <jermccormick@gmail.com>
2014-06-11 23:34:16 +0000

    Oops.  Forgot to put this in last checkin.

---
+++ src/main/java/org/hps/monitoring/plotting/StripChartUtil.java
@@ -0,0 +1,17 @@
+package org.hps.monitoring.plotting;
+
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+
+
+public class StripChartUtil {
+
+    private StripChartUtil() {        
+    }
+    
+    public static TimeSeries getTimeSeries(JFreeChart chart) {
+        return (TimeSeries)((TimeSeriesCollection)chart.getXYPlot().getDataset()).getSeries().get(0);
+    }
+    
+}
