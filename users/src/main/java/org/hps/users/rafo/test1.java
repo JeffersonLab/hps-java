bc7c4e3de678eea315f66bb70ecd0eba01ebf8e0
Kyle McCarty <mccaky@gmail.com>
2014-09-02 21:58:56 +0000

    Added fixed test code and steering file.

--- users/src/main/java/org/hps/users/rafo/test1.java
+++ users/src/main/java/org/hps/users/rafo/test1.java
@@ -1,33 +1,18 @@
 package org.hps.users.rafo;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import org.lcsim.event.CalorimeterHit;
 import org.lcsim.event.EventHeader;
-import org.lcsim.geometry.Detector;
-import org.lcsim.geometry.subdetector.HPSEcal3;
-import org.lcsim.geometry.subdetector.HPSEcal3.NeighborMap;
-import org.lcsim.lcio.LCIOConstants;
 import org.lcsim.util.Driver;
 
-public class test1 extends Driver
-{
-    int clusterID;
-
-    public void process(EventHeader event )
-    {
-	System.out.println("The cluster ID = " + clusterID);
-    }
+public class test1 extends Driver {
+	private int clusterID;
     
-    public void setClusterID( int a_clusterID )
-    {
-	clusterID = a_clusterID;
-    }
+	public void process(EventHeader event) {
+    	System.out.println("The cluster ID = " + clusterID);
+	}
+    
+	public void setClusterID(int clusterID) {
+    	this.clusterID = clusterID;
+	}
 }
 
 
