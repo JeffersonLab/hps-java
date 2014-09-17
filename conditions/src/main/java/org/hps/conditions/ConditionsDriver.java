2af87da118627caa4142408bb4d6a2c1a1bfa868
Omar Moreno <email@omarmoreno.net>
2014-09-17 18:27:46 +0000

    Change the name of the class HpsConditionsDriver to ConditionsDriver to make Jeremy happy.

---
+++ conditions/src/main/java/org/hps/conditions/ConditionsDriver.java
@@ -0,0 +1,25 @@
+package org.hps.conditions;
+
+/**
+ * This {@link org.lcsim.util.Driver} is a subclass of {@link AbstractConditionsDriver}
+ * and specifies the database connection parameters and configuration for the
+ * development database. 
+ * 
+ * @author Omar Moreno <omoreno1@ucsc.edu>
+ * @author Jeremy McCormick <jeremym@slac.stanford.edu>
+ */
+public class ConditionsDriver extends AbstractConditionsDriver {
+
+    // Default conditions system XML config, which is for the Test Run 2012 database.
+    static final String DB_CONFIG = "/org/hps/conditions/config/conditions_dev.xml";
+
+    // Default database connection parameters, which points to the SLAC development database.
+    static final String DB_CONNECTION = "/org/hps/conditions/config/conditions_dev.properties";
+    
+    public ConditionsDriver(){
+        manager = new DatabaseConditionsManager();
+        manager.setConnectionResource(DB_CONNECTION);
+        manager.configure(DB_CONFIG);
+        manager.register();
+    }
+}
