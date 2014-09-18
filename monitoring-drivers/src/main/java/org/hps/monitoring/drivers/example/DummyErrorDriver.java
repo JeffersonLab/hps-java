23c454cb46afbcf69cb1cc455c11302ea51f5489
Jeremy McCormick <jermccormick@gmail.com>
2014-09-18 22:51:19 +0000

    Add a few monitoring drivers used for testing.

---
+++ monitoring-drivers/src/main/java/org/hps/monitoring/drivers/example/DummyErrorDriver.java
@@ -0,0 +1,53 @@
+package org.hps.monitoring.drivers.example;
+
+import org.lcsim.event.EventHeader;
+import org.lcsim.geometry.Detector;
+import org.lcsim.util.Driver;
+
+/**
+ * Driver for testing the error handling in the monitoring app.
+ * @author Jeremy McCormick <jeremym@slac.stanford.edu>
+ */
+public class DummyErrorDriver extends Driver {
+    
+    boolean throwProcess = true;
+    boolean throwEndOfData = false;
+    boolean throwStartOfData = false;
+    boolean throwDetectorChanged = false;
+    
+    public void throwProcess(boolean throwProcess) {
+        this.throwProcess = throwProcess;
+    }
+    
+    public void throwEndOfData(boolean throwEndOfData) {
+        this.throwEndOfData = throwEndOfData;
+    }
+    
+    public void throwStartOfData(boolean throwStartOfData) {
+        this.throwStartOfData = throwStartOfData;
+    }
+    
+    public void throwDetectorChanged(boolean throwDetectorChanged) {
+        this.throwDetectorChanged = throwDetectorChanged;
+    }
+    
+    public void startOfData() {
+        if (throwStartOfData)
+            throw new RuntimeException("This is a dummy error from the startOfData method.");
+    }
+    
+    public void endOfData() {
+        if (throwEndOfData)
+            throw new RuntimeException("This is a dummy error from the endOfData method.");
+    }
+    
+    public void process(EventHeader event) {
+        if (throwProcess)
+            throw new RuntimeException("This is a dummy error from the process method.");
+    }
+    
+    public void detectorChanged(Detector detector) {
+        if (throwDetectorChanged)
+            throw new RuntimeException("This is a dummy error from the detectorChanged method.");
+    }
+}
