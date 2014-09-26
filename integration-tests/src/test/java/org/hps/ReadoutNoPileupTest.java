19b58e1a8d0e6ad5cef5eebe5719c5fc5b68e5f9
Jeremy McCormick <jermccormick@gmail.com>
2014-09-26 01:45:28 +0000

    Add javadoc.

--- integration-tests/src/test/java/org/hps/ReadoutNoPileupTest.java
+++ integration-tests/src/test/java/org/hps/ReadoutNoPileupTest.java
@@ -8,7 +8,22 @@ import org.lcsim.util.cache.FileCache;
 import org.lcsim.util.test.TestUtil.TestOutputFile;
 
 import junit.framework.TestCase;
-
+/**
+ * <p>
+ * This test runs the readout simulation on MC events without simulated pile-up and writes LCIO.
+ * <p>
+ * According to the Confluence documentation for this task:
+ * <p>
+ * "Each event is treated independently, all detectors are reset between events".
+ * <p>
+ * <a href="https://confluence.slac.stanford.edu/display/hpsg/Running+Readout+Simulation">Running Readout Simulation</a>
+ * <p>
+ * The original name of the input MC file was:
+ * <p>
+ * ap2.2gev100mev_SLIC-v04-00-00_Geant4-v10-00-02_QGSP_BERT_HPS-Proposal2014-v8-2pt2.slcio
+ * 
+ * @author Jeremy McCormick <jeremym@slac.stanford.edu>
+ */
 public class ReadoutNoPileupTest extends TestCase {
         
     public void testReadoutNoPileup() throws Exception {
