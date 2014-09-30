44fc6fbc6a13f6dcc9dad1540ca2a2cdf356afbe
Jeremy McCormick <jermccormick@gmail.com>
2014-09-30 20:54:52 +0000

    Add basic class doc.

--- plugin/src/main/java/org/hps/plugin/HPSPlugin.java
+++ plugin/src/main/java/org/hps/plugin/HPSPlugin.java
@@ -3,7 +3,7 @@ package org.hps.plugin;
 import org.freehep.application.studio.Plugin;
 
 /**
- *
+ * This is just a dummy plugin class to make JAS3 happy.
  * @author Jeremy McCormick
  * @version $Id: HPSPlugin.java,v 1.1 2013/06/03 16:23:47 jeremy Exp $
  */
