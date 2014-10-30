6fc2bf6b7f2c28122503264ca50fb7de36cd1079
Omar Moreno <email@omarmoreno.net>
2014-10-30 20:04:24 +0000

    HPS Test run implementation of AbstractSvtT0Shifts.  This class is used to store and access t0 shifts using ID's specific to the HPS test run.

---
+++ conditions/src/main/java/org/hps/conditions/svt/TestRunSvtT0Shift.java
@@ -0,0 +1,54 @@
+package org.hps.conditions.svt;
+
+import org.hps.util.Pair;
+
+/**
+ * This class is a container that allows associating a t0 shift with a specific 
+ * sensor by FPGA ID and hybrid ID.
+ * 
+ * @author Omar Moreno <omoreno1@ucsc.edu>
+ */
+public final class TestRunSvtT0Shift extends AbstractSvtT0Shift {
+
+	public static class TestRunSvtT0ShiftCollection
+			extends AbstractSvtT0Shift.AbstractSvtT0ShiftCollection<TestRunSvtT0Shift> {
+
+        /**
+         * Get the {@link TestRunSvtT0Shift} associated with a given DAQ pair
+         * 
+         * @param DAQ pair for a given sensor
+         * @return The {@link TestRunSvtT0Shift} associated with the DAQ pair.  If a t0 shift
+         *         for a given DAQ pair can't be found, it returns null.
+         */
+		@Override
+		public TestRunSvtT0Shift getT0Shift(Pair<Integer, Integer> pair) {
+	
+            int fpgaID = pair.getFirstElement();
+            int hybridID = pair.getSecondElement();
+            for (TestRunSvtT0Shift t0Shift : this.getObjects()) {
+                if (t0Shift.getFpgaID() == fpgaID && t0Shift.getHybridID() == hybridID) {
+                    return t0Shift;
+                }
+            }
+            return null;
+		} 
+	}
+	
+	/**
+	 * Get the FPGA ID.
+	 * 
+	 * @return The FPGA ID.
+	 */
+	int getFpgaID() { 
+		return getFieldValue("fpga");
+	}
+	
+	/**
+	 * Get the hybrid ID.
+	 * 
+	 * @return The hybrid ID. 
+	 */
+	int getHybridID() { 
+		return getFieldValue("hybrid");
+	}
+}
