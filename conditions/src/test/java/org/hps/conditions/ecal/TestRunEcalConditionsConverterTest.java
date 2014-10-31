f77ad4571f6f2eea164c373cc5fb035180bddc73
Omar Moreno <email@omarmoreno.net>
2014-10-31 00:08:55 +0000

    Update EcalConditionsConverterTest so it loads engineering run conditions.  A separate test (TestRunEcalConditionsConverterTest) was added to check if the EcalConditionsConverter is loading test run conditions onto an EcalConditions object without errors.

--- conditions/src/test/java/org/hps/conditions/ecal/TestRunEcalConditionsConverterTest.java
+++ conditions/src/test/java/org/hps/conditions/ecal/TestRunEcalConditionsConverterTest.java
@@ -3,25 +3,28 @@ package org.hps.conditions.ecal;
 import junit.framework.TestCase;
 
 import org.hps.conditions.DatabaseConditionsManager;
-import org.hps.conditions.config.DevReadOnlyConfiguration;
+import org.hps.conditions.config.TestRunReadOnlyConfiguration;
 
 /**
- * Tests that a {@link EcalConditions} objects loads without errors.
- * @author Jeremy McCormick <jeremym@slac.stanford.edu>
+ * Tests that {@link EcalConditionsConverter} load test run conditions to 
+ * an {@link EcalConditions} without errors.
+ * 
+ * @author Omar Moreno <omoreno1@ucsc.edu>
  */
-public class EcalConditionsConverterTest extends TestCase {
+public class TestRunEcalConditionsConverterTest extends TestCase {
 
     DatabaseConditionsManager conditionsManager;
-    
-    public void setUp() {                
-        new DevReadOnlyConfiguration().setup().load("HPS-Proposal2014-v7-2pt2", 0);
+	
+	public void setUp() {
+        new TestRunReadOnlyConfiguration().setup().load("HPS-TestRun-v5", 1351);
         conditionsManager = DatabaseConditionsManager.getInstance();
-    }
-
+	}
+	
     public void test() {       
         // Test that the manager gets ECAL conditions.
         EcalConditions conditions = conditionsManager.getCachedConditions(EcalConditions.class, "ecal_conditions").getCachedData();
         assertNotNull(conditions);
         System.out.println(conditions);
     }
+	
 }
