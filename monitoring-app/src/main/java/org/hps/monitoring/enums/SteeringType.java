75447a344b3a56b53ce5bf94340b6790067ffe43
Jeremy McCormick <jermccormick@gmail.com>
2014-11-10 22:46:50 +0000

    Apply better enum formatting.

--- monitoring-app/src/main/java/org/hps/monitoring/enums/SteeringType.java
+++ monitoring-app/src/main/java/org/hps/monitoring/enums/SteeringType.java
@@ -4,5 +4,6 @@ package org.hps.monitoring.enums;
  * The type of steering to use for event processing.
  */
 public enum SteeringType {
-    RESOURCE, FILE;
+    RESOURCE,
+    FILE;
 }
--- monitoring-app/src/main/java/org/hps/monitoring/subsys/Subsystem.java
+++ monitoring-app/src/main/java/org/hps/monitoring/subsys/Subsystem.java
@@ -4,5 +4,8 @@ package org.hps.monitoring.subsys;
  * Names of valid subsystems that can be monitored.
  */
 public enum Subsystem {
-    ET, ECAL, SVT, TRIGGER;
+    ET,
+    ECAL,
+    SVT,
+    TRIGGER;
 }
