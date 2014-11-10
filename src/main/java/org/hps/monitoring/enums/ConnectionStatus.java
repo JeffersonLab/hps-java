857eb7600224ffab3d51fd18cda07dee42ca3567
Jeremy McCormick <jermccormick@gmail.com>
2014-11-10 22:46:50 +0000

    Apply better enum formatting.

--- src/main/java/org/hps/monitoring/enums/ConnectionStatus.java
+++ src/main/java/org/hps/monitoring/enums/ConnectionStatus.java
@@ -4,5 +4,10 @@ package org.hps.monitoring.enums;
  * Status of the connection to the ET server from the monitoring client.
  */
 public enum ConnectionStatus {
-    DISCONNECTED, DISCONNECTING, ERROR, CONNECTION_REQUESTED, CONNECTED, PAUSED
+    DISCONNECTED,
+    DISCONNECTING,
+    ERROR,
+    CONNECTION_REQUESTED,
+    CONNECTED,
+    PAUSED
 }
--- src/main/java/org/hps/monitoring/subsys/StatusCode.java
+++ src/main/java/org/hps/monitoring/subsys/StatusCode.java
@@ -4,5 +4,13 @@ package org.hps.monitoring.subsys;
  * Code that represents a sub-system status.
  */
 public enum StatusCode {
-    OKAY, UNKNOWN, CLEARED, OFFLINE, INFO, WARNING, ERROR, ALARM, HALT;
+    OKAY,
+    UNKNOWN,
+    CLEARED,
+    OFFLINE,
+    INFO,
+    WARNING,
+    ERROR,
+    ALARM,
+    HALT;
 }
