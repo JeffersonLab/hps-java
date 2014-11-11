d806c05ce29368fea56d06161453424a93561fe8
Jeremy McCormick <jermccormick@gmail.com>
2014-11-11 02:57:37 +0000

    Simplify the ConditionsSeries API.

--- conditions/src/main/java/org/hps/conditions/ConditionsSeries.java
+++ conditions/src/main/java/org/hps/conditions/ConditionsSeries.java
@@ -1,7 +1,6 @@
 package org.hps.conditions;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -10,38 +9,36 @@ import java.util.List;
  * are multiple ones with the same key that are valid for the current run.
  * 
  * @author Jeremy McCormick <jeremym@slac.stanford.edu>
- *
- * @param <CollectionType> The specific type of the
- *            <tt>ConditionsObjectCollection</tt> the series contains.
  */
-public class ConditionsSeries<CollectionType extends ConditionsObjectCollection> implements Iterable<CollectionType> {
+@SuppressWarnings("rawtypes")
+public class ConditionsSeries {
 
-    List<CollectionType> collections = new ArrayList<CollectionType>();
+    List<ConditionsObjectCollection> collections = new ArrayList<ConditionsObjectCollection>();
 
-    CollectionType getCollection(int series) {
+    public ConditionsObjectCollection getCollection(int series) {
         return collections.get(series);
     }
 
-    int addCollection(CollectionType collection) {
+    public int addCollection(ConditionsObjectCollection collection) {
         if (collections.contains(collection))
             throw new IllegalArgumentException("The collection is already registered with this object.");
         collections.add(collection);
         return collections.indexOf(collection);
     }
 
-    int getNumberOfCollections() {
+    public int getNumberOfCollections() {
         return collections.size();
     }
 
-    CollectionType findCollection(ConditionsRecord record) {
-        for (CollectionType collection : collections) {
+    public ConditionsObjectCollection findCollection(ConditionsRecord record) {
+        for (ConditionsObjectCollection collection : collections) {
             if (collection.conditionsRecord == record)
                 return collection;
         }
         return null;
     }
-
-    public Iterator<CollectionType> iterator() {
-        return collections.iterator();
+    
+    public List<ConditionsObjectCollection> getCollections() {
+        return collections;
     }
 }
