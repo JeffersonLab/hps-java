1516b5e5a07a1ab09580edab1d2cfb1f228e745a
Jeremy McCormick <jermccormick@gmail.com>
2014-11-17 21:27:32 +0000

    Add JobControlManager implementation that uses DatabaseConditionsManager by default.

---
+++ record-util/src/main/java/org/hps/job/JobManager.java
@@ -0,0 +1,17 @@
+package org.hps.job;
+
+import org.hps.conditions.database.DatabaseConditionsManager;
+import org.lcsim.job.JobControlManager;
+
+public class JobManager extends JobControlManager {
+    
+    public JobManager() {
+        // Override the basic LCSim conditions system with the HPS conditions manager.
+        new DatabaseConditionsManager();
+    }
+    
+    public static void main(String args[]) {
+        JobManager job = new JobManager();
+        job.run(args);
+    }
+}
