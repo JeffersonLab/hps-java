63463970e4e51738cba1d6ddbe11215a6af3d664
Jeremy McCormick <jermccormick@gmail.com>
2014-11-17 21:44:19 +0000

    Comment out constants moved to separate classes.  Remove methods that were moved to EvioEventUtilities class in record-util.

--- evio/src/main/java/org/hps/evio/EventConstants.java
+++ evio/src/main/java/org/hps/evio/EventConstants.java
@@ -1,18 +1,16 @@
 package org.hps.evio;
 
-import org.jlab.coda.jevio.EvioEvent;
 
 public final class EventConstants {
 
     //event type tag
-    public static final int PHYSICS_EVENT_TAG = 1;
-    public static final int SYNC_EVENT_TAG = 16;
-    public static final int PRESTART_EVENT_TAG = 17;
-    public static final int GO_EVENT_TAG = 18;
-    public static final int PAUSE_EVENT_TAG = 19;
-    public static final int END_EVENT_TAG = 20;
-    //event type tag
-    public static final int EVENTID_BANK_TAG = 0xC000;
+    //public static final int PHYSICS_EVENT_TAG = 1;
+    //public static final int SYNC_EVENT_TAG = 16;
+    //public static final int PRESTART_EVENT_TAG = 17;
+    //public static final int GO_EVENT_TAG = 18;
+    //public static final int PAUSE_EVENT_TAG = 19;
+    //public static final int END_EVENT_TAG = 20;
+    //event type tag    
     public static final int EVENT_BANK_NUM = 0xCC;
     // These correspond to ROC (readout crate) IDs from the DAQ.
     public static final int ECAL_TOP_BANK_TAG = 0x1;
@@ -42,42 +40,4 @@ public final class EventConstants {
     public static final int TRIGGER_BANK_TAG = 0xe106;
     public static final int SVT_TOTAL_NUMBER_FPGAS = 8;
     public static final int MC_TIME = 2019686400; //Unix time (in seconds) used for Monte Carlo data - 1/1/2034
-
-    public static boolean isSyncEvent(EvioEvent event) {
-        return event.getHeader().getTag() == EventConstants.SYNC_EVENT_TAG;
-    }
-
-    // TODO: Move these methods into an EvioEventUtil class.
-    
-    /**
-     * Check if this event is a Pre Start Event.
-     *
-     * @param event
-     * @return
-     */
-    public static boolean isPreStartEvent(EvioEvent event) {
-        return event.getHeader().getTag() == EventConstants.PRESTART_EVENT_TAG;
-    }
-
-    public static boolean isGoEvent(EvioEvent event) {
-        return event.getHeader().getTag() == EventConstants.GO_EVENT_TAG;
-    }
-
-    public static boolean isPauseEvent(EvioEvent event) {
-        return event.getHeader().getTag() == EventConstants.PAUSE_EVENT_TAG;
-    }
-
-    /**
-     * Check if this event is an End Event.
-     *
-     * @param event
-     * @return True if this event is an End Event; false if not.
-     */
-    public static boolean isEndEvent(EvioEvent event) {
-        return event.getHeader().getTag() == EventConstants.END_EVENT_TAG;
-    }
-    
-    public static boolean isPhysicsEvent(EvioEvent event) {
-        return event.getHeader().getTag() == EventConstants.PHYSICS_EVENT_TAG;
-    }
 }
