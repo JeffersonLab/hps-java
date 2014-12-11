3b439b1e58ae3b3144782afed1b65230f10e6ccc
Jeremy McCormick <jermccormick@gmail.com>
2014-12-11 20:18:09 +0000

    Add incomplete test so I can sync to laptop.

---
+++ conditions/src/test/java/org/hps/conditions/api/AbstractConditionsObjectCollectionTest.java
@@ -0,0 +1,54 @@
+package org.hps.conditions.api;
+
+import java.sql.SQLException;
+
+import junit.framework.TestCase;
+
+import org.hps.conditions.database.ConnectionParameters;
+import org.hps.conditions.database.DatabaseConditionsManager;
+import org.hps.conditions.database.TableMetaData;
+
+public class AbstractConditionsObjectCollectionTest extends TestCase {
+    
+    public void testAbstractConditionsObjectCollection() throws Exception {
+        
+        ConnectionParameters connection = 
+                new ConnectionParameters("hpscalibrations", "heavyphotonsearch", "hps_conditions_dev", "ppa-jeremym-l.slac.stanford.edu", ConnectionParameters.DEFAULT_PORT);
+        DatabaseConditionsManager conditionsManager = DatabaseConditionsManager.getInstance();
+        TableMetaData tableMetaData = new TableMetaData("dummy_conditions", "dummy_conditions", DummyObject.class, DummyCollection.class);
+        conditionsManager.setConnectionParameters(connection);
+        conditionsManager.setDetector(DatabaseConditionsManager.getDefaultEngRunDetectorName(), 2000);
+        conditionsManager.addTableMetaData(tableMetaData);
+                        
+        DummyCollection collection = new DummyCollection();
+        collection.add(new DummyObject(42));
+        
+        // Test inserting the collection into the database.
+        try {
+            collection.insert();
+        } catch (ConditionsObjectException | SQLException e) {
+            throw new RuntimeException("Error inserting object into database.", e);
+        }
+                                
+        // TODO: test select into the collection
+        
+        // TODO: test update records from the collection to db
+        
+        // TODO: test delete records in the db by previously assigned collection ID
+    }
+    
+    static class DummyObject extends AbstractConditionsObject {
+                
+        DummyObject(int dummyValue) {
+            this.setFieldValue("dummy_value", dummyValue);
+        }
+        
+        public void getDummyValue() {
+            this.getFieldValue("dummy_value");
+        }
+        
+    }
+    
+    static class DummyCollection extends AbstractConditionsObjectCollection<DummyObject> {        
+    }    
+}
