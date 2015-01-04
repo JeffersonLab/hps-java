cc7b5a2ade19d7d4578a8a844f70266d46f99094
Jeremy McCormick <jermccormick@gmail.com>
2015-01-04 02:11:15 +0000

    Remove usage of CalibrationDriver.

--- evio/src/main/java/org/hps/evio/TestRunTriggeredReconToEvio.java
+++ evio/src/main/java/org/hps/evio/TestRunTriggeredReconToEvio.java
@@ -6,7 +6,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-import org.hps.conditions.deprecated.CalibrationDriver;
 import org.hps.readout.ecal.ReadoutTimestamp;
 import org.hps.readout.ecal.TriggerableDriver;
 import org.hps.record.evio.EvioEventConstants;
