c6e60128a87cf8c555d8e5522d867d1d30b6bde5
Jeremy McCormick <jermccormick@gmail.com>
2015-03-14 19:24:51 +0000

    Add a default display color to system status codes for GUI use.

--- monitoring-util/src/main/java/org/hps/monitoring/subsys/StatusCode.java
+++ monitoring-util/src/main/java/org/hps/monitoring/subsys/StatusCode.java
@@ -1,16 +1,29 @@
 package org.hps.monitoring.subsys;
 
+import java.awt.Color;
+
 /**
  * Code that represents a sub-system status.
  */
 public enum StatusCode {
-    OKAY,
-    UNKNOWN,
-    CLEARED,
-    OFFLINE,
-    INFO,
-    WARNING,
-    ERROR,
-    ALARM,
-    HALT;
+    
+    OKAY(Color.GREEN),
+    UNKNOWN(Color.GRAY),
+    CLEARED(Color.LIGHT_GRAY),
+    OFFLINE(Color.ORANGE),
+    INFO(Color.WHITE),
+    WARNING(Color.YELLOW),
+    ERROR(Color.RED),
+    ALARM(Color.RED),
+    HALT(Color.RED);
+    
+    Color color;
+    
+    StatusCode(Color color) {
+        this.color = color;
+    }
+    
+    public Color getColor() {
+        return color;
+    }    
 }
