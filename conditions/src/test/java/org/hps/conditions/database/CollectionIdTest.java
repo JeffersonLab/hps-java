6e1541173296ef27b0275da6ff71b0ab81b174af
Jeremy McCormick <jermccormick@gmail.com>
2015-04-21 22:55:32 +0000

    Add simple collection ID test (excluded from build).

---
+++ conditions/src/test/java/org/hps/conditions/database/CollectionIdTest.java
@@ -0,0 +1,51 @@
+/**
+ *
+ */
+package org.hps.conditions.database;
+
+import java.sql.SQLException;
+
+import junit.framework.TestCase;
+
+/**
+ * Test adding a new collection and getting its unique ID.
+ *
+ * @author <a href="mailto:jeremym@slac.stanford.edu">Jeremy McCormick</a>
+ */
+public class CollectionIdTest extends TestCase {
+
+    /**
+     * The conditions manager.
+     */
+    private DatabaseConditionsManager manager;
+
+    /**
+     * Setup the test.
+     */
+    @Override
+    public void setUp() {
+        this.manager = DatabaseConditionsManager.getInstance();
+        this.manager.openConnection();
+    }
+
+    /**
+     * Tear down the test.
+     */
+    @Override
+    public void tearDown() {
+        this.manager.closeConnection();
+    }
+
+    /**
+     * Run the test.
+     *
+     * @throws SQLException if there is an error executing SQL queries
+     */
+    public void testCollectionId() throws SQLException {
+        int collectionId = this.manager.addCollection("dummy", "foo bar baz");
+        System.out.println("created new collection " + collectionId);
+
+        collectionId = this.manager.addCollection("dummy", null);
+        System.out.println("created new collection " + collectionId);
+    }
+}
