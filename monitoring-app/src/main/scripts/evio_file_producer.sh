83ff826271f8ccf805d2db218e059e4ce46eb960
Jeremy McCormick <jermccormick@gmail.com>
2015-05-11 21:57:39 +0000

    Correct script so it actually works.

--- monitoring-app/src/main/scripts/evio_file_producer.sh
+++ monitoring-app/src/main/scripts/evio_file_producer.sh
@@ -3,9 +3,6 @@
 # First argument, with no command switch, is name of EVIO file.
 eviofile=$1
 
-# Subsequent arguments are appended to the end of the command.
-shift
-
 # Classpath pointing to the hps-evio jar.
 classpath=${project.build.directory}/${project.artifactId}-${project.version}-bin.jar
 
