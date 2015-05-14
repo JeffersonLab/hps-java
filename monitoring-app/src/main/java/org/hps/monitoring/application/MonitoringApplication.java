74ff5013ec5b674596dcd321f5664f85b793275d
Jeremy McCormick <jermccormick@gmail.com>
2015-05-14 02:39:09 +0000

    Use cleaned up PDF export method for writing plotter graphics to file.

--- monitoring-app/src/main/java/org/hps/monitoring/application/MonitoringApplication.java
+++ monitoring-app/src/main/java/org/hps/monitoring/application/MonitoringApplication.java
@@ -771,7 +771,10 @@ final class MonitoringApplication implements ActionListener, PropertyChangeListe
                 try {
                     if ("pdf".equals(extension)) {
                         // Write out all plot graphics from the tabs to a single PDF file.
-                        ExportPdf.write(this.frame.getPlotPanel().getPlotPane(), fileName, getRunData());
+                        ExportPdf.write( 
+                                MonitoringPlotFactory.getPlotterRegistry().getPlotters(),
+                                fileName, 
+                                getRunData());
                     } else {
                         // Save plot object data to AIDA or ROOT file.
                         AIDA.defaultInstance().saveAs(fileName);
