4ded6685f15bc0a727131b6c138cc5d72f11bc01
Sho Uemura <supersho@gmail.com>
2015-05-28 18:18:51 +0000

    rename plots and variables

--- users/src/main/java/org/hps/users/meeg/SVTSmallHitsDriver.java
+++ users/src/main/java/org/hps/users/meeg/SVTSmallHitsDriver.java
@@ -25,9 +25,9 @@ public class SVTSmallHitsDriver extends Driver {
     private String rawTrackerHitCollectionName = "SVTRawTrackerHits";
     private static final int SYNC_INTERVAL = 35 * 24;
     private static final int READOUT_LENGTH = 6 * 140 * 24;
-    private long[] previousTriggerTimesPhased = new long[5];
-    private long[] previousTriggerTimes = new long[5];
-    private long[] previousReadoutTimes = new long[5];
+    private long[] triggerSyncTimes = new long[5];
+    private long[] triggerTimes = new long[5];
+    private long[] readoutTimes = new long[5];
     private boolean printADCValues = false;
     private boolean printEvents = false;
     Map<HpsSiSensor, Set<Integer>> hitMap;
@@ -40,12 +40,12 @@ public class SVTSmallHitsDriver extends Driver {
     private int syncPhaseOffset = 12;
     private int daqDelay = 6200;
 
-    IHistogram1D trigDt, trigDtWithSmallHits, smallHitFracDt, smallHitCount1D;
-    IHistogram2D smallHitsDt2D;
-    IHistogram1D trigDtPhased, trigDtWithSmallHitsPhased, smallHitFracDtPhased;
-    IHistogram2D smallHitsDt2DPhased;
-    IHistogram1D trigDtReadout, trigDtWithSmallHitsReadout, smallHitFracDtReadout;
-    IHistogram2D smallHitsDt2DReadout;
+    IHistogram1D trigDt, trigDtWithSmallHits, smallHitFracVsTrigDt, smallHitCount1D;
+    IHistogram2D smallHitsVsTrigDt;
+    IHistogram1D syncDt, syncDtWithSmallHits, smallHitFracVsSyncDt;
+    IHistogram2D smallHitsVsSyncDt;
+    IHistogram1D readoutDt, readoutDtWithSmallHits, smallHitFracVsReadoutDt;
+    IHistogram2D smallHitsVsReadoutDt;
     IHistogram2D trigDt2DWithSmallHits;
 
     IHistogram2D trigDtPhase, trigDtWithSmallHitsPhase, trigDtReadoutWithSmallHitsPhase;
@@ -63,48 +63,48 @@ public class SVTSmallHitsDriver extends Driver {
 
         smallHitCount1D = aida.histogram1D("small hit count", 100, 0, 100);
 
-        trigDt2DWithSmallHits = aida.histogram2D("trigger dt vs. previous trigger dt, events with small hits", 1000, 0, 9.6e4, 1000, 0, 9.6e4);
+        trigDt2DWithSmallHits = aida.histogram2D("trigger-to-trigger dt vs. trigger-to-trigger dt, events with small hits", 1000, 0, 9.6e4, 1000, 0, 9.6e4);
 
-        trigDt = aida.histogram1D("trigger dt", 4000, 0, 9.6e4);
-        trigDtWithSmallHits = aida.histogram1D("trigger dt, events with small hits", 4000, 0, 9.6e4);
-        smallHitsDt2D = aida.histogram2D("small hit count vs. trigger dt", 4000, 0, 9.6e4, 100, 0, 100);
-        smallHitFracDt = aida.histogram1D("fraction of events with small hits vs. dt", 4000, 0, 9.6e4);
+        trigDt = aida.histogram1D("trigger-to-trigger dt", 4000, 0, 9.6e4);
+        trigDtWithSmallHits = aida.histogram1D("trigger-to-trigger dt, events with small hits", 4000, 0, 9.6e4);
+        smallHitsVsTrigDt = aida.histogram2D("small hit count vs. trigger-to-trigger dt", 4000, 0, 9.6e4, 100, 0, 100);
+        smallHitFracVsTrigDt = aida.histogram1D("fraction of events with small hits vs. trigger-to-trigger dt", 4000, 0, 9.6e4);
 
-        trigDtPhased = aida.histogram1D("trigger dt, sync phase subtracted", 4000, 0, 9.6e4);
-        trigDtWithSmallHitsPhased = aida.histogram1D("trigger dt, events with small hits, sync phase subtracted", 4000, 0, 9.6e4);
-        smallHitsDt2DPhased = aida.histogram2D("small hit count vs. trigger dt, sync phase subtracted", 4000, 0, 9.6e4, 100, 0, 100);
-        smallHitFracDtPhased = aida.histogram1D("fraction of events with small hits vs. dt, sync phase subtracted", 4000, 0, 9.6e4);
+        syncDt = aida.histogram1D("trigger-to-trigger dt, sync phase subtracted", 4000, 0, 9.6e4);
+        syncDtWithSmallHits = aida.histogram1D("trigger-to-trigger dt, events with small hits, sync phase subtracted", 4000, 0, 9.6e4);
+        smallHitsVsSyncDt = aida.histogram2D("small hit count vs. trigger-to-trigger dt, sync phase subtracted", 4000, 0, 9.6e4, 100, 0, 100);
+        smallHitFracVsSyncDt = aida.histogram1D("fraction of events with small hits vs. trigger-to-trigger dt, sync phase subtracted", 4000, 0, 9.6e4);
 
-        trigDtReadout = aida.histogram1D("trigger dt, using previous readout time", 4000, 0, 9.6e4);
-        trigDtWithSmallHitsReadout = aida.histogram1D("trigger dt, events with small hits, using previous readout time", 4000, 0, 9.6e4);
-        smallHitsDt2DReadout = aida.histogram2D("small hit count vs. trigger dt, using previous readout time", 4000, 0, 9.6e4, 100, 0, 100);
-        smallHitFracDtReadout = aida.histogram1D("fraction of events with small hits vs. dt, using previous readout time", 4000, 0, 9.6e4);
+        readoutDt = aida.histogram1D("readout-to-trigger dt", 4000, 0, 9.6e4);
+        readoutDtWithSmallHits = aida.histogram1D("readout-to-trigger dt, events with small hits", 4000, 0, 9.6e4);
+        smallHitsVsReadoutDt = aida.histogram2D("small hit count vs. readout-to-trigger dt", 4000, 0, 9.6e4, 100, 0, 100);
+        smallHitFracVsReadoutDt = aida.histogram1D("fraction of events with small hits vs. readout-to-trigger dt", 4000, 0, 9.6e4);
 
-        trigDtPhase = aida.histogram2D("sync phase vs. trigger dt", 4000, 0, 9.6e4, 210, 0, SYNC_INTERVAL);
-        trigDtWithSmallHitsPhase = aida.histogram2D("sync phase vs. trigger dt, events with small hits", 4000, 0, 9.6e4, 210, 0, SYNC_INTERVAL);
-        trigDtReadoutWithSmallHitsPhase = aida.histogram2D("sync phase vs. trigger dt, using previous readout time, events with small hits", 4000, 0, 9.6e4, 210, 0, SYNC_INTERVAL);
+        trigDtPhase = aida.histogram2D("sync phase vs. trigger-to-trigger dt", 4000, 0, 9.6e4, 210, 0, SYNC_INTERVAL);
+        trigDtWithSmallHitsPhase = aida.histogram2D("sync phase vs. trigger-to-trigger dt, events with small hits", 4000, 0, 9.6e4, 210, 0, SYNC_INTERVAL);
+        trigDtReadoutWithSmallHitsPhase = aida.histogram2D("sync phase vs. readout-to-trigger dt, events with small hits", 4000, 0, 9.6e4, 210, 0, SYNC_INTERVAL);
     }
 
 //    @Override
     protected void process(EventHeader event) {
-        for (int i = previousTriggerTimes.length - 1; i > 0; i--) {
-            previousTriggerTimes[i] = previousTriggerTimes[i - 1];
+        for (int i = triggerTimes.length - 1; i > 0; i--) {
+            triggerTimes[i] = triggerTimes[i - 1];
         }
-        previousTriggerTimes[0] = event.getTimeStamp();
+        triggerTimes[0] = event.getTimeStamp();
 
-        for (int i = previousTriggerTimesPhased.length - 1; i > 0; i--) {
-            previousTriggerTimesPhased[i] = previousTriggerTimesPhased[i - 1];
+        for (int i = triggerSyncTimes.length - 1; i > 0; i--) {
+            triggerSyncTimes[i] = triggerSyncTimes[i - 1];
         }
-        previousTriggerTimesPhased[0] = getNextSyncTime(event.getTimeStamp(), syncPhaseOffset);
+        triggerSyncTimes[0] = getNextSyncTime(event.getTimeStamp(), syncPhaseOffset);
 
-        for (int i = previousReadoutTimes.length - 1; i > 0; i--) {
-            previousReadoutTimes[i] = previousReadoutTimes[i - 1];
+        for (int i = readoutTimes.length - 1; i > 0; i--) {
+            readoutTimes[i] = readoutTimes[i - 1];
         }
-        previousReadoutTimes[0] = Math.max(getNextSyncTime(event.getTimeStamp() + daqDelay, syncPhaseOffset + daqDelay), previousReadoutTimes[1] + READOUT_LENGTH);
+        readoutTimes[0] = Math.max(getNextSyncTime(event.getTimeStamp() + daqDelay, syncPhaseOffset + daqDelay), readoutTimes[1] + READOUT_LENGTH);
 
-        int dt = (int) (previousTriggerTimes[0] - previousTriggerTimes[1]);
-        int dtReadout = (int) (previousTriggerTimes[0] - previousReadoutTimes[1]);
-        long previousTimestampPhased = previousTriggerTimesPhased[1];
+        int dt = (int) (triggerTimes[0] - triggerTimes[1]);
+        int dtReadout = (int) (triggerTimes[0] - readoutTimes[1]);
+        long previousTimestampPhased = triggerSyncTimes[1];
         int dtPhased = (int) (event.getTimeStamp() - previousTimestampPhased);
 
         if (event.hasCollection(RawTrackerHit.class, rawTrackerHitCollectionName)) {
@@ -143,20 +143,20 @@ public class SVTSmallHitsDriver extends Driver {
                 }
             }
             trigDt.fill(dt);
-            trigDtPhased.fill(dtPhased);
-            trigDtReadout.fill(dtReadout);
+            syncDt.fill(dtPhased);
+            readoutDt.fill(dtReadout);
             smallHitCount1D.fill(smallHitCount);
-            smallHitsDt2D.fill(dt, smallHitCount);
-            smallHitsDt2DPhased.fill(dtPhased, smallHitCount);
-            smallHitsDt2DReadout.fill(dtReadout, smallHitCount);
-            trigDtPhase.fill(dt, previousTriggerTimes[1] % SYNC_INTERVAL);
+            smallHitsVsTrigDt.fill(dt, smallHitCount);
+            smallHitsVsSyncDt.fill(dtPhased, smallHitCount);
+            smallHitsVsReadoutDt.fill(dtReadout, smallHitCount);
+            trigDtPhase.fill(dt, triggerTimes[1] % SYNC_INTERVAL);
             if (smallHitCount > 3) {
                 trigDtWithSmallHits.fill(dt);
-                trigDt2DWithSmallHits.fill(previousTriggerTimes[0] - previousTriggerTimesPhased[1], previousTriggerTimes[0] - previousTriggerTimesPhased[2]);
-                trigDtWithSmallHitsPhased.fill(dtPhased);
-                trigDtWithSmallHitsReadout.fill(dtReadout);
-                trigDtWithSmallHitsPhase.fill(dt, previousTriggerTimes[1] % SYNC_INTERVAL);
-                trigDtReadoutWithSmallHitsPhase.fill(dtReadout, previousTriggerTimes[1] % SYNC_INTERVAL);
+                trigDt2DWithSmallHits.fill(triggerTimes[0] - triggerSyncTimes[1], triggerTimes[0] - triggerSyncTimes[2]);
+                syncDtWithSmallHits.fill(dtPhased);
+                readoutDtWithSmallHits.fill(dtReadout);
+                trigDtWithSmallHitsPhase.fill(dt, triggerTimes[1] % SYNC_INTERVAL);
+                trigDtReadoutWithSmallHitsPhase.fill(dtReadout, triggerTimes[1] % SYNC_INTERVAL);
                 dtSumWith += dt;
                 nHitsWith += smallHitCount;
                 nWith++;
@@ -167,7 +167,7 @@ public class SVTSmallHitsDriver extends Driver {
             }
             if (printEvents) {
                 System.out.format("%f ns with small hits (%d events with %f small hits),\t%f ns without small hits (%d events with %f small hits)\n", dtSumWith / nWith, nWith, nHitsWith / nWith, dtSumWithout / nWithout, nWithout, nHitsWithout / nWithout);
-                System.out.format("%d %d %d %d ", event.getEventNumber(), event.getTimeStamp(), event.getTimeStamp() - previousTriggerTimes[1], smallHitCount);
+                System.out.format("%d %d %d %d ", event.getEventNumber(), event.getTimeStamp(), event.getTimeStamp() - triggerTimes[1], smallHitCount);
             }
 
             if (printEvents) {
@@ -202,13 +202,13 @@ public class SVTSmallHitsDriver extends Driver {
 
     public void endOfData() {
         if (trigDt != null) {
-            smallHitFracDt.reset();
-            smallHitFracDtPhased.reset();
-            smallHitFracDtReadout.reset();
+            smallHitFracVsTrigDt.reset();
+            smallHitFracVsSyncDt.reset();
+            smallHitFracVsReadoutDt.reset();
             for (int i = 0; i < trigDt.axis().bins(); i++) {
-                smallHitFracDt.fill(trigDt.axis().binCenter(i), trigDtWithSmallHits.binHeight(i) / trigDt.binHeight(i));
-                smallHitFracDtPhased.fill(trigDt.axis().binCenter(i), trigDtWithSmallHitsPhased.binHeight(i) / trigDtPhased.binHeight(i));
-                smallHitFracDtReadout.fill(trigDt.axis().binCenter(i), trigDtWithSmallHitsReadout.binHeight(i) / trigDtReadout.binHeight(i));
+                smallHitFracVsTrigDt.fill(trigDt.axis().binCenter(i), trigDtWithSmallHits.binHeight(i) / trigDt.binHeight(i));
+                smallHitFracVsSyncDt.fill(trigDt.axis().binCenter(i), syncDtWithSmallHits.binHeight(i) / syncDt.binHeight(i));
+                smallHitFracVsReadoutDt.fill(trigDt.axis().binCenter(i), readoutDtWithSmallHits.binHeight(i) / readoutDt.binHeight(i));
             }
         }
     }
