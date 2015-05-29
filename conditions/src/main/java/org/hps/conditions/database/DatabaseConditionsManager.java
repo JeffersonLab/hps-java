c2b0f6830aa39a47ade3e94d2fbeef52507bd36e
Jeremy McCormick <jermccormick@gmail.com>
2015-05-29 21:23:15 +0000

    Remove unused import.

--- conditions/src/main/java/org/hps/conditions/database/DatabaseConditionsManager.java
+++ conditions/src/main/java/org/hps/conditions/database/DatabaseConditionsManager.java
@@ -12,7 +12,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map.Entry;
