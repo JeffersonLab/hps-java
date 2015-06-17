93071344d6bf85955bd0aa5b9d0b0145bf3aa2aa
Sho Uemura <supersho@gmail.com>
2015-06-17 22:57:55 +0000

    add converter

---
+++ ecal-recon/src/main/java/org/hps/recon/ecal/triggerbank/HeadBankData.java
@@ -0,0 +1,79 @@
+package org.hps.recon.ecal.triggerbank;
+
+import java.util.Date;
+import org.lcsim.event.GenericObject;
+
+/**
+ * 
+ * @author Sho Uemura <meeg@slac.stanford.edu>
+ */
+public class HeadBankData extends AbstractIntData {
+
+    public static final int BANK_TAG = 0xe10f;
+    public static final int BANK_SIZE = 5;
+
+    public static final int VERSION_NUM = 0;
+    public static final int RUN_NUM = 1;
+    public static final int EVENT_NUM = 2;
+    public static final int UNIX_TIME = 3;
+    public static final int EVENT_TYPE = 4;
+
+    public HeadBankData(int[] bank) {
+        super(bank);
+        decodeData();
+    }
+
+    public HeadBankData(GenericObject data) {
+        super(data, BANK_TAG);
+        decodeData();
+    }
+
+    public int getRunNum() {
+        return bank[RUN_NUM];
+    }
+
+    public int getEventNum() {
+        return bank[EVENT_NUM];
+    }
+
+    public int getUnixTime() {
+        return bank[UNIX_TIME];
+    }
+
+    public Date getDate() {
+        return getDate(this);
+    }
+
+    public static int getRunNum(GenericObject object) {
+        return AbstractIntData.getBankInt(object, RUN_NUM);
+    }
+
+    public static int getEventNum(GenericObject object) {
+        return AbstractIntData.getBankInt(object, EVENT_NUM);
+    }
+
+    public static int getUnixTime(GenericObject object) {
+        return AbstractIntData.getBankInt(object, UNIX_TIME);
+    }
+
+    public static Date getDate(GenericObject object) {
+        long unixTime = getUnixTime(object);
+        if (unixTime == 0) {
+            return null;
+        } else {
+            return new Date(unixTime * 1000);
+        }
+    }
+
+    @Override
+    public int getTag() {
+        return BANK_TAG;
+    }
+
+    @Override
+    protected final void decodeData() { //doesn't actually do anything since there is no decoding done on the ints
+        if (this.bank.length != BANK_SIZE) {
+            throw new RuntimeException("Invalid Data Length:  " + bank.length);
+        }
+    }
+}
