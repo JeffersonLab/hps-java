3df5ab0c0b1823bc158bf5da378cd26651bd2fbd
Norman Graf <ngraf@slac.stanford.edu>
2015-07-13 16:25:55 +0000

    Added ability to select on number of hits in event

--- analysis/src/main/java/org/hps/analysis/examples/StripEventDriver.java
+++ analysis/src/main/java/org/hps/analysis/examples/StripEventDriver.java
@@ -3,6 +3,7 @@ package org.hps.analysis.examples;
 import java.util.List;
 import org.lcsim.event.EventHeader;
 import org.lcsim.event.Track;
+import org.lcsim.event.TrackerHit;
 import org.lcsim.event.Vertex;
 import org.lcsim.util.Driver;
 
@@ -19,6 +20,8 @@ public class StripEventDriver extends Driver
     private int _minNumberOfHitsOnTrack = 0;
     private int _numberOfEventsWritten = 0;
     private int _minNumberOfUnconstrainedV0Vertices = 0;
+    private int _minNumberOfStripHits = 0;
+    private int _maxNumberOfStripHits = Integer.MAX_VALUE;
 
     @Override
     protected void process(EventHeader event)
@@ -43,6 +46,15 @@ public class StripEventDriver extends Driver
         if (event.hasCollection(Vertex.class, "UnconstrainedV0Vertices")) {
             int nVertices = event.get(Vertex.class, "UnconstrainedV0Vertices").size();
             if (nVertices < _minNumberOfUnconstrainedV0Vertices) {
+                skipEvent = true;
+            }
+        }
+        if(event.hasCollection(TrackerHit.class, "StripClusterer_SiTrackerHitStrip1D")) {
+            int nHits = event.get(TrackerHit.class, "StripClusterer_SiTrackerHitStrip1D").size();
+            if (nHits < _minNumberOfStripHits) {
+                skipEvent = true;
+            }
+            if (nHits > _maxNumberOfStripHits) {
                 skipEvent = true;
             }
         }
@@ -72,6 +84,16 @@ public class StripEventDriver extends Driver
     public void setMinNumberOfUnconstrainedV0Vertices(int nVertices)
     {
         _minNumberOfUnconstrainedV0Vertices = nVertices;
+    }
+    
+    public void setMinNumberOfStripHits(int n)
+    {
+        _minNumberOfStripHits = n;
+    }
+    
+    public void setMaxNumberOfStripHits(int n)
+    {
+        _maxNumberOfStripHits = n;
     }
 
 }
