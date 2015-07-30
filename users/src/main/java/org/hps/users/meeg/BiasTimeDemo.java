dae62bec84a2575b305936246695f761ad870991
Sho Uemura <supersho@gmail.com>
2015-07-30 23:48:41 +0000

    example for testing timestamps

---
+++ users/src/main/java/org/hps/users/meeg/BiasTimeDemo.java
@@ -0,0 +1,25 @@
+package org.hps.users.meeg;
+
+import org.hps.conditions.database.DatabaseConditionsManager;
+import org.hps.conditions.svt.SvtBiasConstant;
+import org.hps.conditions.svt.SvtBiasConstant.SvtBiasConstantCollection;
+import org.lcsim.geometry.Detector;
+import org.lcsim.util.Driver;
+
+/**
+ * @author Per Hansson Adrian <phansson@slac.stanford.edu>
+ *
+ */
+public class BiasTimeDemo extends Driver {
+
+    SvtBiasConstantCollection svtBiasConstants = null;
+
+    @Override
+    protected void detectorChanged(Detector detector) {
+        svtBiasConstants = DatabaseConditionsManager.getInstance().getCachedConditions(SvtBiasConstant.SvtBiasConstantCollection.class, "svt_bias_constants").getCachedData();
+        System.out.println("there are " + svtBiasConstants.size() + " constants to search");
+        for (SvtBiasConstant constant : svtBiasConstants) {
+            System.out.format("start %d (%s), end %d (%s), value %f\n", constant.getStart().getTime(), constant.getStart().toString(), constant.getEnd().getTime(), constant.getEnd().toString(), constant.getValue());
+        }
+    }
+}
