b207b988b0f522c166f254abedebc302bf6dc426
Jeremy McCormick <jermccormick@gmail.com>
2015-07-30 00:28:31 +0000

    Add DummyDriver that does nothing.

---
+++ integration-tests/src/main/java/org/hps/test/util/DummyDriver.java
@@ -0,0 +1,11 @@
+package org.hps.test.util;
+
+import org.lcsim.util.Driver;
+
+/**
+ * This Driver does nothing.
+ *
+ * @author Jeremy McCormick, SLAC
+ */
+public class DummyDriver extends Driver {
+}
