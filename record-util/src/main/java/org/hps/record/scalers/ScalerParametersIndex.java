6429c144e73dd9f225991f610cd43844e25e1ce2
Jeremy McCormick <jermccormick@gmail.com>
2015-07-30 00:54:07 +0000

    Add class for reading selected scaler data from event parameters.  HPSJAVA-562

---
+++ record-util/src/main/java/org/hps/record/scalers/ScalerParametersIndex.java
@@ -0,0 +1,14 @@
+package org.hps.record.scalers;
+
+/**
+ * Indices into the {@link ScalerParameters} from the event header.
+ * 
+ * @author Jeremy McCormick, SLAC
+ */
+public enum ScalerParametersIndex {    
+    FCUP_TDC_GATED,
+    FCUP_TDC_UNGATED,
+    BEAM_POS_X,
+    BEAM_POS_Y,
+    SVT_BIAS_VOLTAGE;     
+}
