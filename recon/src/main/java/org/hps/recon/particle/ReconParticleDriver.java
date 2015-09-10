6cb91115070887dcbabfcf0860d15e099c7396ce
Omar Moreno <email@omarmoreno.net>
2015-09-10 20:37:43 +0000

    This also needs to be commented out ...

--- recon/src/main/java/org/hps/recon/particle/ReconParticleDriver.java
+++ recon/src/main/java/org/hps/recon/particle/ReconParticleDriver.java
@@ -501,7 +501,7 @@ public abstract class ReconParticleDriver extends Driver {
 
     @Override
     protected void endOfData() { 
-        matcher.saveHistograms();
+        //matcher.saveHistograms();
     }
 
     // ==============================================================
