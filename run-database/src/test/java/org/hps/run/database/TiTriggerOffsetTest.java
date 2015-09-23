d8984c684e897250d589889c2f2ce212c3930597
Jeremy McCormick <jermccormick@gmail.com>
2015-09-23 15:21:00 +0000

    Add simple test that gets TI trigger offset from run database.

---
+++ run-database/src/test/java/org/hps/run/database/TiTriggerOffsetTest.java
@@ -0,0 +1,41 @@
+package org.hps.run.database;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.hps.record.triggerbank.TriggerConfig;
+
+/**
+ * Test of getting the TI trigger offset from the run database.
+ * 
+ * @author Jeremy McCormick, SLAC
+ */
+public class TiTriggerOffsetTest extends TestCase {
+    
+    /**
+     * Get the TI trigger offset for all runs.
+     */
+    public void testAllRuns() {
+        RunManager runManager = new RunManager();
+        List<Integer> runs = runManager.getRuns();
+        TriggerConfigDao triggerConfigDao = runManager.createDaoFactory().createTriggerConfigDao();
+        for (Integer run : runs) {
+            TriggerConfig triggerConfig = triggerConfigDao.getTriggerConfig(run);
+            Long tiTimeOffset = triggerConfig.getTiTimeOffset();
+            System.out.println("run " + run + " tiTriggerOffset = " + tiTimeOffset);
+        }
+    }
+    
+    /**
+     * Get the TI trigger offset for a single run.
+     */
+    public void testSingleRun() {
+        int run = 5772;
+        RunManager runManager = new RunManager();
+        TriggerConfigDao triggerConfigDao = runManager.createDaoFactory().createTriggerConfigDao();
+        TriggerConfig triggerConfig = triggerConfigDao.getTriggerConfig(run);
+        Long tiTimeOffset = triggerConfig.getTiTimeOffset();
+        System.out.println("run " + run + " tiTriggerOffset = " + tiTimeOffset);
+    }
+}
