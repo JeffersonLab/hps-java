2e8e3f7650a44435d6fe0a4bca41871b224c9693
Jeremy McCormick <jermccormick@gmail.com>
2015-09-29 20:11:10 +0000

    Add method to factory for creating client with non default configuration.

--- datacat-client/src/main/java/org/hps/datacat/client/DatacatClientFactory.java
+++ datacat-client/src/main/java/org/hps/datacat/client/DatacatClientFactory.java
@@ -1,5 +1,6 @@
 package org.hps.datacat.client;
 
+
 /**
  * Factory class for providing user access to interfaces that have protected implementations.
  * 
@@ -8,11 +9,23 @@ package org.hps.datacat.client;
 public final class DatacatClientFactory {
         
     /**
-     * Create a datacat client.
+     * Create a data catalog client with default parameters.
      * 
      * @return the datacat client
      */
     public DatacatClient createClient() {
         return new DatacatClientImpl();
+    }
+    
+    /**
+     * Create a data catalog client with specified parameters.
+     * 
+     * @param url the URL of the datacat server
+     * @param site the site (e.g. <code>SLAC</code>, <code>JLAB</code> etc.)
+     * @param rootDir the root directory in the data catalog
+     * @return the data catalog client
+     */
+    public DatacatClient createClient(String url, DatasetSite site, String rootDir) {
+        return new DatacatClientImpl(url, site, rootDir);
     }
 }
