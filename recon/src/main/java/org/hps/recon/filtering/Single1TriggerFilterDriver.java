c28386f7cf1aa1e3afb858b6e3d86c31c3825eef
Nathan Baltzell <baltzell@gmx.com>
2015-10-03 02:01:06 +0000

    adding filters for all non-Pair1 triggers that also keep all EPICS and Scaler events

---
+++ recon/src/main/java/org/hps/recon/filtering/Single1TriggerFilterDriver.java
@@ -0,0 +1,43 @@
+package org.hps.recon.filtering;
+
+import org.lcsim.event.EventHeader;
+import org.lcsim.event.GenericObject;
+import org.lcsim.util.Driver;
+import org.hps.record.epics.EpicsData;
+import org.hps.record.scalers.ScalerData;
+import org.hps.record.triggerbank.AbstractIntData;
+import org.hps.record.triggerbank.TIData;
+/**
+ * Keep single-1 triggered events.
+ * Also keep EPICS events, and Scaler events.
+ * Drop all other events.
+ * 
+ * @author baltzell
+ *
+ */
+public class Single1TriggerFilterDriver extends Driver
+{
+  public void process(EventHeader event) {
+
+    // 1. keep all events with EPICS data (could also use event tag = 31):
+    if (EpicsData.read(event) != null) return;
+
+    // 2. keep all events with Scaler data:
+    if (ScalerData.read(event) != null) return;
+
+    // 3. drop event if it doesn't have a TriggerBank
+    if (!event.hasCollection(GenericObject.class,"TriggerBank"))
+      throw new Driver.NextEventException();
+  
+    // 4. keep event if it was from a Single-1 trigger:
+    for (GenericObject gob : event.get(GenericObject.class,"TriggerBank"))
+    {
+      if (!(AbstractIntData.getTag(gob) == TIData.BANK_TAG)) continue;
+      TIData tid = new TIData(gob);
+      if (tid.isSingle1Trigger()) return;
+    }
+    
+    // 5. Else, drop event:
+    throw new Driver.NextEventException();
+  }
+}
