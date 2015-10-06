bc690aedc26d593a50a2b8cc5b3e6df0653da0ec
Jeremy McCormick <jermccormick@gmail.com>
2015-10-06 18:30:01 +0000

    Give JobManager its own package for dependency reasons.

--- job/src/main/java/org/hps/job/JobManager.java
+++ job/src/main/java/org/hps/job/JobManager.java
@@ -1,14 +1,17 @@
 package org.hps.job;
 
+import java.io.InputStream;
+
 import org.hps.conditions.ConditionsDriver;
 import org.hps.conditions.database.DatabaseConditionsManager;
+import org.hps.detector.svt.SvtDetectorSetup;
 import org.lcsim.job.JobControlManager;
 import org.lcsim.util.Driver;
 
 /**
  * Extension of standard LCSim job manager which does some HPS-specific management of the conditions system.
  *
- * @author <a href="mailto:jeremym@slac.stanford.edu">Jeremy McCormick</a>
+ * @author Jeremy McCormick, SLAC
  */
 public class JobManager extends JobControlManager {
 
@@ -35,11 +38,21 @@ public class JobManager extends JobControlManager {
         } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
             // Just don't print info if not accessible (running in test case?).
         }
-
-        // Always want to reset the conditions system before starting the job.
-        DatabaseConditionsManager.resetInstance();
     }
 
+    /**
+     * Override setup so the conditions system can be reset.
+     * 
+     * @param is the input stream containing config information
+     */
+    public void setup(InputStream is) {
+        
+        // Always want to reset the conditions system before initialization.
+        DatabaseConditionsManager.resetInstance();
+
+        super.setup(is);
+    }
+    
     /**
      * Override the parent classes method that runs the job in order to perform conditions system initialization.
      *
@@ -47,8 +60,11 @@ public class JobManager extends JobControlManager {
      */
     @Override
     public final boolean run() {
+        
+        // Add class that will setup SVT detector with conditions data (this is awkward but has to be done someplace).
+        DatabaseConditionsManager.getInstance().addConditionsListener(new SvtDetectorSetup());
 
-        // Setup the conditions if there is a ConditionsDriver present.
+        // Setup the conditions system if there is a ConditionsDriver present.
         this.setupConditions();
 
         // Run the job.
