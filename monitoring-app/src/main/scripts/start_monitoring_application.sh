b41e5318e1381fd89f6a33df9e7cc9971579c3ce
Jeremy McCormick <jermccormick@gmail.com>
2015-10-22 22:37:31 +0000

    Remove scripts that will be put into distribution using appassembler instead.

--- monitoring-app/src/main/scripts/start_monitoring_application.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# The bin jar to run.
-jarfile=${project.build.directory}/${project.artifactId}-${project.version}-bin.jar
-
-# Start the monitoring application with supplied arguments.
-prod="java -Xmx1024m -jar $jarfile $@"
-echo $prod
-exec $prod
