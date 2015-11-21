a3c91f9e23cdf0b7fe2ba49036d06dfda3f50393
Holly Szumila <hszumila@jlab.org>
2015-11-21 05:13:08 +0000

    changed parameter name from pedestal to intercept

--- users/src/main/java/org/hps/users/baltzell/RfFitFunction.java
+++ users/src/main/java/org/hps/users/baltzell/RfFitFunction.java
@@ -6,7 +6,7 @@ import hep.aida.ref.function.AbstractIFunction;
  * Function for fitting the leading edge of the RF waveform.
  */
 public class RfFitFunction extends AbstractIFunction {
-	protected double pedestal=0;
+	protected double intercept=0;
 	protected double time=0;
 	protected double slope=0;
 	public RfFitFunction() {
@@ -15,21 +15,21 @@ public class RfFitFunction extends AbstractIFunction {
 	public RfFitFunction(String title) {
 		super();
 		this.variableNames=new String[]{"time"};
-		this.parameterNames=new String[]{"pedestal","time","slope"};
+		this.parameterNames=new String[]{"intercept","time","slope"};
 		init(title);
 	}
 	public double value(double [] v) {
-		return  pedestal + (v[0]-time)*slope;
+		return  intercept + (v[0]-time)*slope;
 	}
 	public void setParameters(double[] pars) throws IllegalArgumentException {
 		super.setParameters(pars);
-		pedestal=pars[0];
+		intercept=pars[0];
 		time=pars[1];
 		slope=pars[2];
 	}
 	public void setParameter(String key,double value) throws IllegalArgumentException{
 		super.setParameter(key,value);
-		if      (key.equals("pedestal")) pedestal=value;
+		if      (key.equals("intercept")) intercept=value;
 		else if (key.equals("time"))     time=value;
 		else if (key.equals("slope"))    slope=value;
 	}
