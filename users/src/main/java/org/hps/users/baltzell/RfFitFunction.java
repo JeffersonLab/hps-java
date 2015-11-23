e55c4ddae18051f81e9884af8541e70569b018ea
Holly Szumila <hszumila@jlab.org>
2015-11-23 15:17:40 +0000

    cleaned up function code comments

--- users/src/main/java/org/hps/users/baltzell/RfFitFunction.java
+++ users/src/main/java/org/hps/users/baltzell/RfFitFunction.java
@@ -4,10 +4,10 @@ import hep.aida.ref.function.AbstractIFunction;
 
 /*
  * Function for fitting the leading edge of the RF waveform.
+ * Straight line fit
  */
 public class RfFitFunction extends AbstractIFunction {
 	protected double intercept=0;
-	//protected double time=0;
 	protected double slope=0;
 	public RfFitFunction() {
 		this("");
@@ -15,25 +15,21 @@ public class RfFitFunction extends AbstractIFunction {
 	public RfFitFunction(String title) {
 		super();
 		this.variableNames=new String[]{"time"};
-		//this.parameterNames=new String[]{"intercept","time","slope"};
 		this.parameterNames=new String[]{"intercept","slope"};
 
 		init(title);
 	}
 	public double value(double [] v) {
-		//return  intercept + (v[0]-time)*slope;
 		return  intercept + (v[0])*slope;
 	}
 	public void setParameters(double[] pars) throws IllegalArgumentException {
 		super.setParameters(pars);
 		intercept=pars[0];
-		//time=pars[1];
 		slope=pars[1];
 	}
 	public void setParameter(String key,double value) throws IllegalArgumentException{
 		super.setParameter(key,value);
 		if      (key.equals("intercept")) intercept=value;
-		//else if (key.equals("time"))     time=value;
 		else if (key.equals("slope"))    slope=value;
 	}
 }
