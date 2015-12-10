d17fce2341c216a432ed55e58ff1c0610d561e28
Per Hansson <per.hansson@gmail.com>
2015-12-10 01:00:23 +0000

    use a relevant run

--- detector-model/src/test/java/org/hps/detector/svt/SvtDetectorSetupTest.java
+++ detector-model/src/test/java/org/hps/detector/svt/SvtDetectorSetupTest.java
@@ -64,7 +64,7 @@ public final class SvtDetectorSetupTest extends TestCase {
         final DatabaseConditionsManager conditionsManager = DatabaseConditionsManager.getInstance();
         conditionsManager.addConditionsListener(new SvtDetectorSetup());
         //conditionsManager.setDetector("HPS-Proposal2014-v7-2pt2", 0);
-        conditionsManager.setDetector("HPS-EngRun2015-Nominal-v3", 0);
+        conditionsManager.setDetector("HPS-EngRun2015-Nominal-v3", 5772);
 
         // Get the detector.
         final Detector detector = conditionsManager.getCachedConditions(Detector.class, "compact.xml").getCachedData();
