a6b1a4e936671cab515967b1b1041007f65d427f
Matthew Solt <mrsolt@stanford.edu>
2016-12-08 00:48:24 +0000

    Adding a file for the layer 0 detector.

---
+++ detector-model/src/main/java/org/lcsim/geometry/subdetector/HPSTracker2017.java
@@ -0,0 +1,32 @@
+package org.lcsim.geometry.subdetector;
+
+import hep.graphics.heprep.HepRep;
+import hep.graphics.heprep.HepRepFactory;
+
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.lcsim.detector.converter.heprep.DetectorElementToHepRepConverter;
+
+    public class HPSTracker2017 extends AbstractTracker {
+        
+        public HPSTracker2017(Element node) throws JDOMException 
+        {
+            super(node);
+        }
+
+        public void appendHepRep(HepRepFactory factory, HepRep heprep) 
+        {
+            DetectorElementToHepRepConverter.convert(getDetectorElement(), factory, heprep, -1, false, getVisAttributes().getColor());
+        }
+        
+        public boolean isEndcap() {
+            return false;
+        }
+        
+        public boolean isBarrel() {
+            return true;
+        }
+
+    }
+    
+
