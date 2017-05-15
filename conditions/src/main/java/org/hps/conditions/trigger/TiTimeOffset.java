a13c53a167cb66ee77fd8a473a34944babac847e
JeremyMcCormick <jermccormick@gmail.com>
2017-05-15 13:26:33 -0700

    add comment to class

--- conditions/src/main/java/org/hps/conditions/trigger/TiTimeOffset.java
+++ conditions/src/main/java/org/hps/conditions/trigger/TiTimeOffset.java
@@ -5,6 +5,14 @@ import org.hps.conditions.api.BaseConditionsObjectCollection;
 import org.hps.conditions.database.Converter;
 import org.hps.conditions.database.Table;
 
+/**
+ * <p>
+ * Represents the per-run trigger time offset in nanoseconds.
+ * </p>
+ * <p>
+ * A single instance of this class is returned for an entire run.
+ * </p>
+ */
 @Table(names = {"ti_time_offsets"})
 @Converter(converter = TiTimeOffsetConverter.class)
 public class TiTimeOffset extends BaseConditionsObject {
