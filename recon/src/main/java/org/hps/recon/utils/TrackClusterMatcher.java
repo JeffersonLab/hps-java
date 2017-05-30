980a5775b5544927c293a14c21602b29c9fca594
Sebouh Paul <sebouh.paul@gmail.com>
2017-05-30 02:44:57 -0400

    improved method for finding edges using database.

--- recon/src/main/java/org/hps/recon/utils/TrackClusterMatcher.java
+++ recon/src/main/java/org/hps/recon/utils/TrackClusterMatcher.java
@@ -418,7 +418,7 @@ public class TrackClusterMatcher {
 
       //if the track's extrapolated position is within 1/2 a crystal width of the edge of 
         // the ecal edge, then move it to be 1/2 a crystal from the edge in y.  
-        tPos= snapToEdge(tPos, cluster);
+        tPos= snapToEdge(tPos);
         
         // calculate nSigma between track and cluster:
         final double nSigmaX = (cPos.x() - tPos.x() - aDxMean) / aDxSigm;
@@ -427,51 +427,127 @@ public class TrackClusterMatcher {
         //return Math.sqrt( 1 / ( 1/nSigmaX/nSigmaX + 1/nSigmaY/nSigmaY ) );
     }
 
-    private Map<Integer, Integer> innermostRow;
-    int getInnerEdgeCrystalRow(int column){
-        if(innermostRow == null){ //load the map with stuff from database
-            innermostRow = new HashMap();
-            for(EcalChannel channel : channels){
-                if(channel.getY()< 0)
-                    continue;
-                if(!innermostRow.containsKey(channel.getX()))
-                    innermostRow.put(channel.getX(),channel.getY());
-                else if(innermostRow.get(channel.getX())> channel.getY())
-                    innermostRow.put(channel.getX(), channel.getY());
-            }
-        }
-        return innermostRow.get(column);
-    }
-
-    private Hep3Vector snapToEdge(Hep3Vector tPos, Cluster cluster) {
-        double ty = tPos.y();
-        double tx = tPos.x();
-
-        double closestDist = 9999;
-        if(positions == null || channels == null)
-            return tPos;
-        Double snapToY = null;
+    //parameters for the snap-to-edge algorithm
+    
+    private double x_outer_bot[];
+    private double y_outer_bot[];
+    private double x_inner_bot[];
+    private double y_inner_bot[];
+    private double x_outer_top[];
+    private double y_outer_top[];
+    private double x_inner_top[];
+    private double y_inner_top[];
+    
+    private void loadEdges(){
+        int nCol = 46;
+        x_outer_bot= new double[nCol];
+        x_inner_bot= new double[nCol];
+        y_outer_bot= new double[nCol];
+        y_inner_bot= new double[nCol];
+        x_outer_top= new double[nCol];
+        x_inner_top= new double[nCol];
+        y_outer_top= new double[nCol];
+        y_inner_top= new double[nCol];
         for(EcalCrystalPosition pos : positions){
             EcalChannel chan = channels.findChannel(pos.getChannelId());
             int ix = chan.getX();
             int iy = chan.getY();
-            //must be on same side
-            if(ty*iy < 0)
-                continue;
-            //must be an edge crystal
-            if(!( Math.abs(iy) == 5 || Math.abs(iy) == getInnerEdgeCrystalRow(ix)))
-                continue;
+            
             double cx = pos.getFrontX();
             double cy = pos.getFrontY();
+            
+            
+            //there is a bug in the numbering system.  The next 4 lines compensate for that
+            if(Math.abs(iy) ==1 && ix>=1 && ix<=9)
+                ix-=10;
+            else if(Math.abs(iy) ==1 && ( ix==10 || (ix>=-2 && ix<=-1)))
+                ix-=9;
+                
+            
+            
+            if(ix<0) ix += 23; else if(ix>0) ix+= 22;
+            else continue;
+            
+            
+            
+            ix = 45 - ix;
+            if(cy < 0 && iy < 0 && iy >= -5) {
+                if(y_outer_bot[ix] == 0 || y_outer_bot[ix]>cy){
+                    y_outer_bot[ix] = cy;
+                    x_outer_bot[ix] = cx;        
+                }
+                if(y_inner_bot[ix] == 0 || y_inner_bot[ix]<cy){
+                    y_inner_bot[ix] = cy;
+                    x_inner_bot[ix] = cx;        
+                }
+            }
+            if(cy>0 && iy > 0 && iy <= 5) {
+                if(y_outer_top[ix] == 0 || y_outer_top[ix]<cy){
+                    y_outer_top[ix] = cy;
+                    x_outer_top[ix] = cx;        
+                }
+                if(y_inner_top[ix] == 0 || y_inner_top[ix]>cy){
+                    y_inner_top[ix] = cy;
+                    x_inner_top[ix] = cx;        
+                }
+            }
+        }
+        
+        /*System.out.println(Arrays.toString(x_inner_top));
+        System.out.println(Arrays.toString(y_inner_top));
+        System.out.println(Arrays.toString(x_inner_bot));
+        System.out.println(Arrays.toString(y_inner_bot));*/
+    }
 
-            if(((iy == 5 || iy == -1 || iy == -2) && cy > ty) 
-                    || (((iy == -5 || iy == 1 || iy == 2) && cy < ty)))
-                continue;
-            double dist = Math.hypot(tx-cx, ty-cy);
+    public Hep3Vector snapToEdge(Hep3Vector tPos) {
+        double ty = tPos.y();
+        double tx = tPos.x();
 
-            if(dist>closestDist) continue;
-            closestDist = dist;
-            snapToY = cy;
+        if (x_outer_bot == null)
+            loadEdges();
+        Double snapToY = null;
+        
+        if(ty > 0)
+        {
+            double outer_edge = y_outer_top[0];
+            double inner_edge = y_inner_top[0];
+            for(int i = 0; i< x_outer_bot.length-1; i++){
+                if(tx > x_outer_top[i] && tx < x_outer_top[i+1])
+                    outer_edge = y_outer_top[i] + (tx-x_outer_top[i])
+                                    *(y_outer_top[i+1]-y_outer_top[i])
+                                    /(x_outer_top[i+1]-x_outer_top[i]);
+
+                if(tx > x_inner_top[i] && tx < x_inner_top[i+1])
+                    inner_edge = y_inner_top[i] + (tx-x_inner_top[i])
+                                    *(y_inner_top[i+1]-y_inner_top[i])
+                                    /(x_inner_top[i+1]-x_inner_top[i]);
+                
+            }
+            if(ty>outer_edge)
+                snapToY = outer_edge;
+            if(ty<inner_edge)
+                snapToY = inner_edge;
+        }
+        if(ty < 0)
+        {
+            double outer_edge = y_outer_bot[0];
+            double inner_edge = y_inner_bot[0];
+            for(int i = 0; i< x_outer_bot.length-1; i++){
+                if(tx > x_outer_bot[i] && tx < x_outer_bot[i+1])
+                    outer_edge = y_outer_bot[i] + (tx-x_outer_bot[i])
+                                    *(y_outer_bot[i+1]-y_outer_bot[i])
+                                    /(x_outer_bot[i+1]-x_outer_bot[i]);
+
+                if(tx > x_inner_bot[i] && tx < x_inner_bot[i+1])
+                    inner_edge = y_inner_bot[i] + (tx-x_inner_bot[i])
+                                    *(y_inner_bot[i+1]-y_inner_bot[i])
+                                    /(x_inner_bot[i+1]-x_inner_bot[i]);
+                
+            }
+            if(ty<outer_edge)
+                snapToY = outer_edge;
+            if(ty>inner_edge)
+                snapToY = inner_edge;
         }
         if(snapToY == null)
             return tPos;
