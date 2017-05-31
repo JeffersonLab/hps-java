6e1190411ef679820374156b4ecd3aeb3a40aef7
normangraf <Norman.Graf@slac.stanford.edu>
2017-05-31 15:29:31 -0700

    Separated time and memory report intervals

--- recon/src/main/java/org/hps/recon/utils/ReconDiagnosticsDriver.java
+++ recon/src/main/java/org/hps/recon/utils/ReconDiagnosticsDriver.java
@@ -19,7 +19,8 @@ public class ReconDiagnosticsDriver extends Driver {
 
     private int mb = 1024 * 1024;
 
-    private int _interval = 1;
+    private int _timeInterval = 1;
+    private int _memInterval = 1;
 
     private int _nEvents;
 
@@ -35,14 +36,15 @@ public class ReconDiagnosticsDriver extends Driver {
     @Override
     protected void process(EventHeader event) {
         _nEvents++;
-        if ((_nEvents % _interval) == 0) {
+        if ((_nEvents % _timeInterval) == 0) {
             long deltaTime = System.nanoTime() - _startTime;
             List<RawTrackerHit> rawHits = event.get(RawTrackerHit.class, "SVTRawTrackerHits");
             double svtsize = rawHits.size();
             aida.cloud2D("elapsed time vs SVT size").fill(svtsize, deltaTime);
             _startTime = System.nanoTime();
             System.out.println(event.getRunNumber() + " " + event.getEventNumber() + " " + svtsize + " " + deltaTime);
-
+        }
+        if ((_nEvents % _memInterval) == 0) {
             System.out.println("##### Heap utilization statistics [MB] #####");
 
             //Print used memory
@@ -60,9 +62,12 @@ public class ReconDiagnosticsDriver extends Driver {
             System.out.println("Max Memory:" + runtime.maxMemory() / mb);
         }
     }
-    
-    public void setInterval(int i)
-    {
-        _interval = i;
+
+    public void setTimeInterval(int i) {
+        _timeInterval = i;
+    }
+
+    public void setMemoryInterval(int i) {
+        _memInterval = i;
     }
 }
