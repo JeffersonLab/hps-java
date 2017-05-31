7be4d9aeefd53c7cf61e8c1d6e0c0e36ebfbc0e9
normangraf <Norman.Graf@slac.stanford.edu>
2017-05-31 14:12:09 -0700

    Memory diagnostics
    Added some checks on the amount of memory being used.

--- recon/src/main/java/org/hps/recon/utils/ReconDiagnosticsDriver.java
+++ recon/src/main/java/org/hps/recon/utils/ReconDiagnosticsDriver.java
@@ -17,18 +17,52 @@ public class ReconDiagnosticsDriver extends Driver {
 
     private AIDA aida = AIDA.defaultInstance();
 
+    private int mb = 1024 * 1024;
+
+    private int _interval = 1;
+
+    private int _nEvents;
+
+    //Getting the runtime reference from system
+    Runtime runtime;
+
     @Override
     protected void detectorChanged(Detector detector) {
         _startTime = System.nanoTime();
+        runtime = Runtime.getRuntime();
     }
 
     @Override
     protected void process(EventHeader event) {
-        long deltaTime = System.nanoTime() - _startTime;
-        List<RawTrackerHit> rawHits = event.get(RawTrackerHit.class, "SVTRawTrackerHits");
-        double svtsize = rawHits.size();
-        aida.cloud2D("elapsed time vs SVT size").fill(svtsize, deltaTime);
-        _startTime = System.nanoTime();
-        System.out.println(event.getRunNumber()+" "+event.getEventNumber()+" "+svtsize+" "+deltaTime);
+        _nEvents++;
+        if ((_nEvents % _interval) == 0) {
+            long deltaTime = System.nanoTime() - _startTime;
+            List<RawTrackerHit> rawHits = event.get(RawTrackerHit.class, "SVTRawTrackerHits");
+            double svtsize = rawHits.size();
+            aida.cloud2D("elapsed time vs SVT size").fill(svtsize, deltaTime);
+            _startTime = System.nanoTime();
+            System.out.println(event.getRunNumber() + " " + event.getEventNumber() + " " + svtsize + " " + deltaTime);
+
+            System.out.println("##### Heap utilization statistics [MB] #####");
+
+            //Print used memory
+            System.out.println("Used Memory:"
+                    + (runtime.totalMemory() - runtime.freeMemory()) / mb);
+
+            //Print free memory
+            System.out.println("Free Memory:"
+                    + runtime.freeMemory() / mb);
+
+            //Print total available memory
+            System.out.println("Total Memory:" + runtime.totalMemory() / mb);
+
+            //Print Maximum available memory
+            System.out.println("Max Memory:" + runtime.maxMemory() / mb);
+        }
+    }
+    
+    public void setInterval(int i)
+    {
+        _interval = i;
     }
 }
