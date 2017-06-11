33d4484437ec7a6e44653cabd8bae9a16f532607
Sebouh Paul <sebouh.paul@gmail.com>
2017-06-11 16:52:39 -0400

    cached commonly used combinations of parameters in 4 pole function.

--- tracking/src/main/java/org/hps/recon/tracking/PulseShape.java
+++ tracking/src/main/java/org/hps/recon/tracking/PulseShape.java
@@ -48,12 +48,18 @@ public abstract class PulseShape {
         private double tp2;
         private double peak_t, peak_amp;
 
+        //combinations of tp and tp2:
+        private double A, B;
+        
         @Override
         public void setParameters(int channel, HpsSiSensor sensor) {
             tp = sensor.getShapeFitParameters(channel)[HpsSiSensor.TP_INDEX];
             tp2 = sensor.getShapeFitParameters(channel)[HpsSiSensor.TP_INDEX + 1];
             peak_t = 3.0 * Math.pow(tp * Math.pow(tp2, 3), 0.25); //approximate solution to exp(x)=1+x+x^2*tp/(2*tp2), where x=(1/tp2-1/tp)*t
             peak_amp = getAmplitudeIntegralNorm(peak_t);
+            
+            A = (Math.pow(tp, 2) / Math.pow(tp - tp2, 3));
+            B = (tp - tp2) / (tp * tp2);
         }
 
         @Override
@@ -62,9 +68,14 @@ public abstract class PulseShape {
                 return 0;
             }
             //===> return (time / channelConstants.getTp()) * Math.exp(1 - time / channelConstants.getTp());
-            return (Math.pow(tp, 2) / Math.pow(tp - tp2, 3))
-                    * (Math.exp(-time / tp)
-                    - Math.exp(-time / tp2) * (1 + time * (tp - tp2) / (tp * tp2) + 0.5 * Math.pow(time * (tp - tp2) / (tp * tp2), 2)));
+            //return (Math.pow(tp, 2) / Math.pow(tp - tp2, 3))
+            //        * (Math.exp(-time / tp)
+            //        - Math.exp(-time / tp2) * (1 + time * (tp - tp2) / (tp * tp2) + 0.5 * Math.pow(time * (tp - tp2) / (tp * tp2), 2)));
+            
+            return A * (Math.exp(-time / tp)
+                            - Math.exp(-time / tp2) * (1 + time * B + 0.5 * time * time * B*B));
+                    
+            
 //            return (time / tp) * Math.exp(1 - time / tp);
         }
 
