53f2bdc804481fac3d5c7793198a0470182f76c4
Miriam Diamond <Miriam Diamond>
2017-07-07 12:53:29 -0700

    put mergeTracks back in

--- tracking/src/main/java/org/hps/recon/tracking/gbl/GBLRefitterDriver.java
+++ tracking/src/main/java/org/hps/recon/tracking/gbl/GBLRefitterDriver.java
@@ -2,8 +2,11 @@ package org.hps.recon.tracking.gbl;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
 import org.apache.commons.math3.util.Pair;
 import org.hps.recon.tracking.MaterialSupervisor;
 import org.hps.recon.tracking.MultipleScattering;
@@ -19,6 +22,7 @@ import org.lcsim.event.LCRelation;
 import org.lcsim.event.RawTrackerHit;
 import org.lcsim.event.RelationalTable;
 import org.lcsim.event.Track;
+import org.lcsim.event.TrackerHit;
 import org.lcsim.event.base.BaseLCRelation;
 import org.lcsim.geometry.Detector;
 import org.lcsim.lcio.LCIOConstants;
@@ -89,6 +93,60 @@ public class GBLRefitterDriver extends Driver {
 
             kinkDataCollection.add(newTrack.getSecond());
             kinkDataRelations.add(new BaseLCRelation(newTrack.getSecond(), newTrack.getFirst()));
+        }
+
+        if (mergeTracks) {
+            List<Track> mergedTracks = new ArrayList<Track>();
+
+            for (Track track : refittedTracks) {
+                List<TrackerHit> trackHth = track.getTrackerHits();
+                otherTrackLoop: for (Track otherTrack : refittedTracks) {
+                    if (track == otherTrack) {
+                        continue;
+                    }
+
+                    Set<TrackerHit> allHth = new HashSet<TrackerHit>(otherTrack.getTrackerHits());
+                    allHth.addAll(trackHth);
+                    //                if (allHth.size() == trackHth.size()) {
+                    //                    continue;
+                    //                }
+
+                    boolean[] hasHit = new boolean[6];
+
+                    for (TrackerHit hit : allHth) {
+                        int layer = (TrackUtils.getLayer(hit) - 1) / 2;
+                        if (hasHit[layer]) {
+                            continue otherTrackLoop;
+                        }
+                        hasHit[layer] = true;
+                    }
+                    for (Track mergedTrack : mergedTracks) {
+                        if (mergedTrack.getTrackerHits().containsAll(allHth)) {
+                            continue otherTrackLoop;
+                        }
+                    }
+
+                    Pair<Track, GBLKinkData> mergedTrack = MakeGblTracks.refitTrack(TrackUtils.getHTF(track), TrackUtils.getStripHits(track, hitToStrips, hitToRotated), allHth, 5, track.getType(), _scattering, bfield);
+                    mergedTracks.add(mergedTrack.getFirst());
+                    //                    System.out.format("%f %f %f\n", fit.get_chi2(), inputToRefitted.get(track).getChi2(), inputToRefitted.get(otherTrack).getChi2());
+                    //                mergedTrackToTrackList.put(mergedTrack, new ArrayList<Track>());
+                }
+            }
+
+            for (Track mergedTrack : mergedTracks) {
+                List<Track> subTracks = new ArrayList<Track>();
+                Set<TrackerHit> trackHth = new HashSet<TrackerHit>(mergedTrack.getTrackerHits());
+                for (Track track : refittedTracks) {
+                    if (trackHth.containsAll(track.getTrackerHits())) {
+                        subTracks.add(track);
+                    }
+                }
+                System.out.format("%f:\t", mergedTrack.getChi2());
+                for (Track subTrack : subTracks) {
+                    System.out.format("%f (%d)\t", subTrack.getChi2(), subTrack.getTrackerHits().size());
+                }
+                System.out.println();
+            }
         }
 
         // Put the tracks back into the event and exit
