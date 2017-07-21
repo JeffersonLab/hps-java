3cbaff00c1da824a5c4dd803003446e5e8f0ddc7
Miriam Diamond <mdiamond@rhel6-64i.slac.stanford.edu>
2017-07-21 14:52:05 -0700

    add and remove file to fix compilation problem

--- tracking/src/main/java/org/hps/recon/tracking/MergeTrackCollections.java
+++ tracking/src/main/java/org/hps/recon/tracking/MergeTrackCollections.java
@@ -38,18 +38,18 @@ public class MergeTrackCollections extends Driver {
     private AmbiguityResolver ambi;
     // private AcceptanceHelper acc;
 
-    private AIDA aida2 = AIDA.defaultInstance();
-    private IHistogram1D trackScoresPreAmbi = aida2.histogram1D("trackScoresPreAmbi", 200, -100, 100);
-    private IHistogram1D trackScoresPostAmbi = aida2.histogram1D("trackScoresPostAmbi", 200, -100, 100);
-    private IHistogram1D numDuplicateTracks = aida2.histogram1D("numDuplicateTracks", 10, 0, 10);
-    private IHistogram1D numPartialTracks = aida2.histogram1D("numPartialTracks", 10, 0, 10);
-    private IHistogram1D numSharedTracks = aida2.histogram1D("numSharedTracks", 10, 0, 10);
-    private IHistogram1D sharedHitsPreAmbi = aida2.histogram1D("sharedHitsPreAmbi", 10, 0, 10);
-    private IHistogram1D numTracksPreAmbi = aida2.histogram1D("numTracksPreAmbi", 10, 0, 10);
-    private IHistogram1D numTracksPostAmbi = aida2.histogram1D("numTracksPostAmbi", 10, 0, 10);
-    private IHistogram1D sharedHitsPostAmbi = aida2.histogram1D("sharedHitsPostAmbi", 10, 0, 10);
-    private IHistogram1D numHitsPreAmbi = aida2.histogram1D("numHitsPreAmbi", 10, 0, 10);
-    private IHistogram1D numHitsPostAmbi = aida2.histogram1D("numHitsPostAmbi", 10, 0, 10);
+    private AIDA aida2;
+    private IHistogram1D trackScoresPreAmbi;
+    private IHistogram1D trackScoresPostAmbi;
+    private IHistogram1D numDuplicateTracks;
+    private IHistogram1D numPartialTracks;
+    private IHistogram1D numSharedTracks;
+    private IHistogram1D sharedHitsPreAmbi;
+    private IHistogram1D numTracksPreAmbi;
+    private IHistogram1D numTracksPostAmbi;
+    private IHistogram1D sharedHitsPostAmbi;
+    private IHistogram1D numHitsPreAmbi;
+    private IHistogram1D numHitsPostAmbi;
 
     /**
      * Default constructor
@@ -114,6 +114,21 @@ public class MergeTrackCollections extends Driver {
         // acc = new AcceptanceHelper();
         // acc.initializeMaps(detector, "Tracker");
         ambi = new SimpleAmbiguityResolver();
+
+        if (doPlots) {
+            aida2 = AIDA.defaultInstance();
+            trackScoresPreAmbi = aida2.histogram1D("trackScoresPreAmbi", 200, -100, 100);
+            trackScoresPostAmbi = aida2.histogram1D("trackScoresPostAmbi", 200, -100, 100);
+            numDuplicateTracks = aida2.histogram1D("numDuplicateTracks", 10, 0, 10);
+            numPartialTracks = aida2.histogram1D("numPartialTracks", 10, 0, 10);
+            numSharedTracks = aida2.histogram1D("numSharedTracks", 10, 0, 10);
+            sharedHitsPreAmbi = aida2.histogram1D("sharedHitsPreAmbi", 10, 0, 10);
+            numTracksPreAmbi = aida2.histogram1D("numTracksPreAmbi", 10, 0, 10);
+            numTracksPostAmbi = aida2.histogram1D("numTracksPostAmbi", 10, 0, 10);
+            sharedHitsPostAmbi = aida2.histogram1D("sharedHitsPostAmbi", 10, 0, 10);
+            numHitsPreAmbi = aida2.histogram1D("numHitsPreAmbi", 10, 0, 10);
+            numHitsPostAmbi = aida2.histogram1D("numHitsPostAmbi", 10, 0, 10);
+        }
     }
 
     @Override
@@ -185,13 +200,14 @@ public class MergeTrackCollections extends Driver {
     @Override
     protected void endOfData() {
         super.endOfData();
-
-        File outputFile2 = new TestOutputFile("mergingPlots.aida");
-        outputFile2.getParentFile().mkdirs();
-        try {
-            aida2.saveAs(outputFile2);
-        } catch (IOException ex) {
-            Logger.getLogger(MergeTrackCollections.class.getName()).log(Level.SEVERE, null, ex);
+        if (doPlots) {
+            File outputFile2 = new TestOutputFile("mergingPlots.aida");
+            outputFile2.getParentFile().mkdirs();
+            try {
+                aida2.saveAs(outputFile2);
+            } catch (IOException ex) {
+                Logger.getLogger(MergeTrackCollections.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 }
