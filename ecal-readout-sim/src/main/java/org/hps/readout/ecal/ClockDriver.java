ef600ca37e913207949ce0b670cf85f4b7e53fa1
mccaky <mccaky@gmail.com>
2017-07-23 15:05:25 -0400

    Resubmitting ClockDriver changes, since they still display as changed.

--- ecal-readout-sim/src/main/java/org/hps/readout/ecal/ClockDriver.java
+++ ecal-readout-sim/src/main/java/org/hps/readout/ecal/ClockDriver.java
@@ -4,22 +4,37 @@ import org.lcsim.event.EventHeader;
 import org.lcsim.util.Driver;
 
 /**
- * Driver to run the clock in ClockSingleton. Run this driver last.
- *
+ * Class <code>ClockDriver</code> is used to manage the readout
+ * simulation time object {@link org.hps.readout.ecal.ClockSingleton
+ * ClockSingleton}. It increments the total simulation every event
+ * by an amount specified in the steering file, or 2 ns by default.
+ * </br/><br/>
+ * Note: This driver must be run <b>last</b> to ensure that all of
+ * the drivers in the event chain receive the same simulation time if
+ * they access the simulation clock.
+ * 
  * @author Sho Uemura <meeg@slac.stanford.edu>
- * @version $Id: ClockDriver.java,v 1.2 2011/10/07 23:14:55 meeg Exp $
  */
 public class ClockDriver extends Driver {
-    public void setDt(double dt) {
-        ClockSingleton.setStepSize(dt);
-    }
-
+    @Override
     public void process(EventHeader event) {
         ClockSingleton.step();
         TriggerDriver.resetTriggerBit();
     }
-
+    
+    @Override
     public void startOfData() {
         ClockSingleton.init();
+    }
+    
+    /**
+     * Sets the step size of the simulation clock. The value is in
+     * units of nanoseconds. This must be equal to the size of the
+     * beam bunches in the input Monte Carlo. By default, this is set
+     * to 2 ns.
+     * @param stepSize - The beam bunch size in units of nanoseconds.
+     */
+    public void setStepSize(double stepSize) {
+        ClockSingleton.setStepSize(stepSize);
     }
 }
