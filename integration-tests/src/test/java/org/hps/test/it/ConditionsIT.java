ef3cad1b065ed9112eb6332eff1732e607e801b2
JeremyMcCormick <jermccormick@gmail.com>
2017-07-27 15:24:00 -0700

    add additional test methods to conditions IT

--- integration-tests/src/test/java/org/hps/test/it/ConditionsIT.java
+++ integration-tests/src/test/java/org/hps/test/it/ConditionsIT.java
@@ -11,13 +11,13 @@ import org.hps.job.JobManager;
 import org.lcsim.util.test.TestUtil.TestOutputFile;
 
 public class ConditionsIT extends TestCase {
-        
+            
     public void setUp() {
         new DatabaseConditionsManager();
     }
     
     public void testEvioToLcioEngRun2015() {
-        File inputFile = new TestDataUtility().getTestData("run5772_integrationTest.evio");        
+        File inputFile = new TestDataUtility().getTestData("run5772_integrationTest.evio");
         File outputFile = new TestOutputFile("EvioToLcioEngRun2015");
         String args[] = {"-r", "-x", "/org/hps/steering/test/EngRun2015FullRecon_CondCheck.lcsim", "-d",
                 "HPS-EngRun2015-Nominal-v3", "-D", "outputFile=" + outputFile.getPath(), "-n", "2",
@@ -25,7 +25,7 @@ public class ConditionsIT extends TestCase {
         EvioToLcio.main(args);
     }
         
-    public void testLcioRecon() {
+    public void testLcioReconEngRun2015() {
         File inputFile = new TestDataUtility().getTestData("run_5772_data_only.slcio");
         File outputFile = new TestOutputFile("LcioRecon");
         JobManager job = new JobManager();
@@ -39,21 +39,37 @@ public class ConditionsIT extends TestCase {
     public void testEvioToLcioPhysRun2016() {
         File inputFile = new TestDataUtility().getTestData("hps_007457_1000.evio.0");        
         File outputFile = new TestOutputFile("EvioToLcioEngRun2015");
-        String args[] = {"-r", "-x", "/org/hps/steering/recon/PhysicsRun2016FullRecon.lcsim", "-d",
+        String args[] = {"-r", "-x", "/org/hps/steering/test/PhysicsRun2016FullRecon_CondCheck.lcsim", "-d",
                 "HPS-EngRun2015-Nominal-v3", "-D", "outputFile=" + outputFile.getPath(), "-n", "2",
                 inputFile.getPath()};
         EvioToLcio.main(args);
     }
     
     public void testEvioToLcioTestRun() {
+        File inputFile = new TestDataUtility().getTestData("hps_001306_1000.evio.0");
+        File outputFile = new TestOutputFile("EvioToLcioTestRun.slcio");
+        String args[] = {"-r", "-d", "HPS-TestRun-v8-5", "-l", outputFile.getPath(), "-n", "2",
+                inputFile.getPath()};
+        try {
+            EvioToLcio.main(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public void testMCDefaultConditions() {        
+    public void testMCDefaultConditions() {
+        // TODO: MC job using the default "run 0" conditions
     }
     
     public void testMCUserConditions() {
+        // TODO: MC job using user settings for run and detector
+    }
+    
+    public void testMCLayer0() {
+        // TODO: MC job with L0 detector
     }
     
     public void testReadoutSim() {
+        // TODO: readout simulation job
     }
 }
