0a12072c35b6d31f8697ba651238e73722f57869
JeremyMcCormick <jermccormick@gmail.com>
2017-07-27 17:20:06 -0700

    add skeleton for testing MC conditions

--- integration-tests/src/test/java/org/hps/test/it/ConditionsIT.java
+++ integration-tests/src/test/java/org/hps/test/it/ConditionsIT.java
@@ -7,10 +7,15 @@ import junit.framework.TestCase;
 import org.hps.conditions.database.DatabaseConditionsManager;
 import org.hps.data.test.TestDataUtility;
 import org.hps.evio.EvioToLcio;
+import org.hps.job.DatabaseConditionsManagerSetup;
 import org.hps.job.JobManager;
 import org.lcsim.util.test.TestUtil.TestOutputFile;
 
 public class ConditionsIT extends TestCase {
+    
+    static {
+        System.getProperties().setProperty("disableSvtAlignmentConstants", "true");
+    }
             
     public void setUp() {
         new DatabaseConditionsManager();
@@ -48,7 +53,7 @@ public class ConditionsIT extends TestCase {
     public void testEvioToLcioTestRun() {
         File inputFile = new TestDataUtility().getTestData("hps_001306_1000.evio.0");
         File outputFile = new TestOutputFile("EvioToLcioTestRun.slcio");
-        String args[] = {"-r", "-d", "HPS-TestRun-v8-5", "-l", outputFile.getPath(), "-n", "2",
+        String args[] = {"-r", "-d", "HPS-TestRun-v8-5", "-l", outputFile.getPath(), "-n", "1000",
                 inputFile.getPath()};
         try {
             EvioToLcio.main(args);
@@ -56,20 +61,36 @@ public class ConditionsIT extends TestCase {
             e.printStackTrace();
         }
     }
-
-    public void testMCDefaultConditions() {
-        // TODO: MC job using the default "run 0" conditions
+       
+    public void testMCReconDefaultConditions() {
+        File inputFile = new TestDataUtility().getTestData("tritrig_EngRun2015_readout.slcio");
+        File outputFile = new TestOutputFile("MCReconDefaultConditions");
+        JobManager job = new JobManager();
+        job.addVariableDefinition("outputFile", outputFile.getPath());
+        job.addInputFile(inputFile);
+        job.setup("/org/hps/steering/test/EngineeringRun2015FullReconMC_CondCheck.lcsim");
+        job.setNumberOfEvents(2);
+        job.run();
     }
     
-    public void testMCUserConditions() {
-        // TODO: MC job using user settings for run and detector
-    }
-    
-    public void testMCLayer0() {
-        // TODO: MC job with L0 detector
-    }
+    public void testMCReconUserConditions() {
+        File inputFile = new TestDataUtility().getTestData("tritrig_EngRun2015_readout.slcio");
+        File outputFile = new TestOutputFile("MCReconDefaultConditions");
+        JobManager job = new JobManager();
+        DatabaseConditionsManagerSetup cond = new DatabaseConditionsManagerSetup();
+        cond.setDetectorName("HPS-EngRun2015-Nominal-v5-0-fieldmap");
+        cond.setRun(5772);
+        cond.setFreeze(true);
+        job.setConditionsSetup(cond);
+        job.addVariableDefinition("outputFile", outputFile.getPath());
+        job.addInputFile(inputFile);
+        job.setup("/org/hps/steering/test/EngineeringRun2015FullReconMC_CondCheck.lcsim");
+        job.setNumberOfEvents(2);
+        job.run();
+    }    
     
     public void testReadoutSim() {
         // TODO: readout simulation job
+        // /org/hps/steering/readout/EngineeringRun2015TrigPairs1_Pass2.lcsim
     }
 }
