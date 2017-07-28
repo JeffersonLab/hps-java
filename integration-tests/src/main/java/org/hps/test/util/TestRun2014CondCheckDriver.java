6cad5c2305f4008d5e33944ba65458b322696581
JeremyMcCormick <jermccormick@gmail.com>
2017-07-27 18:30:15 -0700

    complete checks of Test Run 2014 conditions

--- integration-tests/src/main/java/org/hps/test/util/TestRun2014CondCheckDriver.java
+++ integration-tests/src/main/java/org/hps/test/util/TestRun2014CondCheckDriver.java
@@ -5,14 +5,24 @@ import java.util.logging.Logger;
 import junit.framework.TestCase;
 
 import org.hps.conditions.database.DatabaseConditionsManager;
+import org.hps.conditions.ecal.EcalCalibration.EcalCalibrationCollection;
+import org.hps.conditions.ecal.EcalChannel.EcalChannelCollection;
 import org.hps.conditions.ecal.EcalConditions;
-import org.hps.conditions.svt.SvtConditions;
+import org.hps.conditions.ecal.EcalGain.EcalGainCollection;
+import org.hps.conditions.ecal.EcalPulseWidth.EcalPulseWidthCollection;
+import org.hps.conditions.svt.SvtCalibration.SvtCalibrationCollection;
+import org.hps.conditions.svt.SvtGain.SvtGainCollection;
+import org.hps.conditions.svt.SvtShapeFitParameters.SvtShapeFitParametersCollection;
+import org.hps.conditions.svt.TestRunSvtChannel.TestRunSvtChannelCollection;
+import org.hps.conditions.svt.TestRunSvtConditions;
+import org.hps.conditions.svt.TestRunSvtDaqMapping.TestRunSvtDaqMappingCollection;
+import org.hps.conditions.svt.TestRunSvtT0Shift.TestRunSvtT0ShiftCollection;
 import org.lcsim.conditions.ConditionsManager;
 import org.lcsim.geometry.Detector;
 import org.lcsim.util.Driver;
 
 /**
- * Checks that the correct conditions are loaded for engineering run 2015 reconstruction
+ * Checks that the correct conditions are loaded for Test Run 2015 reconstruction
  * by verifying the collection IDs.
  */
 public class TestRun2014CondCheckDriver extends Driver {
@@ -32,52 +42,38 @@ public class TestRun2014CondCheckDriver extends Driver {
         TestCase.assertFalse("The detectorChanged method was executed more than once!", executed);
         
         ConditionsManager mgr = DatabaseConditionsManager.getInstance();
+
+        TestRunSvtChannelCollection svtChannels = mgr.getCachedConditions(TestRunSvtChannelCollection.class, "test_run_svt_channels").getCachedData();
+        TestCase.assertEquals("SvtChannelCollection has wrong collection ID.", 1, svtChannels.getCollectionId());
+
+        TestRunSvtDaqMappingCollection svtDaqMap = mgr.getCachedConditions(TestRunSvtDaqMappingCollection.class, "test_run_svt_daq_map").getCachedData();
+        TestCase.assertEquals("SvtDaqMappingCollection has wrong collection ID.", 1, svtDaqMap.getCollectionId());
         
-        /*
-        SvtChannelCollection svtChannels = mgr.getCachedConditions(SvtChannelCollection.class, "svt_channels").getCachedData();
-        TestCase.assertEquals("SvtChannelCollection has wrong collection ID.", 2, svtChannels.getCollectionId());
+        TestRunSvtT0ShiftCollection svtT0Shifts = mgr.getCachedConditions(TestRunSvtT0ShiftCollection.class, "test_run_svt_t0_shifts").getCachedData();
+        TestCase.assertEquals("SvtT0ShiftCollection has wrong collection ID.", 1, svtT0Shifts.getCollectionId());
         
-        SvtDaqMappingCollection svtDaqMap = mgr.getCachedConditions(SvtDaqMappingCollection.class, "svt_daq_map").getCachedData();
-        TestCase.assertEquals("SvtDaqMappingCollection has wrong collection ID.", 2, svtDaqMap.getCollectionId());
+        SvtCalibrationCollection svtCalib = mgr.getCachedConditions(SvtCalibrationCollection.class, "test_run_svt_calibrations").getCachedData();
+        TestCase.assertEquals("SvtCalibrationCollection has wrong collection ID.", 1, svtCalib.getCollectionId());
         
-        SvtT0ShiftCollection svtT0Shifts = mgr.getCachedConditions(SvtT0ShiftCollection.class, "svt_t0_shifts").getCachedData();
-        TestCase.assertEquals("SvtT0ShiftCollection has wrong collection ID.", 1940, svtT0Shifts.getCollectionId());
+        SvtShapeFitParametersCollection svtShapeFit = mgr.getCachedConditions(SvtShapeFitParametersCollection.class, "test_run_svt_shape_fit_parameters").getCachedData();
+        TestCase.assertEquals("SvtShapeFitParametersCollection has wrong collection ID.", 1, svtShapeFit.getCollectionId());
         
-        SvtCalibrationCollection svtCalib = mgr.getCachedConditions(SvtCalibrationCollection.class, "svt_calibrations").getCachedData();
-        TestCase.assertEquals("SvtCalibrationCollection has wrong collection ID.", 3, svtCalib.getCollectionId());
+        SvtGainCollection svtGains = mgr.getCachedConditions(SvtGainCollection.class, "test_run_svt_gains").getCachedData();
+        TestCase.assertEquals("SvtGainCollection has wrong collection ID.", 1, svtGains.getCollectionId());
+
+        EcalChannelCollection ecalChannels = mgr.getCachedConditions(EcalChannelCollection.class, "test_run_ecal_channels").getCachedData();
+        TestCase.assertEquals("EcalChannelCollection has wrong collection ID.", 1, ecalChannels.getCollectionId());
+                
+        EcalGainCollection ecalGains = mgr.getCachedConditions(EcalGainCollection.class, "test_run_ecal_gains").getCachedData();
+        TestCase.assertEquals("EcalGainCollection has wrong collection ID.", 1, ecalGains.getCollectionId());
         
-        SvtShapeFitParametersCollection svtShapeFit = mgr.getCachedConditions(SvtShapeFitParametersCollection.class, "svt_shape_fit_parameters").getCachedData();
-        TestCase.assertEquals("SvtShapeFitParametersCollection has wrong collection ID.", 1003, svtShapeFit.getCollectionId());
-        
-        SvtGainCollection svtGains = mgr.getCachedConditions(SvtGainCollection.class, "svt_gains").getCachedData();
-        TestCase.assertEquals("SvtGainCollection has wrong collection ID.", 2, svtGains.getCollectionId());
-        
-        EcalChannelCollection ecalChannels = mgr.getCachedConditions(EcalChannelCollection.class, "ecal_channels").getCachedData();
-        TestCase.assertEquals("EcalChannelCollection has wrong collection ID.", 2, ecalChannels.getCollectionId());
-        
-        EcalGainCollection ecalGains = mgr.getCachedConditions(EcalGainCollection.class, "ecal_gains").getCachedData();
-        TestCase.assertEquals("EcalGainCollection has wrong collection ID.", 1898, ecalGains.getCollectionId());
-        
-        EcalCalibrationCollection ecalCalib = mgr.getCachedConditions(EcalCalibrationCollection.class, "ecal_calibrations").getCachedData();
-        TestCase.assertEquals("EcalCalibrationCollection has wrong collection ID.", 1008, ecalCalib.getCollectionId());
-        
-        EcalTimeShiftCollection ecalTimeShifts = mgr.getCachedConditions(EcalTimeShiftCollection.class, "ecal_time_shifts").getCachedData();
-        TestCase.assertEquals("EcalTimeShiftCollectio has wrong collection ID.", 1899, ecalTimeShifts.getCollectionId());
-        
+        EcalCalibrationCollection ecalCalib = mgr.getCachedConditions(EcalCalibrationCollection.class, "test_run_ecal_calibrations").getCachedData();
+        TestCase.assertEquals("EcalCalibrationCollection has wrong collection ID.", 1, ecalCalib.getCollectionId());
+                
         EcalPulseWidthCollection ecalPulseWidths = mgr.getCachedConditions(EcalPulseWidthCollection.class, "ecal_pulse_widths").getCachedData();
         TestCase.assertEquals("EcalPulseWidthCollection has wrong collection ID.", 1034, ecalPulseWidths.getCollectionId());
-        
-        SvtBiasConstantCollection svtBiasConstants = mgr.getCachedConditions(SvtBiasConstantCollection.class, "svt_bias_constants").getCachedData();
-        TestCase.assertEquals("SvtBiasConstantCollection has wrong collection ID.", 1605, svtBiasConstants.getCollectionId());
-        
-        SvtMotorPositionCollection svtMotorPositions = mgr.getCachedConditions(SvtMotorPositionCollection.class, "svt_motor_positions").getCachedData();
-        TestCase.assertEquals("SvtMotorPositionCollection has wrong collection ID.", 1873, svtMotorPositions.getCollectionId());
-        
-        SvtTimingConstantsCollection svtTimingConstants = mgr.getCachedConditions(SvtTimingConstantsCollection.class, "svt_timing_constants").getCachedData();
-        TestCase.assertEquals("SvtTimingConstantsCollection has wrong collection ID.", 1029, svtTimingConstants.getCollectionId());
-        */
-        
-        SvtConditions svtConditions = mgr.getCachedConditions(SvtConditions.class, "svt_conditions").getCachedData();
+                        
+        TestRunSvtConditions svtConditions = mgr.getCachedConditions(TestRunSvtConditions.class, "svt_conditions").getCachedData();
         TestCase.assertNotNull("SvtConditions is null!", svtConditions);
         System.out.println(svtConditions.toString());
         
