8e8a0c9e24b1c129dae1cd0b2b64220c08f4ebc8
normangraf <Norman.Graf@slac.stanford.edu>
2017-09-12 15:27:55 -0700

    Trident Recon integration tests

---
+++ integration-tests/src/test/java/org/hps/test/it/EngRun2015V0ReconTest.java
@@ -0,0 +1,53 @@
+package org.hps.test.it;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import junit.framework.TestCase;
+import org.hps.evio.EvioToLcio;
+import org.hps.test.util.TestOutputFile;
+import org.lcsim.util.cache.FileCache;
+import org.lcsim.util.loop.LCSimLoop;
+import org.lcsim.util.test.TestUtil;
+
+/**
+ *
+ * @author Norman A. Graf
+ */
+public class EngRun2015V0ReconTest extends TestCase {
+static final String testURLBase = "http://www.lcsim.org/test/hps-java/calibration";
+    static final String testFileName = "hps_005772_v0skim_10k.evio";
+    private final int nEvents = -1;
+
+    public void testIt() throws Exception {
+        URL testURL = new URL(testURLBase + "/" + testFileName);
+        FileCache cache = new FileCache();
+        File evioInputFile = cache.getCachedFile(testURL);
+        File outputFile = new TestOutputFile(EngRun2015V0ReconTest.class, "EngRun2015MollerReconTest");
+        String args[] = {"-r", "-x", "/org/hps/steering/recon/EngineeringRun2015FullRecon.lcsim", "-d",
+            "HPS-EngRun2015-Nominal-v6-0-fieldmap", "-D", "outputFile=" + outputFile.getPath(),// "-n", "200",
+            evioInputFile.getPath(), "-e", "1000"};
+        System.out.println("Running EngRun2015MollerReconTest.main ...");
+        System.out.println("writing to: " + outputFile.getPath());
+        long startTime = System.currentTimeMillis();
+        EvioToLcio.main(args);
+        long endTime = System.currentTimeMillis();
+        System.out.println("That took " + (endTime - startTime) + " milliseconds");
+        // Read in the LCIO event file and print out summary information.
+        System.out.println("Running ReconCheckDriver on output ...");
+        LCSimLoop loop = new LCSimLoop();
+        EngRun2015V0Recon reconDriver = new EngRun2015V0Recon();
+        String aidaOutputFile = new TestUtil.TestOutputFile(getClass().getSimpleName()).getPath() + File.separator + this.getClass().getSimpleName() + ".aida";
+        System.out.println("writing aida file to: "+aidaOutputFile);
+        reconDriver.setAidaFileName(aidaOutputFile);
+        loop.add(reconDriver);
+        try {
+            loop.setLCIORecordSource(new File(outputFile.getPath() + ".slcio"));
+            loop.loop(-1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Loop processed " + loop.getTotalSupplied() + " events.");
+        System.out.println("Done!");
+    }
+}
