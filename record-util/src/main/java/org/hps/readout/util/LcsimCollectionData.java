bca809abf57438b5b368a31ee39f0111e59e11d5
mccaky <mccaky@gmail.com>
2017-12-15 10:24:21 -0500

    Updated class to account for changes in LcsimCollection.

--- record-util/src/main/java/org/hps/readout/util/LcsimCollectionData.java
+++ record-util/src/main/java/org/hps/readout/util/LcsimCollectionData.java
@@ -2,38 +2,28 @@ package org.hps.readout.util;
 
 import java.util.LinkedList;
 
-import org.hps.readout.ReadoutDriver;
-
 public class LcsimCollectionData<T> extends LcsimCollection<T> {
 	private final LinkedList<TimedList<?>> data;
 	
-	public LcsimCollectionData(String collectionName, ReadoutDriver productionDriver, Class<T> objectType, double globalTimeDisplacement) {
-		this(collectionName, productionDriver,objectType, true, globalTimeDisplacement, 0, null);
+	public LcsimCollectionData(LcsimCollection<T> params) {
+		super(params.getCollectionName(), params.getProductionDriver(), params.getObjectType(), params.getGlobalTimeDisplacement());
+		setPersistent(params.isPersistent());
+		setFlags(params.getFlags());
+		setReadoutName(params.getReadoutName());
+		setWindowBefore(params.getWindowBefore());
+		setWindowAfter(params.getWindowAfter());
+		
+		this.data = new LinkedList<TimedList<?>>();
 	}
 	
-	public LcsimCollectionData(String collectionName, ReadoutDriver productionDriver, Class<T> objectType, double globalTimeDisplacement,
-			boolean persistent) {
-		this(collectionName, productionDriver,objectType, persistent, globalTimeDisplacement, 0, null);
-	}
-	
-	public LcsimCollectionData(String collectionName, ReadoutDriver productionDriver, Class<T> objectType, double globalTimeDisplacement,
-			int flags) {
-		this(collectionName, productionDriver,objectType, true, globalTimeDisplacement, flags, null);
-	}
-	
-	public LcsimCollectionData(String collectionName, ReadoutDriver productionDriver, Class<T> objectType, double globalTimeDisplacement,
-			boolean persistent, int flags) {
-		this(collectionName, productionDriver,objectType, persistent, globalTimeDisplacement, flags, null);
-	}
-	
-	public LcsimCollectionData(String collectionName, ReadoutDriver productionDriver, Class<T> objectType, double globalTimeDisplacement,
-			int flags, String readoutName) {
-		this(collectionName, productionDriver,objectType, true, globalTimeDisplacement, flags, readoutName);
-	}
-	
-	public LcsimCollectionData(String collectionName, ReadoutDriver productionDriver, Class<T> objectType, boolean persistent, double globalTimeDisplacement,
-			int flags, String readoutName) {
-		super(collectionName, productionDriver, objectType, persistent, globalTimeDisplacement, flags, readoutName);
+	public LcsimCollectionData(LcsimCollection<T> params, double timeDisplacement) {
+		super(params.getCollectionName(), params.getProductionDriver(), params.getObjectType(), timeDisplacement);
+		setPersistent(params.isPersistent());
+		setFlags(params.getFlags());
+		setReadoutName(params.getReadoutName());
+		setWindowBefore(params.getWindowBefore());
+		setWindowAfter(params.getWindowAfter());
+		
 		this.data = new LinkedList<TimedList<?>>();
 	}
 	
