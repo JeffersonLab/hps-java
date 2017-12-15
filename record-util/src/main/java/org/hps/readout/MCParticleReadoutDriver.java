7e5d23779421da0cc7c1c7999430464e1a96c0dd
mccaky <mccaky@gmail.com>
2017-12-15 10:21:58 -0500

    Added documentation to MCParticleReadoutDriver.java.

--- record-util/src/main/java/org/hps/readout/MCParticleReadoutDriver.java
+++ record-util/src/main/java/org/hps/readout/MCParticleReadoutDriver.java
@@ -2,7 +2,21 @@ package org.hps.readout;
 
 import org.lcsim.event.MCParticle;
 
+/**
+ * <code>MCParticleReadoutDriver</code> handles SLIC objects in input
+ * Monte Carlo files of type {@link org.lcsim.event.MCParticle
+ * MCParticle}.
+ * 
+ * @author Kyle McCarty <mccarty@jlab.org>
+ * @see org.hps.readout.SLICDataReadoutDriver
+ */
 public class MCParticleReadoutDriver extends SLICDataReadoutDriver<MCParticle> {
+	/**
+	 * Instantiate an instance of {@link
+	 * org.hps.readout.SLICDataReadoutDriver SLICDataReadoutDriver}
+	 * for objects of type {@link org.lcsim.event.MCParticle
+	 * MCParticle}. These do not require any special LCIO flags.
+	 */
 	public MCParticleReadoutDriver() {
 		super(MCParticle.class);
 		setCollectionName("MCParticle");
