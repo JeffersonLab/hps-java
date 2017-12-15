ed000ab58a0d85e238f320566b23f7c887ef845a
mccaky <mccaky@gmail.com>
2017-12-15 10:34:19 -0500

    Updated driver to support new structure for debug output. This should be removed after final integration has been completed.

--- record-util/src/main/java/org/hps/readout/ReadoutDriver.java
+++ record-util/src/main/java/org/hps/readout/ReadoutDriver.java
@@ -8,9 +8,33 @@ import org.hps.readout.util.LcsimSingleEventCollectionData;
 import org.lcsim.util.Driver;
 
 public abstract class ReadoutDriver extends Driver {
+	protected boolean debug = true;
 	private final Set<String> dependencies = new HashSet<String>();
+	protected java.util.List<TempOutputWriter> writers = new java.util.ArrayList<TempOutputWriter>();
 	
-	protected abstract double getTimeDisplacement();
+	protected ReadoutDriver() {
+		ReadoutDataManager.registerReadoutDriver(this);
+	}
+	
+	@Override
+	public void endOfData() {
+		for(TempOutputWriter writer : writers) {
+			if(debug && writer != null) { writer.close(); }
+		}
+	}
+	
+	@Override
+	public void startOfData() {
+		// Set the debug status for each writer. If the writer should
+		// output anything, tell it to delete the file on exit.
+		if(debug) {
+			for(TempOutputWriter writer : writers) {
+				System.out.println(writer.toString());
+				writer.initialize();
+				writer.setEnabled(debug);
+			}
+		}
+	}
 	
 	protected void addDependency(String collectionName) {
 		dependencies.add(collectionName);
@@ -22,5 +46,13 @@ public abstract class ReadoutDriver extends Driver {
 	
 	protected Collection<LcsimSingleEventCollectionData<?>> getOnTriggerData(double triggerTime) {
 		return null;
+	}
+	
+	protected abstract double getTimeDisplacement();
+	
+	protected abstract double getTimeNeededForLocalOutput();
+	
+	public void setDebug(boolean state) {
+		debug = state;
 	}
 }
