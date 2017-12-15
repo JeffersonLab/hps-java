f484e1884b2b57bed2ca6b3a79b4eecb45274ba2
mccaky <mccaky@gmail.com>
2017-12-15 10:24:33 -0500

    Updated class to account for changes in LcsimCollection.

--- record-util/src/main/java/org/hps/readout/util/LcsimSingleEventCollectionData.java
+++ record-util/src/main/java/org/hps/readout/util/LcsimSingleEventCollectionData.java
@@ -3,34 +3,17 @@ package org.hps.readout.util;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hps.readout.ReadoutDriver;
-
 public class LcsimSingleEventCollectionData<T> extends LcsimCollection<T> {
 	private final List<T> data;
 	
-	public LcsimSingleEventCollectionData(String collectionName, ReadoutDriver productionDriver, Class<T> objectType) {
-		this(collectionName, productionDriver,objectType, true, 0, null);
-	}
-	
-	public LcsimSingleEventCollectionData(String collectionName, ReadoutDriver productionDriver, Class<T> objectType, boolean persistent) {
-		this(collectionName, productionDriver,objectType, persistent, 0, null);
-	}
-	
-	public LcsimSingleEventCollectionData(String collectionName, ReadoutDriver productionDriver, Class<T> objectType, int flags) {
-		this(collectionName, productionDriver,objectType, true, flags, null);
-	}
-	
-	public LcsimSingleEventCollectionData(String collectionName, ReadoutDriver productionDriver, Class<T> objectType, boolean persistent, int flags) {
-		this(collectionName, productionDriver,objectType, persistent, flags, null);
-	}
-	
-	public LcsimSingleEventCollectionData(String collectionName, ReadoutDriver productionDriver, Class<T> objectType, int flags, String readoutName) {
-		this(collectionName, productionDriver,objectType, true, flags, readoutName);
-	}
-	
-	public LcsimSingleEventCollectionData(String collectionName, ReadoutDriver productionDriver, Class<T> objectType, boolean persistent,
-			int flags, String readoutName) {
-		super(collectionName, productionDriver, objectType, persistent, Double.NaN, flags, readoutName);
+	public LcsimSingleEventCollectionData(LcsimCollection<T> params) {
+		super(params.getCollectionName(), params.getProductionDriver(), params.getObjectType(), params.getGlobalTimeDisplacement());
+		setPersistent(params.isPersistent());
+		setFlags(params.getFlags());
+		setReadoutName(params.getReadoutName());
+		setWindowBefore(params.getWindowBefore());
+		setWindowAfter(params.getWindowAfter());
+		
 		this.data = new ArrayList<T>();
 	}
 	
