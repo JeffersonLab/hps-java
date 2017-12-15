ba8b33a00ebf581a194fc2ca86813967ec79c948
mccaky <mccaky@gmail.com>
2017-12-15 10:32:23 -0500

    Added debug output behavior matching this driver's analogue in the new framework to allow for behavioral comparison.

--- ecal-recon/src/main/java/org/hps/recon/ecal/cluster/GTPClusterDriver.java
+++ ecal-recon/src/main/java/org/hps/recon/ecal/cluster/GTPClusterDriver.java
@@ -25,7 +25,28 @@ public class GTPClusterDriver extends ClusterDriver {
     /** An instance of the clustering algorithm object for producing
      * cluster objects. */
     private final GTPClusterer gtp;
+    protected boolean debug = true;
+	
     
+	@Override
+	public void endOfData() {
+		super.endOfData();
+		if(debug) { gtp.closeWriters(); }
+	}
+    
+	@Override
+	public void startOfData() {
+		super.startOfData();
+		if(debug) { gtp.initializeWriters(); }
+	}
+	
+	@Override
+	public void process(org.lcsim.event.EventHeader event) {
+		gtp.writeEventHeaders(event);
+		gtp.setEvent(event.getEventNumber());
+		super.process(event);
+	}
+	
     /**
      * Instantiates a new <code>GTPClusterer</code>, which will produce
      * clusters using the GTP algorithm in the 2-ns beam bunch scheme.
@@ -52,6 +73,10 @@ public class GTPClusterDriver extends ClusterDriver {
     @Deprecated
     public void setLimitClusterRange(boolean limitClusterRange) {
         gtp.setAsymmetricWindow(limitClusterRange);
+    }
+    
+    public void setDebug(boolean state) {
+    	debug = state;
     }
     
     /**
