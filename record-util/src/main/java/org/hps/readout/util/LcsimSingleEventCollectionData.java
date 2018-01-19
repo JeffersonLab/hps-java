24d790be5452d2aa5ef36b1a32a7b8a6a1e1dd6c
mccaky <mccaky@gmail.com>
2018-01-18 23:42:26 -0500

    Added documentation for class LcsimSingleEventCollectionData.

--- record-util/src/main/java/org/hps/readout/util/LcsimSingleEventCollectionData.java
+++ record-util/src/main/java/org/hps/readout/util/LcsimSingleEventCollectionData.java
@@ -3,9 +3,28 @@ package org.hps.readout.util;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class <code>LcsimSingleEventCollectionData</code> is an extension
+ * of {@link org.hps.readout.util.LcsimCollection LcsimCollection}
+ * which additionally stores a single set of collection data. It is
+ * designed to be used to store on-trigger special data from {@link
+ * org.hps.readout.ReadoutDriver ReadoutDriver} objects.
+ * 
+ * @author Kyle McCarty <mccarty@jlab.org>
+ * @param <T> - The object type of the data stored by the collection.
+ */
 public class LcsimSingleEventCollectionData<T> extends LcsimCollection<T> {
+	/**
+	 * The collection data.
+	 */
 	private final List<T> data;
 	
+	/**
+	 * Creates a new <code>LcsimSingleEventCollectionData</code
+	 * based on the collection parameters defined by the
+	 * <code>params</code> object.
+	 * @param params - The collection parameters.
+	 */
 	public LcsimSingleEventCollectionData(LcsimCollection<T> params) {
 		super(params.getCollectionName(), params.getProductionDriver(), params.getObjectType(), params.getGlobalTimeDisplacement());
 		setPersistent(params.isPersistent());
@@ -17,6 +36,11 @@ public class LcsimSingleEventCollectionData<T> extends LcsimCollection<T> {
 		this.data = new ArrayList<T>();
 	}
 	
+	/**
+	 * Gets the collection data.
+	 * @return Returns the collection data as a {@link java.util.List
+	 * List}.
+	 */
 	public List<T> getData() {
 		return data;
 	}
