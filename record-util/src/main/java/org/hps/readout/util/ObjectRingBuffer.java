c3f6127110ec083ba5ea6dbea7ba226df8767196
mccaky <mccaky@gmail.com>
2018-01-27 14:44:41 -0500

    Updated ObjectRingBuffer to override the selector and mutator methods to prevent users from directly modifying the underlying collections objects that contain buffer cell data.

--- record-util/src/main/java/org/hps/readout/util/ObjectRingBuffer.java
+++ record-util/src/main/java/org/hps/readout/util/ObjectRingBuffer.java
@@ -1,5 +1,7 @@
 package org.hps.readout.util;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -10,6 +12,11 @@ import java.util.Set;
  * {@link org.hps.readout.util.RingBuffer#addToCell(int, Object)
  * addToCell(int, Object)} method simply adds the indicated object to
  * the set stored in the specified cell.
+ * <br/><br/>
+ * <b>Warning:</b> When getting a cell value, the underlying {@link
+ * java.util.Set Set} object is returned. The <code>Set</code> object
+ * should not be manipulated directly, or the ring buffer may behave
+ * in an unexpected manner.
  * 
  * @author Kyle McCarty <mccarty@jlab.org>
  * @param <T> - The type of object that is stored in each of the
@@ -44,6 +51,100 @@ public class ObjectRingBuffer<T> extends RingBuffer<Set<T>, T> {
 	@Override
 	public void clearValue(int position) {
 		getValue(position).clear();
+	}
+	
+	// ==============================================================
+	// Selector methods are overridden to return an unmodifiable Set
+	// object instead of the actual underlying set object to prevent
+	// a user from directly altering the underlying Set itself, which
+	// could produce unexpected behavior.
+	// ==============================================================
+	
+	@Override
+	public Set<T> getValue() {
+		return Collections.unmodifiableSet(super.getValue());
+	}
+	
+	@Override
+	public Set<T> getValue(int position) {
+		return Collections.unmodifiableSet(super.getValue(position));
+	}
+	
+	// ==============================================================
+	// Mutator methods are overridden to prevent direct modification
+	// of the underlying Set objects used by the buffer. Instead, the
+	// underlying sets are simply cleared and the elements of the
+	// arguments are added instead.
+	// ==============================================================
+	
+	@Override
+	public void setValue(Set<T> values) {
+		setValue(0, values);
+	}
+	
+	/**
+	 * Sets the current value in the buffer.
+	 * @param values - The new buffer values.
+	 */
+	@SuppressWarnings("unchecked")
+	public void setValue(T... values) {
+		setValue(0, values);
+	}
+	
+	/**
+	 * Sets the current value in the buffer.
+	 * @param values - The new buffer values.
+	 */
+	public void setValue(Collection<T> values) {
+		setValue(0, values);
+	}
+	
+	@Override
+	public void setValue(int position, Set<T> values) {
+		setValue(position, (Collection<T>) values);
+	}
+	
+	/**
+	 * Sets the value at the specified position in the buffer.
+	 * @param position - The position of the target value relative
+	 * to the current value. A value of <code>0</code> represents the
+	 * current position.
+	 * @param values - The new buffer values.
+	 */
+	@SuppressWarnings("unchecked")
+	public void setValue(int position, T... values) {
+		// Make sure that the indicated position is valid, and then
+		// clear the underlying buffer set.
+		validatePosition(position);
+		clearValue(position);
+		
+		// If the argument array is empty, then there is nothing new
+		// to add. Similarly, a null argument is treated as an empty
+		// array. Otherwise, add the argument elements.
+		if(values != null && values.length != 0) {
+			for(T element : values) { addToCell(0, element); }
+		}
+	}
+	
+	/**
+	 * Sets the value at the specified position in the buffer.
+	 * @param position - The position of the target value relative
+	 * to the current value. A value of <code>0</code> represents the
+	 * current position.
+	 * @param values - The new buffer values.
+	 */
+	public void setValue(int position, Collection<T> values) {
+		// Make sure that the indicated position is valid, and then
+		// clear the underlying buffer set.
+		validatePosition(position);
+		clearValue(position);
+		
+		// If the argument collection is empty, then there is nothing
+		// new to add. Similarly, a null argument is treated as an
+		// empty collection. Otherwise, add the argument elements.
+		if(values != null && !values.isEmpty()) {
+			for(T element : values) { addToCell(0, element); }
+		}
 	}
 	
 	@Override
