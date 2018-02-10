f8f6c8031fe66393d12d0f4c28363930c0cb56b0
mccaky <mccaky@gmail.com>
2018-02-09 19:57:27 -0500

    Removed tabs.

--- record-util/src/main/java/org/hps/readout/util/IntegerRingBuffer.java
+++ record-util/src/main/java/org/hps/readout/util/IntegerRingBuffer.java
@@ -11,50 +11,50 @@ package org.hps.readout.util;
  * @author Kyle McCarty <mccarty@jlab.org>
  */
 public class IntegerRingBuffer extends RingBuffer<Integer, Integer> {
-	/**
-	 * Instantiates an <code>IntegerRingBuffer</code> of the
-	 * specified size and initializes all values to zero.
-	 * @param size - The number of entries in the buffer.
-	 */
-	public IntegerRingBuffer(int size) {
-		super(size, Integer.class);
-	}
-	
-	/**
-	 * Instantiates an <code>IntegerRingBuffer</code> of the
-	 * specified size and initializes all values to the indicated
-	 * value.
-	 * @param size - The number of entries in the buffer.
-	 * @param initialValue - The initial value of buffer cells.
-	 */
-	public IntegerRingBuffer(int size, int initialValue) {
-		super(size, Integer.class);
-		setAll(initialValue);
-	}
-	
-	@Override
-	public void addToCell(int position, Integer value) {
-		validatePosition(position);
-		setValue(position, getValue(position) + value);
-	}
-	
-	@Override
-	public void clearValue() {
-		setValue(0);
-	}
-	
-	@Override
-	public void clearValue(int position) {
-		setValue(position, 0);
-	}
-	
-	@Override
-	protected void clearAll() {
-		setAll(0);
-	}
-	
-	@Override
-	protected void instantiateBuffer() {
-		clearAll();
-	}
+    /**
+     * Instantiates an <code>IntegerRingBuffer</code> of the
+     * specified size and initializes all values to zero.
+     * @param size - The number of entries in the buffer.
+     */
+    public IntegerRingBuffer(int size) {
+        super(size, Integer.class);
+    }
+    
+    /**
+     * Instantiates an <code>IntegerRingBuffer</code> of the
+     * specified size and initializes all values to the indicated
+     * value.
+     * @param size - The number of entries in the buffer.
+     * @param initialValue - The initial value of buffer cells.
+     */
+    public IntegerRingBuffer(int size, int initialValue) {
+        super(size, Integer.class);
+        setAll(initialValue);
+    }
+    
+    @Override
+    public void addToCell(int position, Integer value) {
+        validatePosition(position);
+        setValue(position, getValue(position) + value);
+    }
+    
+    @Override
+    public void clearValue() {
+        setValue(0);
+    }
+    
+    @Override
+    public void clearValue(int position) {
+        setValue(position, 0);
+    }
+    
+    @Override
+    protected void clearAll() {
+        setAll(0);
+    }
+    
+    @Override
+    protected void instantiateBuffer() {
+        clearAll();
+    }
 }
