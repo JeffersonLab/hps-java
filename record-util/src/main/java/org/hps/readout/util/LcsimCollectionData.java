90f9056391f27f9af130ded2a86666433d058960
mccaky <mccaky@gmail.com>
2018-02-09 19:57:07 -0500

    Removed tabs.

--- record-util/src/main/java/org/hps/readout/util/LcsimCollectionData.java
+++ record-util/src/main/java/org/hps/readout/util/LcsimCollectionData.java
@@ -13,61 +13,61 @@ import java.util.LinkedList;
  * @param <T> - The object type of the data stored by the collection.
  */
 public class LcsimCollectionData<T> extends LcsimCollection<T> {
-	/**
-	 * The collection data. Each entry in the data list represents a
-	 * specific simulation time quantum, while the list itself holds
-	 * the collection object data.
-	 */
-	private final LinkedList<TimedList<?>> data;
-	
-	/**
-	 * Creates a new <code>LcsimCollectionData</code> based on the
-	 * collection parameters defined by the <code>params</code>
-	 * object.
-	 * @param params - The collection parameters.
-	 */
-	public LcsimCollectionData(LcsimCollection<T> params) {
-		super(params.getCollectionName(), params.getProductionDriver(), params.getObjectType(), params.getGlobalTimeDisplacement());
-		setPersistent(params.isPersistent());
-		setFlags(params.getFlags());
-		setReadoutName(params.getReadoutName());
-		setWindowBefore(params.getWindowBefore());
-		setWindowAfter(params.getWindowAfter());
-		
-		this.data = new LinkedList<TimedList<?>>();
-	}
-	
-	/**
-	 * Creates a new <code>LcsimCollectionData</code> based on the
-	 * collection parameters defined by the <code>params</code>
-	 * object and a custom time displacement.
-	 * @param params - The collection parameters.
-	 * @param timeDisplacement - The time displacement for the
-	 * collection. This overrides any values set in
-	 * <code>params</code>.
-	 */
-	public LcsimCollectionData(LcsimCollection<T> params, double timeDisplacement) {
-		super(params.getCollectionName(), params.getProductionDriver(), params.getObjectType(), timeDisplacement);
-		setPersistent(params.isPersistent());
-		setFlags(params.getFlags());
-		setReadoutName(params.getReadoutName());
-		setWindowBefore(params.getWindowBefore());
-		setWindowAfter(params.getWindowAfter());
-		
-		this.data = new LinkedList<TimedList<?>>();
-	}
-	
-	/**
-	 * Gets the collection data.
-	 * @return Returns the collection data. Collection data is stored
-	 * in a single list, which itself contains {@link
-	 * org.hps.readout.util.TimedList TimedList} objects. Each
-	 * <code>TimedList</code> object represents the collection data
-	 * generated at the simulation time of the list, which can be
-	 * obtained through the method {@link
-	 * org.hps.readout.util.TimedList#getTime() TimedList.getTime()}.
-	 */
-	public LinkedList<TimedList<?>> getData() {
-		return data;
-	}
+    /**
+     * The collection data. Each entry in the data list represents a
+     * specific simulation time quantum, while the list itself holds
+     * the collection object data.
+     */
+    private final LinkedList<TimedList<?>> data;
+    
+    /**
+     * Creates a new <code>LcsimCollectionData</code> based on the
+     * collection parameters defined by the <code>params</code>
+     * object.
+     * @param params - The collection parameters.
+     */
+    public LcsimCollectionData(LcsimCollection<T> params) {
+        super(params.getCollectionName(), params.getProductionDriver(), params.getObjectType(), params.getGlobalTimeDisplacement());
+        setPersistent(params.isPersistent());
+        setFlags(params.getFlags());
+        setReadoutName(params.getReadoutName());
+        setWindowBefore(params.getWindowBefore());
+        setWindowAfter(params.getWindowAfter());
+        
+        this.data = new LinkedList<TimedList<?>>();
+    }
+    
+    /**
+     * Creates a new <code>LcsimCollectionData</code> based on the
+     * collection parameters defined by the <code>params</code>
+     * object and a custom time displacement.
+     * @param params - The collection parameters.
+     * @param timeDisplacement - The time displacement for the
+     * collection. This overrides any values set in
+     * <code>params</code>.
+     */
+    public LcsimCollectionData(LcsimCollection<T> params, double timeDisplacement) {
+        super(params.getCollectionName(), params.getProductionDriver(), params.getObjectType(), timeDisplacement);
+        setPersistent(params.isPersistent());
+        setFlags(params.getFlags());
+        setReadoutName(params.getReadoutName());
+        setWindowBefore(params.getWindowBefore());
+        setWindowAfter(params.getWindowAfter());
+        
+        this.data = new LinkedList<TimedList<?>>();
+    }
+    
+    /**
+     * Gets the collection data.
+     * @return Returns the collection data. Collection data is stored
+     * in a single list, which itself contains {@link
+     * org.hps.readout.util.TimedList TimedList} objects. Each
+     * <code>TimedList</code> object represents the collection data
+     * generated at the simulation time of the list, which can be
+     * obtained through the method {@link
+     * org.hps.readout.util.TimedList#getTime() TimedList.getTime()}.
+     */
+    public LinkedList<TimedList<?>> getData() {
+        return data;
+    }
 }
