3b00debb60307f77d2385dbf1661ff83edcde1f6
mccaky <mccaky@gmail.com>
2018-02-09 19:57:00 -0500

    Removed tabs.

--- record-util/src/main/java/org/hps/readout/util/LcsimSingleEventCollectionData.java
+++ record-util/src/main/java/org/hps/readout/util/LcsimSingleEventCollectionData.java
@@ -14,34 +14,34 @@ import java.util.List;
  * @param <T> - The object type of the data stored by the collection.
  */
 public class LcsimSingleEventCollectionData<T> extends LcsimCollection<T> {
-	/**
-	 * The collection data.
-	 */
-	private final List<T> data;
-	
-	/**
-	 * Creates a new <code>LcsimSingleEventCollectionData</code
-	 * based on the collection parameters defined by the
-	 * <code>params</code> object.
-	 * @param params - The collection parameters.
-	 */
-	public LcsimSingleEventCollectionData(LcsimCollection<T> params) {
-		super(params.getCollectionName(), params.getProductionDriver(), params.getObjectType(), params.getGlobalTimeDisplacement());
-		setPersistent(params.isPersistent());
-		setFlags(params.getFlags());
-		setReadoutName(params.getReadoutName());
-		setWindowBefore(params.getWindowBefore());
-		setWindowAfter(params.getWindowAfter());
-		
-		this.data = new ArrayList<T>();
-	}
-	
-	/**
-	 * Gets the collection data.
-	 * @return Returns the collection data as a {@link java.util.List
-	 * List}.
-	 */
-	public List<T> getData() {
-		return data;
-	}
+    /**
+     * The collection data.
+     */
+    private final List<T> data;
+    
+    /**
+     * Creates a new <code>LcsimSingleEventCollectionData</code
+     * based on the collection parameters defined by the
+     * <code>params</code> object.
+     * @param params - The collection parameters.
+     */
+    public LcsimSingleEventCollectionData(LcsimCollection<T> params) {
+        super(params.getCollectionName(), params.getProductionDriver(), params.getObjectType(), params.getGlobalTimeDisplacement());
+        setPersistent(params.isPersistent());
+        setFlags(params.getFlags());
+        setReadoutName(params.getReadoutName());
+        setWindowBefore(params.getWindowBefore());
+        setWindowAfter(params.getWindowAfter());
+        
+        this.data = new ArrayList<T>();
+    }
+    
+    /**
+     * Gets the collection data.
+     * @return Returns the collection data as a {@link java.util.List
+     * List}.
+     */
+    public List<T> getData() {
+        return data;
+    }
 }
