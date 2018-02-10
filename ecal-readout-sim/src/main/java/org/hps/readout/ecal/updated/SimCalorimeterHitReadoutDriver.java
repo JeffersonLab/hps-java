067d5c93fb48e533898766e7254bb098de5b3ccd
mccaky <mccaky@gmail.com>
2018-02-09 19:59:51 -0500

    Removed tabs.

--- ecal-readout-sim/src/main/java/org/hps/readout/ecal/updated/SimCalorimeterHitReadoutDriver.java
+++ ecal-readout-sim/src/main/java/org/hps/readout/ecal/updated/SimCalorimeterHitReadoutDriver.java
@@ -13,23 +13,23 @@ import org.lcsim.event.SimCalorimeterHit;
  * @see org.hps.readout.SLICDataReadoutDriver
  */
 public class SimCalorimeterHitReadoutDriver extends SLICDataReadoutDriver<SimCalorimeterHit> {
-	/**
-	 * Instantiate an instance of {@link
-	 * org.hps.readout.SLICDataReadoutDriver SLICDataReadoutDriver}
-	 * for objects of type {@link
-	 * org.lcsim.event.SimCalorimeterHit SimCalorimeterHit} and set
-	 * the appropriate LCIO flags.
-	 */
-	public SimCalorimeterHitReadoutDriver() {
-		super(SimCalorimeterHit.class, 0xe0000000);
-	}
-	
-	@Override
-	protected void writeData(java.util.List<SimCalorimeterHit> data) {
-		writer.write("Event ??? - " + ReadoutDataManager.getCurrentTime());
-		writer.write("Output");
-		for(SimCalorimeterHit hit : data) {
-			writer.write(String.format("%f;%f;%d", hit.getRawEnergy(), hit.getTime(), hit.getCellID()));
-		}
-	}
+    /**
+     * Instantiate an instance of {@link
+     * org.hps.readout.SLICDataReadoutDriver SLICDataReadoutDriver}
+     * for objects of type {@link
+     * org.lcsim.event.SimCalorimeterHit SimCalorimeterHit} and set
+     * the appropriate LCIO flags.
+     */
+    public SimCalorimeterHitReadoutDriver() {
+        super(SimCalorimeterHit.class, 0xe0000000);
+    }
+    
+    @Override
+    protected void writeData(java.util.List<SimCalorimeterHit> data) {
+        writer.write("Event ??? - " + ReadoutDataManager.getCurrentTime());
+        writer.write("Output");
+        for(SimCalorimeterHit hit : data) {
+            writer.write(String.format("%f;%f;%d", hit.getRawEnergy(), hit.getTime(), hit.getCellID()));
+        }
+    }
 }
