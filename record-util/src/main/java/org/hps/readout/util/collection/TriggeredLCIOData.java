801ce09ed0de552823c56ba27f86f6b0539e2fd3
mccaky <mccaky@gmail.com>
2018-02-13 17:23:01 -0500

    Updated to support new superclass.

--- record-util/src/main/java/org/hps/readout/util/collection/TriggeredLCIOData.java
+++ record-util/src/main/java/org/hps/readout/util/collection/TriggeredLCIOData.java
@@ -12,16 +12,11 @@ import java.util.List;
  * @author Kyle McCarty <mccarty@jlab.org>
  * @param <T> - The object type of the data stored by the collection.
  */
-public class TriggeredLCIOData<T> {
+public class TriggeredLCIOData<T> extends LCIOData<T, LCIOCollection<T>, List<T>> {
     /**
      * The collection data.
      */
     private final List<T> data;
-    /**
-     * Stores the collection parameters for both writing the data to
-     * an LCIO file and also managing it in the readout data manager.
-     */
-    private final LCIOCollection<T> params;
     
     /**
      * Creates a new <code>TriggeredLCIOData</code based on the
@@ -30,26 +25,44 @@ public class TriggeredLCIOData<T> {
      * @param params - The collection parameters.
      */
     public TriggeredLCIOData(LCIOCollection<T> params) {
-        this.params = params;
+        super(params);
         this.data = new ArrayList<T>();
     }
     
     /**
-     * Gets the collection parameters for the data.
-     * @return Returns the collection parameters as a {@link
-     * org.hps.readout.util.collection.LCIOCollection
-     * LCSimCollection} object.
+     * Creates a copy of this object with no data stored.
+     * @return Returns a duplicate of this object with the same
+     * parameters, but no stored data.
      */
-    public LCIOCollection<T> getCollectionParameters() {
-        return params;
+    public TriggeredLCIOData<T> cloneEmpty() {
+        return new TriggeredLCIOData<T>(getCollectionParameters());
+    }
+    
+    @Override
+    public List<T> getData() {
+        return data;
     }
     
     /**
-     * Gets the collection data.
-     * @return Returns the collection data as a {@link java.util.List
-     * List}.
+     * Merges the data set of the argument list into this list.
+     * @param dataList - The data set to be merged into this list.
+     * @throws IllegalArgumentException Occurs if the object type of
+     * the argument list is not compatible with the data type stored
+     * in this list.
      */
-    public List<T> getData() {
-        return data;
+    @SuppressWarnings("unchecked")
+    public void mergeDataList(TriggeredLCIOData<?> dataList) throws IllegalArgumentException {
+        // Verify that the object types are the same.
+        if(!getCollectionParameters().getObjectType().isAssignableFrom(dataList.getCollectionParameters().getObjectType())) {
+            throw new IllegalArgumentException("Error: Can not merge data set of class " + dataList.getCollectionParameters().getObjectType().getSimpleName()
+                    + " into data set of type " + getCollectionParameters().getObjectType().getSimpleName() + ".");
+        }
+        
+        // Otherwise, combine the data sets. This cast is safe, since
+        // we already verified that both lists contain objects of the
+        // same type (or a subclass thereof).
+        for(Object obj : dataList.getData()) {
+            getData().add((T) obj);
+        }
     }
 }
