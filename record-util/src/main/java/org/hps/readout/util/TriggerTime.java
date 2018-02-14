746f6cbfa491bd4e75d706dfbb237cf275309819
mccaky <mccaky@gmail.com>
2018-02-14 00:30:42 -0500

    Removed now-unneeded trigger write time parameter. This function is now handled by the data manager directly.

--- record-util/src/main/java/org/hps/readout/util/TriggerTime.java
+++ record-util/src/main/java/org/hps/readout/util/TriggerTime.java
@@ -4,9 +4,8 @@ import org.hps.readout.ReadoutDriver;
 
 /**
  * Class <code>TriggerTime</code> stores the time at which a trigger
- * occurs in terms of simulation time, and also the time at which a
- * trigger should be written to an output file. Additionally, the
- * triggering driver is also tracked.
+ * occurs in terms of simulation time, and also the triggering driver
+ * is tracked.
  * 
  * @author Kyle McCarty <mccarty@jlab.org>
  */
@@ -20,31 +19,21 @@ public class TriggerTime implements Comparable<TriggerTime> {
      * The driver that produced the trigger.
      */
     private final ReadoutDriver trigger;
-    /**
-     * The time at which the data manager should write the trigger to
-     * the output file.
-     */
-    private final double writeTime;
     
     /**
      * Instantiates a new <code>TriggerTime</code> object.
      * @param time - The simulation time (corrected for time offsets)
      * at which a trigger occurs.
-     * @param writeTime - The time at which the data manager should
-     * write the trigger to the output file.
      * @param trigger - The driver that produced the trigger.
      */
-    public TriggerTime(double time, double writeTime, ReadoutDriver trigger) {
+    public TriggerTime(double time, ReadoutDriver trigger) {
         this.time = time;
-        this.writeTime = writeTime;
         this.trigger = trigger;
     }
     
     @Override
     public int compareTo(TriggerTime tt) {
-        if(writeTime != tt.writeTime) {
-            return Double.compare(writeTime, tt.writeTime);
-        } else if(time != tt.time) {
+        if(time != tt.time) {
             return Double.compare(time, tt.time);
         } else {
             return 0;
@@ -69,16 +58,5 @@ public class TriggerTime implements Comparable<TriggerTime> {
      */
     public double getTriggerTime() {
         return time;
-    }
-    
-    /**
-     * Gets the simulation time at which the {@link
-     * org.hps.readout.ReadoutDataManager ReadoutDataManager} should
-     * write the readout event to the output LCIO file.
-     * @return Returns the write time for this trigger in units of
-     * nanoseconds.
-     */
-    public double getTriggerWriteTime() {
-        return writeTime;
     }
 }
