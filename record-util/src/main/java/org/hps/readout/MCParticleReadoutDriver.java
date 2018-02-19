114326b1394fdc9629904dad19b012198d82d520
mccaky <mccaky@gmail.com>
2018-02-18 20:18:56 -0500

    Updated MCParticle truth handler driver. MCParticles are now reassigned a new time with respect to the time of their respective beam bunches. This makes it possible to determine from which bunch a given MCParticle originated after readout, and establish if a given two particles are from the same event.

--- record-util/src/main/java/org/hps/readout/MCParticleReadoutDriver.java
+++ record-util/src/main/java/org/hps/readout/MCParticleReadoutDriver.java
@@ -1,6 +1,11 @@
 package org.hps.readout;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.lcsim.event.EventHeader;
 import org.lcsim.event.MCParticle;
+import org.lcsim.lcio.SIOMCParticle;
 
 /**
  * <code>MCParticleReadoutDriver</code> handles SLIC objects in input
@@ -20,5 +25,31 @@ public class MCParticleReadoutDriver extends SLICDataReadoutDriver<MCParticle> {
     public MCParticleReadoutDriver() {
         super(MCParticle.class);
         setCollectionName("MCParticle");
+    }
+    
+    @Override
+    public void process(EventHeader event) {
+        // Get the MCParticles.
+        List<MCParticle> particles = null;
+        if(event.hasCollection(MCParticle.class, "MCParticle")) {
+            particles = event.get(MCParticle.class, "MCParticle");
+        } else {
+            particles = new ArrayList<MCParticle>(0);
+        }
+        
+        // Modify the particle times.
+        double currentTime = ReadoutDataManager.getCurrentTime();
+        for(MCParticle particle : particles) {
+            if(SIOMCParticle.class.isAssignableFrom(particle.getClass())) {
+                SIOMCParticle baseParticle = SIOMCParticle.class.cast(particle);
+                baseParticle.setTime(baseParticle.getProductionTime() + currentTime);
+            } else {
+                throw new RuntimeException("Error: Saw unexpected MC particle class \"" + particle.getClass().getSimpleName() + "\".");
+            }
+        }
+        
+        // Run the superclass method to store the particle collection
+        // in the data manager.
+        super.process(event);
     }
 }
