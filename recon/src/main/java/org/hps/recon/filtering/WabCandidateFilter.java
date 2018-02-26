cb8ee83ec51748d25c4a48a076474411a60f01e8
Graf <ngraf@slac.stanford.edu>
2018-02-26 13:56:21 -0800

    Driver to select WAB candidates

---
+++ recon/src/main/java/org/hps/recon/filtering/WabCandidateFilter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hps.recon.filtering;
+
+/**
+ *
+ * @author ngraf
+ */
+public class WabCandidateFilter {
+    
+}
