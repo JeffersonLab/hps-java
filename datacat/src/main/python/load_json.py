807d67b9ba4c65130f74dda7a73426bdf1e90595
Jeremy McCormick <jermccormick@gmail.com>
2018-03-09 18:59:09 -0500

    Catch and print exceptions.

--- datacat/src/main/python/load_json.py
+++ datacat/src/main/python/load_json.py
@@ -20,14 +20,17 @@ data = json.loads(json_data)
 client = client_from_config_file(path=os.getcwd() + "/default.cfg")
 
 for entry in data['datacat']:
-    ds = client.mkds(
-            entry['folder'],
-            entry['name'],
-            entry['data_type'],
-            entry['data_format'],
-            site=entry['site'],
-            resource=entry['resource'],
-            versionMetadata=entry['metadata'])
-    print(repr(ds))
+    try: 
+        ds = client.mkds(
+                entry['folder'],
+                entry['name'],
+                entry['data_type'],
+                entry['data_format'],
+                site=entry['site'],
+                resource=entry['resource'],
+                versionMetadata=entry['metadata'])
+        print(repr(ds))
+    except Exception as e:
+        print e
     if DEBUG:
         break
