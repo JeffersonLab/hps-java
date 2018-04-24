ff3a9bff680d123636006c7599a81604978cb73b
Graf <ngraf@slac.stanford.edu>
2018-04-23 17:14:02 -0700

    prepping profile plots for fitting

--- analysis/src/main/java/org/hps/analysis/alignment/MollerSvtAlignmentAnalyzer.java
+++ analysis/src/main/java/org/hps/analysis/alignment/MollerSvtAlignmentAnalyzer.java
@@ -1,6 +1,16 @@
 package org.hps.analysis.alignment;
 
 import hep.aida.IAnalysisFactory;
+import hep.aida.IDataPointSet;
+import hep.aida.IDataPointSetFactory;
+import hep.aida.IFitData;
+import hep.aida.IFitFactory;
+import hep.aida.IFitResult;
+import hep.aida.IFitter;
+import hep.aida.IFunction;
+import hep.aida.IFunctionFactory;
+import hep.aida.IPlotter;
+import hep.aida.IProfile1D;
 import hep.aida.ITree;
 import java.io.File;
 import java.io.IOException;
@@ -18,16 +28,64 @@ public class MollerSvtAlignmentAnalyzer {
         String rootDir = null;
         String plotFile = "D:/work/hps/analysis/mollerAlignment/2015_MollerSkim_pass8_PC.aida";
         // Get the plots file and open it.
-        IAnalysisFactory af = IAnalysisFactory.create();
-        ITree tree = af.createTreeFactory().create(new File(plotFile).getAbsolutePath());
+        IAnalysisFactory analysisFactory = IAnalysisFactory.create();
+//        IPlotter plotter = analysisFactory.createPlotterFactory().create("Fit.java Plot");
+        ITree tree = analysisFactory.createTreeFactory().create(new File(plotFile).getAbsolutePath());
+        IFunctionFactory functionFactory = analysisFactory.createFunctionFactory(tree);
+        IFitFactory fitFactory = analysisFactory.createFitFactory();
+        IFitter fitter = fitFactory.createFitter("Chi2", "jminuit");
+        IDataPointSetFactory dpsf = analysisFactory.createDataPointSetFactory(tree);
+
         if (tree == null) {
             throw new IllegalArgumentException("Unable to load plot file.");
         }
         // Get the histograms names.
         List<String> objectNameList = getTreeFiles(tree);
-        for(String s : objectNameList)
-        {
-            System.out.println(s);
+
+        for (String s : objectNameList) {
+            IProfile1D prof = null;
+            if (s.contains("Profile")) {
+                System.out.println(s);
+                prof = (IProfile1D) tree.find(s);
+            }
+            if (prof != null) {
+                IPlotter plotter = analysisFactory.createPlotterFactory().create("Fit.java Plot");
+                plotter.createRegions(1, 2);
+                plotter.region(0).plot(prof);
+
+                IFunction line = functionFactory.createFunctionByName("line", "p1");
+
+                IFitData fitData = fitFactory.createFitData();
+                fitData.create1DConnection(prof);
+                fitData.range(0).excludeAll();
+                fitData.range(0).include(-0.4, 0.4);
+
+                IFitResult result = fitter.fit(fitData, line);
+                plotter.region(0).plot(result.fittedFunction());
+
+                System.out.println("Chi2=" + result.quality());
+
+                // now try a data point set
+                IDataPointSet profDataPointSet = dpsf.create("dpsFromProf", prof);
+                //plotter.region(1).plot(profDataPointSet);
+                // seems to be exactly the same
+                //manually set uncertainties to be the error on the mean (not the rms)
+                System.out.println(" profile plot has " + prof.axis().bins() + " bins");
+                for (int i = 0; i < prof.axis().bins(); ++i) {
+//            System.out.println("bin "+i+" error "+prof.binError(i)+" rms "+prof.binRms(i));
+                    profDataPointSet.point(i).coordinate(1).setErrorPlus(prof.binError(i));
+                    profDataPointSet.point(i).coordinate(1).setErrorMinus(prof.binError(i));
+                }
+
+                IFitData fitData1 = fitFactory.createFitData();
+                fitData1.create1DConnection(profDataPointSet,0,1);
+                fitData1.range(0).excludeAll();
+                fitData1.range(0).include(-0.4, 0.4);
+                plotter.region(1).plot(profDataPointSet);
+                IFitResult result2 = fitter.fit(fitData1, line);
+                plotter.region(1).plot(result2.fittedFunction());
+                plotter.show();
+            }
         }
     }
 
