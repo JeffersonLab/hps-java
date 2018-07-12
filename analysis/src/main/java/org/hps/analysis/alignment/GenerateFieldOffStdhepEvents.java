d7523564432a99094533f0ed3231b5214bcdebcd
Graf <ngraf@slac.stanford.edu>
2018-07-12 15:24:00 -0700

    Simple program to generate single electrons uniformly distributed over ECal face

---
+++ analysis/src/main/java/org/hps/analysis/alignment/GenerateFieldOffStdhepEvents.java
@@ -0,0 +1,112 @@
+package org.hps.analysis.alignment;
+
+import hep.io.stdhep.StdhepEndRun;
+import hep.io.stdhep.StdhepEvent;
+import hep.io.stdhep.StdhepWriter;
+import java.io.IOException;
+import static java.lang.Math.sqrt;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Generate single electrons for field-off straight track alignment studies.
+ *
+ * @author ngraf
+ */
+public class GenerateFieldOffStdhepEvents {
+
+    public static void main(String[] args) throws IOException {
+ /*--------------------------------------------------------*/
+ /* NEVHEP          - event number (or some special meaning*/
+ /*                    (see documentation for details)     */
+ /* NHEP            - actual number of entries in current  */
+ /*                    event.                              */
+ /* ISTHEP[IHEP]    - status code for IHEP'th entry - see  */
+ /*                    documentation for details           */
+ /* IDHEP [IHEP]    - IHEP'th particle identifier according*/
+ /*                    to PDG.                             */
+ /* JMOHEP[IHEP][0] - pointer to position of 1st mother    */
+ /* JMOHEP[IHEP][1] - pointer to position of 2nd mother    */
+ /* JDAHEP[IHEP][0] - pointer to position of 1st daughter  */
+ /* JDAHEP[IHEP][1] - pointer to position of 2nd daughter  */
+ /* PHEP  [IHEP][0] - X momentum [Gev/c]                   */
+ /* PHEP  [IHEP][1] - Y momentum [Gev/c]                   */
+ /* PHEP  [IHEP][2] - Z momentum [Gev/c]                   */
+ /* PHEP  [IHEP][3] - Energy [Gev]                         */
+ /* PHEP  [IHEP][4] - Mass[Gev/c^2]                        */
+ /* VHEP  [IHEP][0] - X vertex [mm]                        */
+ /* VHEP  [IHEP][1] - Y vertex [mm]                        */
+ /* VHEP  [IHEP][2] - Z vertex [mm]                        */
+ /* VHEP  [IHEP][3] - production time [mm/c]               */
+ /*========================================================*/
+
+//IstHep convention:
+//                            0      - final state particle if JdaHEP=0
+//                                     intermediate particle if JdaHEP>0
+//                                     (NEUGEN extension; was "null")
+//                            1      - final state particle
+//                            2      - intermediate state
+//                            3      - documentation line
+//                            4-10   - reserved for future
+//                            11-200 - reserved for specific model use
+//                            201+   - reserved for users
+        int isthep[] = {0};
+        int idhep[] = {11}; // electron
+        int jmohep[] = {0, 0};
+        int jdahep[] = {0, 0};
+        double phep[] = new double[5];
+        double vhep[] = {0., 0., -2338., 0.};
+        double emass = 0.000511;
+        double mass2 = emass * emass;
+        double eEnergy = 2.3;
+
+        // eCal dimensions
+        double xMax = 360.;
+        double xMin = -280.;
+        double yMin = 20;
+        double yMax = 90.;
+
+        double z = 1390 + 2338; // ECal face + HARP wire location
+        double p = sqrt(eEnergy * eEnergy - mass2);
+
+        int nEvents = 100;
+        Random ran = new Random();
+        StdhepWriter topEvents = new StdhepWriter("hpsForwardFullEnergyElectrons_z-2338_top.stdhep", "Generated Stdhep Events", "top", 250000);
+        StdhepWriter bottomEvents = new StdhepWriter("hpsForwardFullEnergyElectrons_z-2338_bottom.stdhep", "Generated Stdhep Events", "bottom", 250000);
+
+        topEvents.setCompatibilityMode(false);
+        bottomEvents.setCompatibilityMode(false);
+        // evenly populate face of ECal
+        for (int i = 0; i < nEvents; ++i) {
+            double x = ThreadLocalRandom.current().nextDouble(xMin, xMax);
+            double y = ThreadLocalRandom.current().nextDouble(yMin, yMax);
+            double r = sqrt(x * x + y * y + z * z);
+            phep[0] = p * x / r; //px
+            phep[1] = p * y / r; //py
+            phep[2] = p * z / r; //pz
+            phep[3] = eEnergy;
+            phep[4] = emass;
+
+            StdhepEvent ev = new StdhepEvent(i, 1, isthep, idhep, jmohep, jdahep, phep, vhep);
+            topEvents.writeRecord(ev);
+            phep[1] = -phep[1];
+            ev = new StdhepEvent(i, 1, isthep, idhep, jmohep, jdahep, phep, vhep);
+            bottomEvents.writeRecord(ev);
+        }
+
+        int nreqe = (int) nEvents;
+        int ngene = (int) nEvents;
+        int nwrite = (int) nEvents;
+        float ecme = (float) 2.3;
+        float xsece = (float) 99999997952.;
+        double rn1e = 12345321;
+        double rn2e = 66666666;
+        StdhepEndRun se = new StdhepEndRun(nreqe, ngene, nwrite, ecme, xsece, rn1e, rn2e);
+        topEvents.writeRecord(se);
+        topEvents.close();
+
+        bottomEvents.writeRecord(se);
+        bottomEvents.close();
+
+    }
+}
