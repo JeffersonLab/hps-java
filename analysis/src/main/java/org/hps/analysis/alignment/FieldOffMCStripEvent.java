9da7a5b9f243e781784b70a8dcb79d77c57a0e4f
Graf <ngraf@slac.stanford.edu>
2018-07-12 13:15:40 -0700

    testing

--- analysis/src/main/java/org/hps/analysis/alignment/FieldOffMCStripEvent.java
+++ analysis/src/main/java/org/hps/analysis/alignment/FieldOffMCStripEvent.java
@@ -39,8 +39,8 @@ public class FieldOffMCStripEvent extends Driver {
                 bottomLayers.add(layer);
             }
         }
-//        System.out.println("top has " + topLayers.size() + " hits");
-//        System.out.println("bottom has " + bottomLayers.size() + " hits");
+        System.out.println("top has " + topLayers.size() + " hits");
+        System.out.println("bottom has " + bottomLayers.size() + " hits");
 
         if (_selectTopTracks && topLayers.size() != 12) {
             skipEvent = true;
@@ -49,13 +49,13 @@ public class FieldOffMCStripEvent extends Driver {
         if (_selectBottomTracks && bottomLayers.size() != 12) {
             skipEvent = true;
         }
-//        System.out.println("skipEvent "+skipEvent);
+        System.out.println("skipEvent "+skipEvent);
         if (skipEvent) {
             throw new Driver.NextEventException();
         } else {
             _numberOfEventsWritten++;
         }
- //       System.out.println(_numberOfEventsWritten);
+        System.out.println(_numberOfEventsWritten);
     }
 
     protected void endOfData() {
