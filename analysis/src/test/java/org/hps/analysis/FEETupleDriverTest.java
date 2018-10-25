53cd8f18c0faa38850fb269cf37b2b35343bf322
Miriam Diamond <mdiamond@slac.stanford.edu>
2018-10-25 13:48:57 -0700

    tuple unit test updates

--- analysis/src/test/java/org/hps/analysis/FEETupleDriverTest.java
+++ analysis/src/test/java/org/hps/analysis/FEETupleDriverTest.java
@@ -6,8 +6,8 @@ public class FEETupleDriverTest extends TupleDriverTest {
     @Override
     public void testIt() throws Exception {
          testURLBase = "http://www.lcsim.org/test/hps-java/";
-         txtRefFileName = "ntuple_005772_fee_Ref.txt";
-         lcioInputFileName = "hps_005772.0_recon_Rv4657-0-10000.slcio";
+         txtRefFileName = "ntuple_007796_fee_Ref.txt";
+         lcioInputFileName = "PhysRun2016FeeReconTest.slcio";
          txtOutputFileName = "target/test-output/out_fee.txt";
 
          testTupleDriver = new org.hps.analysis.tuple.FEETupleDriver();
