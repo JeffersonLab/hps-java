84d10784ace3b2643af21d8816dfdf5168c107fe
Jeremy McCormick <jermccormick@gmail.com>
2019-04-15 12:34:01 +0700

    Add util method to get scint dims.

--- detector-model/src/main/java/org/hps/detector/hodoscope/HodoscopeDetectorElement.java
+++ detector-model/src/main/java/org/hps/detector/hodoscope/HodoscopeDetectorElement.java
@@ -2,10 +2,18 @@ package org.hps.detector.hodoscope;
 
 import org.lcsim.detector.IDetectorElement;
 import org.lcsim.detector.converter.compact.SubdetectorDetectorElement;
+import org.lcsim.detector.solids.Box;
+import org.lcsim.event.SimTrackerHit;
 
 public final class HodoscopeDetectorElement extends SubdetectorDetectorElement{
                            
     public HodoscopeDetectorElement(String name, IDetectorElement parent) {
         super(name, parent);
+    }
+    
+    public double[] getScintillatorHalfDimensions(SimTrackerHit hit) {
+        IDetectorElement idDetElem = findDetectorElement(hit.getIdentifier()).get(0);
+        Box box = (Box) idDetElem.getGeometry().getLogicalVolume().getSolid();
+        return new double[] { box.getXHalfLength(), box.getYHalfLength(), box.getZHalfLength() };
     }
 }
