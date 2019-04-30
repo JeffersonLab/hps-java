b846e80fa63711781e6223233affb17f03b313c6
Graf <ngraf@slac.stanford.edu>
2019-04-30 13:41:49 -0700

    Update DetectorBuilder.java
    Added ability to dump the SVT sensor plane geometry to 3D.
    For some reason default v seems already to be in the +x direction...

--- analysis/src/main/java/org/hps/analysis/alignment/straighttrack/DetectorBuilder.java
+++ analysis/src/main/java/org/hps/analysis/alignment/straighttrack/DetectorBuilder.java
@@ -3,6 +3,7 @@ package org.hps.analysis.alignment.straighttrack;
 import Jama.Matrix;
 import hep.physics.vec.Hep3Vector;
 import hep.physics.vec.VecOp;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -22,6 +23,13 @@ import org.hps.recon.tracking.MaterialSupervisor;
 import org.hps.recon.tracking.MaterialSupervisor.ScatteringDetectorVolume;
 import org.hps.recon.tracking.MaterialSupervisor.SiStripPlane;
 import org.lcsim.conditions.ConditionsManager;
+import org.lcsim.detector.ILogicalVolume;
+import org.lcsim.detector.IPhysicalVolume;
+import org.lcsim.detector.IPhysicalVolumeContainer;
+import org.lcsim.detector.material.IMaterial;
+import org.lcsim.detector.solids.Box;
+import org.lcsim.detector.tracker.silicon.HpsSiSensor;
+import org.lcsim.detector.tracker.silicon.SiTrackerModule;
 import org.lcsim.geometry.Detector;
 
 /**
@@ -35,6 +43,10 @@ public class DetectorBuilder {
     Map<String, SiStripPlane> stripPlaneNameMap = new HashMap<String, SiStripPlane>();
     Map<String, List<DetectorPlane>> trackerMap = new HashMap<String, List<DetectorPlane>>();
     Map<String, DetectorPlane> planeMap = new HashMap<String, DetectorPlane>();
+
+    MaterialSupervisor materialManager;
+
+    Boolean _drawDetector = true;
 
     // should be enums?
     public enum Tracker {
@@ -138,7 +150,7 @@ public class DetectorBuilder {
         sensorNameMap.put("bottomHole", bottomHoleNames);
         sensorNameMap.put("topSlot", topSlotNames);
         sensorNameMap.put("bottomSlot", bottomSlotNames);
-        MaterialSupervisor materialManager = new MaterialSupervisor();
+        materialManager = new MaterialSupervisor();
         materialManager.buildModel(_det);
 
         for (ScatteringDetectorVolume vol : materialManager.getMaterialVolumes()) {
@@ -213,6 +225,257 @@ public class DetectorBuilder {
     public List<DetectorPlane> getTracker(String trackerName) {
         return trackerMap.get(trackerName);
     }
+    
+    public void drawDetector()
+    {
+        drawDetector(_det);
+    }
+
+    private void drawDetector(Detector det) {
+        boolean debug = false;
+        List<MaterialSupervisor.ScatteringDetectorVolume> stripPlanes = materialManager.getMaterialVolumes();
+        //TODO replace these lists with a helper class.
+        List<Hep3Vector> oList = new ArrayList<Hep3Vector>();
+        List<Hep3Vector> uList = new ArrayList<Hep3Vector>();
+        List<Hep3Vector> vList = new ArrayList<Hep3Vector>();
+        List<Double> measDim = new ArrayList<Double>();
+        List<Double> unmeasDim = new ArrayList<Double>();
+        List<Boolean> isAxial = new ArrayList<Boolean>();
+
+        for (MaterialSupervisor.ScatteringDetectorVolume vol : stripPlanes) {
+            MaterialSupervisor.SiStripPlane plane = (MaterialSupervisor.SiStripPlane) vol;
+            if (debug) {
+                System.out.println(plane.getName());
+            }
+            Hep3Vector oprime = CoordinateTransformations.transformVectorToDetector(plane.origin());
+            Hep3Vector nprime = CoordinateTransformations.transformVectorToDetector(plane.normal());
+            if (debug) {
+                System.out.println(" origin: " + oprime);
+            }
+            if (debug) {
+                System.out.println(" normal: " + nprime);
+            }
+            if (debug) {
+                System.out.println(" Plane is: " + plane.getMeasuredDimension() + " x " + plane.getUnmeasuredDimension());
+            }
+            HpsSiSensor sensor = (HpsSiSensor) plane.getSensor();
+
+//            if (debug) {
+//                System.out.println(SvtUtils.getInstance().isAxial(sensor) ? "axial" : "stereo");
+//            }
+            Hep3Vector unmeasDir = CoordinateTransformations.transformVectorToDetector(plane.getUnmeasuredCoordinate());
+            // by default, the HPS unmeasDir points along -x, so invert here...
+            // what!?
+//            unmeasDir = VecOp.neg(unmeasDir);
+            if (debug) {
+                System.out.println("unmeasured coordinate:   " + unmeasDir);
+            }
+            Hep3Vector measDir = CoordinateTransformations.transformVectorToDetector(plane.getMeasuredCoordinate());
+            //measDir points either up or down depending on orientation of the sensor. I want it always to pint up, along y.
+            // so let's check something here...
+            Hep3Vector tst = VecOp.cross(unmeasDir, measDir);
+            // if pointing along the z axis, OK. if not, invert measDir...
+            if (tst.z() < 0.) {
+                measDir = VecOp.neg(measDir);
+            }
+            if (debug) {
+                System.out.println("measured coordinate:    " + measDir);
+            }
+
+            if (debug) {
+                System.out.println("thickness: " + plane.getThickness() + " in X0: " + plane.getThicknessInRL());
+            }
+            SiTrackerModule module = (SiTrackerModule) plane.getSensor().getGeometry().getDetectorElement().getParent();
+            IPhysicalVolume parent = module.getGeometry().getPhysicalVolume();
+            IPhysicalVolumeContainer daughters = parent.getLogicalVolume().getDaughters();
+            if (debug) {
+                System.out.printf("%s found %d daughters to SiTrackerModule\n", this.getClass().getSimpleName(), daughters.size());
+            }
+            for (IPhysicalVolume daughter : daughters) {
+                ILogicalVolume logicalVolume = daughter.getLogicalVolume();
+                IMaterial material = logicalVolume.getMaterial();
+                //System.out.println(material);
+                String name = material.getName();
+                double X0 = 10.0 * material.getRadiationLength() / material.getDensity();
+                double X0cm = material.getRadiationLengthWithDensity();
+                if (debug) {
+                    System.out.println("material: " + name + " with X0= " + X0 + " mm " + X0cm);
+                }
+                Box solid = (Box) logicalVolume.getSolid();
+                if (debug) {
+                    System.out.printf("%s x %f y %f z %f box\n", this.getClass().getSimpleName(), solid.getXHalfLength(), solid.getYHalfLength(), solid.getZHalfLength());
+                }
+                double halfThickness = solid.getZHalfLength();
+            }
+            oList.add(oprime);
+            uList.add(measDir);
+            vList.add(unmeasDir);
+            measDim.add(plane.getMeasuredDimension());
+            unmeasDim.add(plane.getUnmeasuredDimension());
+            isAxial.add(sensor.isAxial());
+        }
+
+//        // now get ECal
+//        List<List<Hep3Vector>> calcells = new ArrayList<List<Hep3Vector>>();
+//        IIdentifierHelper helper = _api.getIdentifierHelper();
+//        for (EcalCrystal crystal : _api.getCrystals()) {
+//            IGeometryInfo geom = crystal.getGeometry();
+//            Trd trd = (Trd) geom.getLogicalVolume().getSolid();
+//            List<Point3D> vertices = trd.getVertices();
+//            List<Hep3Vector> verts = new ArrayList<Hep3Vector>();
+//            for (Point3D p : vertices) {
+//                verts.add(geom.transformLocalToGlobal(p.getHep3Vector()));
+//            }
+//            calcells.add(verts);
+//            //
+////            IExpandedIdentifier expandedId = helper.createExpandedIdentifier();
+////            expandedId.setValue(helper.getFieldIndex("ix"), crystal.getX());
+////            expandedId.setValue(helper.getFieldIndex("iy"), crystal.getY());
+////            expandedId.setValue(helper.getFieldIndex("system"), _api.getSystemID());
+////            IIdentifier id = helper.pack(expandedId);
+////            if (id.getValue() != crystal.getIdentifier().getValue()) {
+////                throw new RuntimeException("Reencoded ID " + id.getValue() + " does not match crystal ID " + crystal.getIdentifier().getValue());
+////            }
+////
+////            IDetectorElementStore deStore = DetectorElementStore.getInstance();
+////            if (deStore.find(crystal.getIdentifier()).size() == 0) {
+////                throw new RuntimeException("Failed to find crystal ID in store.");
+////            }
+////
+////            if (deStore.find(id).size() == 0) {
+////                throw new RuntimeException("Failed to find repacked ID in store.");
+////            }
+//        }
+//        System.out.println("calcells has " + calcells.size() + " entries");
+//        // test print...
+//        List<Hep3Vector> vs0 = calcells.get(0);
+//        System.out.println("cell 0 has " + vs0.size() + "points");
+//        System.out.println(vs0);
+//        List<Hep3Vector> vs1 = calcells.get(1);
+//        System.out.println("cell 1 has " + vs1.size() + "points");
+//        System.out.println(vs1);
+        // now draw
+        if (_drawDetector) {
+            //TODO extract the following into a separate method
+            System.out.println("\n\n\n");
+            System.out.println("size(600);");
+            System.out.println("import three;");
+            System.out.println("currentprojection=orthographic(");
+            System.out.println("camera=(-1250,850,-1500),");
+            System.out.println("up=(0.374061249845773,1.48257908496856,0.568508408053946),");
+            System.out.println("target=(0,0,0),");
+            System.out.println("zoom=1);");
+
+            System.out.println("");
+            System.out.println("pen fill=gray(0.6)+opacity(0.2);");
+            System.out.println("pen edge=black;");
+            System.out.println("pen axialEdge=red;");
+            System.out.println("pen stereoEdge=blue;");
+            System.out.println("// o is the origin point in the plane, u is the measurement direction, v is the unmeasured direction");
+            System.out.println("// clockwise normal orientation corresponds to z = u x v");
+            System.out.println("//");
+            System.out.println("//     +----------------------+  ^");
+            System.out.println("//     |                      |  |");
+            System.out.println("//     |                      |  u");
+            System.out.println("//     |           o          |  |");
+            System.out.println("//     |                      |   ");
+            System.out.println("//     |                      |   ");
+            System.out.println("//     +----------------------+   ");
+            System.out.println("//               ------ v ---->   ");
+            System.out.println("path3 myPlane(triple o, triple u, triple v)");
+            System.out.println("{");
+            System.out.println("    path3 P;");
+            System.out.println("    P=o+(u+v)--o-u+v--o-u-v--o+u-v--cycle;");
+            System.out.println("    return P;");
+            System.out.println("}");
+            System.out.println(" void drawAxes(triple o, triple u, triple v)");
+            System.out.println(" {");
+            System.out.println("pen p = red;");
+            System.out.println(" draw(Label(\"$u$\",1),(o--o+u),p,Arrow3);");
+            System.out.println("p=green;");
+            System.out.println(" draw(Label(\"$v$\",1),(o--o+v),p,Arrow3);");
+            System.out.println("p=blue;");
+            System.out.println(" draw(Label(\"$w$\",1),(o--o+10.*cross(unit(v),unit(u))),p,Arrow3);");
+            System.out.println(" }");
+
+            // ECal crystal frustum volume
+            // use volumes for hit crystals in event data
+            System.out.println("void drawvolume(triple p1, triple p2, triple p3, triple p4, triple p5, triple p6, triple p7, triple p8, pen surfpen)");
+            System.out.println("{");
+            System.out.println("surface[] surfs;");
+            System.out.println("");
+            System.out.println("surfs.push( surface(p1--p2--p4--p3--cycle) );");
+            System.out.println("surfs.push( surface(p4--p8--p7--p3--cycle) );");
+            System.out.println("surfs.push( surface(p3--p7--p5--p1--cycle) );");
+            System.out.println("surfs.push( surface(p2--p1--p5--p6--cycle) );");
+            System.out.println("surfs.push( surface(p2--p6--p8--p4--cycle) );");
+            System.out.println("surfs.push( surface(p6--p5--p7--p8--cycle) );");
+            System.out.println("");
+            System.out.println("for (int i = 0; i < surfs.length; ++i) {");
+            System.out.println("        draw(surfs[i], surfpen);");
+            System.out.println("    }");
+            System.out.println("");
+            System.out.println("}");
+
+            //ECal crystal frustum edges
+            // Use edges to draw detector
+            // this causes out-of-memory crashes.
+            // keep this here, but don't use it for now.
+            System.out.println("void drawEdges(triple p1, triple p2, triple p3, triple p4, triple p5, triple p6, triple p7, triple p8, pen edgepen)");
+            System.out.println("{");
+            System.out.println("draw(p1--p2--p3--p4--cycle, edgepen);");
+            System.out.println("draw(p2--p6--p7--p3--cycle, edgepen);");
+            System.out.println("draw(p3--p7--p8--p4--cycle, edgepen);");
+            System.out.println("draw(p1--p4--p8--p5--cycle, edgepen);");
+            System.out.println("draw(p1--p5--p6--p2--cycle, edgepen);");
+            System.out.println("draw(p5--p8--p7--p6--cycle, edgepen);");
+            System.out.println("}");
+
+            DecimalFormat df = new DecimalFormat("###.######");
+
+            System.out.println("draw(Label(\"$x$\",1),(O--100.*X),Arrow3(HookHead3));");
+            System.out.println("draw(Label(\"$y$\",1),(O--100.*Y),Arrow3(HookHead3));");
+            System.out.println("draw(Label(\"$z$\",1),(O--1000.*Z),Arrow3(HookHead3));");
+
+            for (int i = 0; i < oList.size(); ++i) {
+                Hep3Vector o = oList.get(i);
+                Hep3Vector u = uList.get(i);
+                Hep3Vector v = vList.get(i);
+                double l = unmeasDim.get(i) / 2.;
+                double h = measDim.get(i) / 2.;
+                //redefine op as lower left corner, viz.
+                // o' = o-(l*V+h*u)
+                // Hep3Vector op = VecOp.sub(o, VecOp.add(VecOp.mult(l, v), VecOp.mult(h, u)));
+                String planeType = isAxial.get(i) ? "axial" : "stereo";
+                System.out.println("//" + planeType);
+                StringBuffer sb = new StringBuffer("draw(surface( myPlane( ");
+                //origin
+                sb.append("( " + df.format(o.x()) + " , " + df.format(o.y()) + " , " + df.format(o.z()) + " ), ");
+                //u coordinate
+                sb.append(h + "*( " + df.format(u.x()) + " , " + df.format(u.y()) + " , " + df.format(u.z()) + " ), ");
+                //v coordinate
+                sb.append(l + "*( " + df.format(v.x()) + " , " + df.format(v.y()) + " , " + df.format(v.z()) + " )  ) ), fill, " + planeType + "Edge); ");
+
+                System.out.println(sb.toString());
+                // sensor axes
+                sb = new StringBuffer("drawAxes( ");
+                //origin
+                sb.append("( " + df.format(o.x()) + " , " + df.format(o.y()) + " , " + df.format(o.z()) + " ), ");
+                //u coordinate
+                sb.append(h + "*( " + df.format(u.x()) + " , " + df.format(u.y()) + " , " + df.format(u.z()) + " ), ");
+                //v coordinate
+                sb.append(l + "*( " + df.format(v.x()) + " , " + df.format(v.y()) + " , " + df.format(v.z()) + " ) ); ");
+
+                System.out.println(sb.toString());
+            }
+            // now the calorimeter...
+//            StringBuffer sbcal = new StringBuffer();
+//            for (List<Hep3Vector> vertices : calcells) {
+//                addcell(sbcal, vertices);
+//            }
+//            System.out.println(sbcal.toString());
+        }
+    }
 
     public static void main(String[] args) {
         DetectorBuilder db = new DetectorBuilder("HPS-EngRun2015-Nominal-v0");
@@ -222,6 +485,8 @@ public class DetectorBuilder {
             System.out.println(p);
             System.out.println("");
         }
+        
+        db.drawDetector();
 
     }
 }
