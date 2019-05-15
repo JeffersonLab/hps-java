75a18e922011239cc0b0159c6155b3190ad75176
Jeremy McCormick <jermccormick@gmail.com>
2019-05-15 12:35:52 +0700

    organize debug print outs; throw an error if no readout hits are found

--- integration-tests/src/test/java/org/hps/test/it/HodoscopeDataConverterTest.java
+++ integration-tests/src/test/java/org/hps/test/it/HodoscopeDataConverterTest.java
@@ -26,12 +26,13 @@ import junit.framework.TestCase;
 
 public class HodoscopeDataConverterTest extends TestCase {
     
-    private static String DETECTOR = "HPS-HodoscopeTest-v1";
-    private static Integer RUN_NUMBER = 1000000;
+    private static final String DETECTOR = "HPS-HodoscopeTest-v1";
+    private static final Integer RUN_NUMBER = 1000000;
     //private static String LOCAL_FILE_PATH = "/work/slac/hps-projects/projects/hodoscope-dev/hpshodo_000322_100evts.evio";
-    private static String OUTPUT_FILE_NAME = "hodo_cnv_test.slcio";
-    private static String TEST_FILE_URL = "http://lcsim.org/test/hps-java/hpshodo_000322_100evts.evio";
-        
+    private static final String OUTPUT_FILE_NAME = "hodo_cnv_test.slcio";
+    private static final String TEST_FILE_URL = "http://lcsim.org/test/hps-java/hpshodo_000322_100evts.evio";
+    private static final boolean DEBUG = true;
+    
     public void testHodoscopeDataConverter() throws IOException {
         FileCache cache = new FileCache();
         File testFile = cache.getCachedFile(new URL(TEST_FILE_URL));
@@ -51,7 +52,7 @@ public class HodoscopeDataConverterTest extends TestCase {
         DatabaseConditionsManager.getInstance();
         loop.setLCIORecordSource(testOutput);
         loop.add(new HodoscopeDataDriver());
-        loop.loop(1);
+        loop.loop(100);
     }    
     
     private class HodoscopeDataDriver extends Driver {
@@ -63,26 +64,40 @@ public class HodoscopeDataConverterTest extends TestCase {
             Subdetector subdet = detector.getSubdetector("Hodoscope");
             this.hodo = (HodoscopeDetectorElement) subdet.getDetectorElement();
             this.helper = hodo.getIdentifierHelper();
+            System.out.println("Hodoscope pixels: ");
             for (IDetectorElement de : hodo.getChildren()) {
                 System.out.println(de.getName() + " " + helper.unpack(de.getIdentifier()));
             }
+            System.out.println();
         }
         
         public void process(EventHeader event) {
+            if (DEBUG)
+                System.out.println(">>>> Processing hodo event " + event.getEventNumber());
             List<RawTrackerHit> hits = event.get(RawTrackerHit.class, "HodoReadoutHits");
+            if (hits.size() == 0) {
+                throw new RuntimeException("HodoReadoutHits is empty!");
+            }
+            if (DEBUG)
+                System.out.println("HodoReadoutHits has " + hits.size() + " hits.");
             for (RawTrackerHit hit : hits) {
                 IIdentifier id = hit.getIdentifier();
                 IExpandedIdentifier vals = helper.unpack(id);
-                System.out.println(vals);
+                if (DEBUG)
+                    System.out.println(vals);
                 ExpandedIdentifier vals_no_hole = new ExpandedIdentifier(vals.size());
                 for (int i = 0; i < vals.size() - 1; i++) {
                     vals_no_hole.setValue(i, vals.getValue(i));
                 }
-                System.out.println(vals_no_hole + " (no hole)");
+                if (DEBUG)
+                    System.out.println(vals_no_hole + " (no hole)");
                 IIdentifier id_no_hole = helper.pack(vals_no_hole);
+                // This will crash if no pixel is found but that is fine.  --JM
                 hit.setDetectorElement(this.hodo.findDetectorElement(id_no_hole).get(0));
-                System.out.println("Hodo hit " + id + " assigned to pixel " + hit.getDetectorElement().getName());
-                System.out.println();
+                if (DEBUG) {
+                    System.out.println("Hodo hit " + id + " assigned to pixel " + hit.getDetectorElement().getName());
+                    System.out.println();
+                }
             }
         }
     }    
