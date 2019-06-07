d14002445f250ec1df39fa4135f565c314a46a51
Jeremy McCormick <jermccormick@gmail.com>
2019-06-07 18:30:42 +0700

    Remove blocking and position parameters as they are set automatically.

--- online-recon/src/main/java/org/hps/online/recon/EtParallelStation.java
+++ online-recon/src/main/java/org/hps/online/recon/EtParallelStation.java
@@ -7,20 +7,22 @@ import org.jlab.coda.et.EtSystem;
 import org.jlab.coda.et.EtSystemOpenConfig;
 import org.jlab.coda.et.enums.Mode;
 
-// Sub-class EtConnection so we don't accidentally screw up the monitoring application.  --JM
+/**
+ * Create a parallel ET connection appropriate for a pool of stations to 
+ * process reconstruction jobs.
+ * 
+ * Sub-class {@link org.hps.record.et.EtConnection} so we don't accidentally 
+ * screw up the monitoring application.
+ */
 public class EtParallelStation extends EtConnection {
-
-    //static Logger LOGGER = Logger.getLogger(EtParallelStation.class.getPackageName());
     
     public EtParallelStation(
             final String name, 
             final String host,
             final int port, 
-            final boolean blocking,
             final int queueSize, 
             final int prescale, 
             final String stationName, 
-            final int stationPosition,
             final Mode waitMode, 
             final int waitTime, 
             final int chunkSize) throws Exception {
@@ -35,29 +37,20 @@ public class EtParallelStation extends EtConnection {
         // configuration of a new station
         final EtStationConfig stationConfig = new EtStationConfig();
         stationConfig.setFlowMode(EtConstants.stationParallel);
-        if (!blocking) {
-            stationConfig.setBlockMode(EtConstants.stationNonBlocking);
-            if (queueSize > 0) {
-                // Is this only applicable for non-blocking???
-                stationConfig.setCue(queueSize);
-            }
-        } else {
-            stationConfig.setBlockMode(EtConstants.stationBlocking);
-            // Need to set queue size???
-        }
+        stationConfig.setBlockMode(EtConstants.stationBlocking);
+
         // Set prescale.
         if (prescale > 0) {
-            // System.out.println("setting prescale to " + cn.prescale);
             stationConfig.setPrescale(prescale);
         }
 
         // Create the station.
-        //LOGGER.config("Creating ET station: " + stationName);
-        stat = sys.createStation(stationConfig, stationName, stationPosition);
+        stat = sys.createStation(stationConfig, stationName);
         
         // attach to new station
         att = sys.attach(stat);
 
+        // These are used when getting events later.
         this.waitMode = waitMode;
         this.waitTime = waitTime;
         this.chunkSize = chunkSize;
