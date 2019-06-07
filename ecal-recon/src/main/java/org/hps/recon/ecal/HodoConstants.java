dea97663f28d2103ef89ec03bf23a6ff7c735351
Rafayel Paremuzyan <rafopar@jlab.org>
2019-06-07 00:59:35 -0400

    Definition of NSB and NSA is changed, before it was shown ns, now they really indicat # of samples, and also in comment their description is added. In addition A new variable NMax_peak is added, which shows the maximum number of peaks per readout window

--- ecal-recon/src/main/java/org/hps/recon/ecal/HodoConstants.java
+++ ecal-recon/src/main/java/org/hps/recon/ecal/HodoConstants.java
@@ -12,8 +12,10 @@ public class HodoConstants {
     
     public static final int TET_AllCh = 15;
     
-    public static final int NSA = 40;
-    public static final int NSB = 16;
+    public static final int NSA = 11;  // # of samples after the threshold crossings
+    public static final int NSB = 4;   // # of samples before the threshold crossing
+    public static final int NSPerSample = 4;  // ns per ADC sample
+    public static final int NMax_peak = 4;    // Maximum number of peaks in the readout window
     
     
     
