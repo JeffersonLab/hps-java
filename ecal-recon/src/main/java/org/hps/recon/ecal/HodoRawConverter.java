0ac19604705507b1ea93fda96d3889c5f46a0c97
Rafayel Paremuzyan <rafopar@jlab.org>
2019-06-07 01:07:42 -0400

    Performs Hodo hit reconstruction. Somce check are performed, e.g. seems it used roght pedestals, gains, right adc samples etc, although some more checks might be needed

--- ecal-recon/src/main/java/org/hps/recon/ecal/HodoRawConverter.java
+++ ecal-recon/src/main/java/org/hps/recon/ecal/HodoRawConverter.java
@@ -5,12 +5,13 @@
  */
 package org.hps.recon.ecal;
 
-import java.util.ArrayList;
 import org.lcsim.event.RawTrackerHit;
 import org.hps.conditions.hodoscope.HodoscopeConditions;
 import org.hps.conditions.hodoscope.HodoscopeChannel;
 import org.hps.conditions.hodoscope.HodoscopeChannelConstants;
+import org.lcsim.event.CalorimeterHit;
 import org.lcsim.event.EventHeader;
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
@@ -26,7 +27,7 @@ public class HodoRawConverter {
      */
     private boolean useRunningPedestal = true;
 
-    public int FindThresholdCrossings(RawTrackerHit hit) {
+    public ArrayList<Integer> FindThresholdCrossings(RawTrackerHit hit, double ped) {
 
         // Getting the cellID of the hit
         final long cellID = hit.getCellID();
@@ -40,13 +41,77 @@ public class HodoRawConverter {
         // ==== This list will be populated with threshold crossings
         ArrayList<Integer> thr_crossings = new ArrayList<Integer>();
 
+        double Threshold = ped + HodoConstants.TET_AllCh;
+
+        // special case, first sample is above threshold:
+        if (samples[0] > Threshold) {
+            thr_crossings.add(0);
+        }
+
         //double ped = getPedestal();
         // ===== Loop over samples to determine Threshold Crossings =====
-        for (int is = 0; is < n_samp; is++) {
+        for (int is = 1; is < n_samp; is++) {
+
+            if (samples[is] > Threshold && samples[is - 1] <= Threshold) {
+
+                // found one:
+                thr_crossings.add(is);
+
+                // search for next threshold crossing begins at end of this
+                // pulse:
+                is = is + HodoConstants.NSA - 1;
+
+                // Don't find more than NMax_peak peaks
+                if (thr_crossings.size() >= HodoConstants.NMax_peak) {
+                    break;
+                }
+
+            }
 
         }
 
-        return 1;
+        return thr_crossings;
+    }
+
+    public ArrayList<CalorimeterHit> getCaloHits(RawTrackerHit hit, ArrayList<Integer> thr_crosings, double ped) {
+
+        // Getting the cellID of the hit
+        final long cellID = hit.getCellID();
+
+        // ADC values for this hit
+        final short samples[] = hit.getADCValues();
+
+        int n_samp = samples.length;
+
+        double gain = findChannel(cellID).getGain().getGain();
+        //System.out.println("The Gains = " + findChannel(cellID).getGain().toString());
+
+        ArrayList<CalorimeterHit> curHits = new ArrayList<CalorimeterHit>();
+
+        // ==== Will loop over threshold crossings, and for each of thr_crossing will calculate
+        // ==== the integrated signal
+        for (int crs_time : thr_crosings) {
+
+            int samp_Left = java.lang.Math.max(0, crs_time - HodoConstants.NSB);
+            int samp_Right = java.lang.Math.min(n_samp - 1, crs_time + HodoConstants.NSA - 1);
+
+            double ADC_Sum = 0;
+            for (int is = samp_Left; is <= samp_Right; is++) {
+                ADC_Sum = ADC_Sum + samples[is];
+            }
+
+            // ==== Now subtract pedestal from the ADC Sum ====
+            ADC_Sum = ADC_Sum - ped * (samp_Right - samp_Left + 1);
+
+            double Energy = ADC_Sum * gain;
+            double time = crs_time * HodoConstants.NSPerSample;
+
+            //System.out.println("time = " + time + "     gain = " + gain + "        Energy = " + Energy + "ADC Sum is " + ADC_Sum);
+            
+            curHits.add(CalorimeterHitUtilities.create(Energy, time, cellID));
+        }
+
+        return curHits;
     }
 
     public double getPedestal(EventHeader event, long cellid) {
@@ -54,11 +119,7 @@ public class HodoRawConverter {
         if (useRunningPedestal && event != null) {
 
             Map<HodoscopeChannel, Double> runningPedMap = (Map<HodoscopeChannel, Double>) event.get("HodoRunningPedestals");
-//            System.out.println("cellID in the getPedestal method = " + cellid);
-//            System.out.println("Channels in the getPedestal is " + hodoConditions.getChannels());
             HodoscopeChannel chan = hodoConditions.getChannels().findGeometric(cellid);
-
-            System.out.println("chan is " + chan);
 
             return runningPedMap.get(chan);
         } else {
--- ecal-recon/src/main/java/org/hps/recon/ecal/HodoRawConverterDriver.java
+++ ecal-recon/src/main/java/org/hps/recon/ecal/HodoRawConverterDriver.java
@@ -7,13 +7,14 @@ package org.hps.recon.ecal;
 import org.lcsim.util.Driver;
 import org.hps.conditions.database.DatabaseConditionsManager;
 import org.hps.conditions.hodoscope.HodoscopeConditions;
-import org.hps.conditions.hodoscope.HodoscopeChannel;
+import org.lcsim.event.CalorimeterHit;
 import org.lcsim.event.RawTrackerHit;
 import org.lcsim.event.EventHeader;
 import org.lcsim.geometry.Detector;
+import org.lcsim.lcio.LCIOConstants;
 
-import java.util.Map;
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  *
@@ -30,7 +31,12 @@ public class HodoRawConverterDriver extends Driver {
     private String rawCollectionName = "HodoReadoutHits";
 
     private String hodoCollectionName = "HodoCalHits";
-    
+
+    // ===== **NOTE** Seems this name can not be arbitrary, it is taken from the detector
+    // ===== For example you can find out this by running this method of the detector
+    // ===== System.out.println("==== The Detector SubDetector hit collection name " + detector.getSubdetector("Hodoscope").getHitsCollectionName() );
+    private final String hodoReadoutName = "HodoscopeHits";
+
     public HodoRawConverterDriver() {
         converter = new HodoRawConverter();
     }
@@ -46,9 +52,13 @@ public class HodoRawConverterDriver extends Driver {
     @Override
     public void detectorChanged(Detector detector) {
 
+        
+//        System.out.println("==== The Detector Name is " + detector.getDetectorName());
+//        System.out.println("==== The DetectorElement Name is " + detector.getDetectorElement().getName() );
+//        System.out.println("==== The Detector SubDetector hit collection name " + detector.getSubdetector("Hodoscope").getHitsCollectionName() );
         // Hodo conditions object.
         hodoConditions = DatabaseConditionsManager.getInstance().getHodoConditions();
-        
+
         converter.setConditions(hodoConditions);
 
     }
@@ -56,9 +66,13 @@ public class HodoRawConverterDriver extends Driver {
     @Override
     public void process(EventHeader event) {
 
-        System.out.println("===== Starting of Process in the Hodo Recon: The event number is " + event.getEventNumber());
-        
+        ArrayList<CalorimeterHit> hodoHits = new ArrayList<CalorimeterHit>();
+
         if (event.hasCollection(RawTrackerHit.class, rawCollectionName)) {
+
+            int flags = 0;
+            flags += 1 << LCIOConstants.RCHBIT_TIME; // store hit time
+            flags += 1 << LCIOConstants.RCHBIT_LONG; // store hit position; this flag has no effect for RawCalorimeterHits
 
             // ======= Getting list of Mode1 hits from the event ======
             List<RawTrackerHit> hits = event.get(RawTrackerHit.class, rawCollectionName);
@@ -68,19 +82,33 @@ public class HodoRawConverterDriver extends Driver {
 
                 // Getting the cellID of the hit
                 final long cellID = hit.getCellID();
+
+                double ped = converter.getPedestal(event, cellID);
+
+                // ========= Now for the given channel we will loop over all threshold crossings, and
+                // ========= for each of them will calculate the signal by integrating from NSB to NSA
+                ArrayList<Integer> thr_crosings = converter.FindThresholdCrossings(hit, ped);
+
+                //System.out.println("# of thr crossings = " + thr_crosings.size());
+                // ===== For now we will calculate coars time, which is the threshold crossing sample time.
+                // ===== Later will implement the mode7 time
+                hodoHits.addAll(converter.getCaloHits(hit, thr_crosings, ped));
+
                 
-                System.out.println("cellID of the hit is " + cellID);
-                
-                //double ped = converter.getPedestal(event, cellID);                                
+                // ===== The following few lines are the equivalent of the cin.ignore() of C
+//                try {
+//                    System.in.read();
+//                } catch (Exception e) {
+//                }
+
             }
 
+            event.put(hodoCollectionName, hodoHits, CalorimeterHit.class, flags, hodoReadoutName);
         }
-        Map<HodoscopeChannel, Double> runningPedMap = (Map<HodoscopeChannel, Double>) event.get("HodoRunningPedestals");
+        //Map<HodoscopeChannel, Double> runningPedMap = (Map<HodoscopeChannel, Double>) event.get("HodoRunningPedestals");
 
         //System.out.println("runningPedMap = " + runningPedMap);
         // Now 
-        
-        System.out.println("===== End of Process in the Hodo Recon =====");
     }
 
 }
