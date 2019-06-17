df6e3e12b952f13bc366b11c25bf4280ee5c8576
Jeremy McCormick <jermccormick@gmail.com>
2019-06-17 13:37:46 +0700

    Activate plot task on startup. Apply reordering on class members.

--- online-recon/src/main/java/org/hps/online/recon/Server.java
+++ online-recon/src/main/java/org/hps/online/recon/Server.java
@@ -8,6 +8,7 @@ import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.Timer;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Logger;
@@ -28,8 +29,10 @@ import org.json.JSONObject;
  * 
  * @author jeremym
  */
-public class Server {        
+public final class Server {        
        
+    private static final long MILLIS_TO_SECONDS = 1000L;
+
     private class ClientTask implements Runnable {
         
         private final Socket socket;
@@ -62,7 +65,7 @@ public class Server {
                     if (handler != null) {
                         res = handler.execute(params);
                     } else {
-                        res = new CommandStatus(STATUS_ERROR, "Unknown command <" + command + ">");
+                        res = new CommandStatus(STATUS_ERROR, "Unknown command: " + command);
                     }
                 } catch (Exception e) {                   
                     e.printStackTrace();
@@ -119,19 +122,6 @@ public class Server {
         }
     }
     
-    class JSONResult extends CommandResult {
-        
-        final JSONObject jo;
-        
-        JSONResult(JSONObject jo) {
-            this.jo = jo;
-        }
-                
-        public String toString() {
-            return jo.toString();
-        }        
-    }
-    
     class GenericResult extends CommandResult {
         
         final Object o;
@@ -144,7 +134,49 @@ public class Server {
             return o.toString();
         }        
     }
+    
+    class JSONResult extends CommandResult {
+        
+        final JSONObject jo;
+        
+        JSONResult(JSONObject jo) {
+            this.jo = jo;
+        }
+                
+        public String toString() {
+            return jo.toString();
+        }        
+    }
             
+    class ListCommandHandler extends CommandHandler {
+
+        CommandResult execute(JSONObject parameters) {
+            CommandResult res = null;
+            int id = -1;
+            if (parameters.has("id")) {
+                id = parameters.getInt("id");
+            }
+            if (id != -1) {
+                // Return JSON object with single station info
+                StationInfo info = stationManager.findStation(id);
+                if (info != null) {
+                    res = new JSONResult(info.toJSON());
+                } else {
+                    res = new CommandStatus(STATUS_ERROR, "Unknown process id: " + id);
+                }
+            } else {
+                // Return JSON array of station data               
+                JSONArray arr = new JSONArray();
+                for (StationInfo info : stationManager.getStations()) {
+                    arr.put(info.toJSON());
+                }
+                res = new GenericResult(arr);
+            }
+            return res;
+        }
+        
+    }
+    
     class StartCommandHandler extends CommandHandler {
         CommandResult execute(JSONObject parameters) {
             CommandStatus res = null;
@@ -153,12 +185,12 @@ public class Server {
                 count = parameters.getInt("count");
             }
             try {                 
-                LOGGER.info("Starting <" + count + "> processes");
+                LOGGER.info("Starting processes: " + count);
                 for (int i = 0; i < count; i++) {
-                    LOGGER.info("Creating process <" + i + ">");
-                    Server.this.processManager.createStation(parameters);
+                    LOGGER.info("Creating process: " + i);
+                    Server.this.stationManager.createStation(parameters);
                 }
-                res = new CommandStatus(STATUS_SUCCESS, "Started <" + count + "> processes successfully.");
+                res = new CommandStatus(STATUS_SUCCESS, "Started " + count + " processes successfully.");
             } catch (IOException e) {
                 e.printStackTrace();
                 res = new CommandStatus(STATUS_ERROR, "Error creating new process.");
@@ -175,60 +207,99 @@ public class Server {
                 id = parameters.getInt("id");
             }
             if (id != -1) {
-                processManager.stopStation(id);
-                res = new CommandStatus(STATUS_SUCCESS, "Stopped process <" + id + ">");
+                stationManager.stopStation(id);
+                res = new CommandStatus(STATUS_SUCCESS, "Stopped process: " + id);
             } else {
-                processManager.stopAll();
+                stationManager.stopAll();
                 res = new CommandStatus(STATUS_SUCCESS, "Stopped all processes");
             }            
             return res;
         }
     }
     
-    class ListCommandHandler extends CommandHandler {
-
-        CommandResult execute(JSONObject parameters) {
-            CommandResult res = null;
-            int id = -1;
-            if (parameters.has("id")) {
-                id = parameters.getInt("id");
-            }
-            if (id != -1) {
-                // Return JSON object with single station info
-                StationInfo info = processManager.findStation(id);
-                if (info != null) {
-                    res = new JSONResult(info.toJSON());
-                } else {
-                    res = new CommandStatus(STATUS_ERROR, "Unknown process id <" + id + ">");
-                }
-            } else {
-                // Return JSON array of station data               
-                JSONArray arr = new JSONArray();
-                for (StationInfo info : processManager.getStations()) {
-                    arr.put(info.toJSON());
-                }
-                res = new GenericResult(arr);
-            }
-            return res;
-        }
+    private static final int DEFAULT_PORT = 22222;
         
+    static Logger LOGGER = Logger.getLogger(Server.class.getPackageName());    
+    private static final int MAX_PORT = 49152;
+    
+    private static final int MIN_PORT = 1024;
+    
+    public static final String STATUS_ERROR = "ERROR";
+    public static final String STATUS_SUCCESS = "SUCCESS";
+        
+    public static void main(String args[]) { 
+        Server server = new Server();
+        try {
+            server.parse(args);
+        } catch (ParseException e) {
+            throw new RuntimeException("Error parsing command line", e);
+        }        
+        server.start();
     }
     
-    static Logger LOGGER = Logger.getLogger(Server.class.getPackageName());
-        
-    public static final String STATUS_ERROR = "ERROR";    
-    public static final String STATUS_SUCCESS = "SUCCESS";
+    final ExecutorService clientProcessingPool = Executors.newFixedThreadPool(10);
+            
+    private final String DEFAULT_WORK_PATH = System.getProperty("user.dir");
     
+    private File plotFile;
+    private Long plotIntervalMillis = (long) (60 * 1000);
+    private Timer plotTimer = new Timer();
+    
+    private int port = DEFAULT_PORT;
+        
+    private String stationBase = "HPS_RECON";
+        
     private StationConfiguration stationConfig = new StationConfiguration();
         
+    private final StationManager stationManager;
+    
+    private File workDir;
+    
+    private String workPath = DEFAULT_WORK_PATH;    
+  
+    private boolean dryRun = false;
+    
+    public Server() {        
+        this.stationManager = new StationManager(this);
+    }
+        
+    File createProcessDir(String name) {
+        String path = this.workDir.getPath() + File.separator + name;
+        File dir = new File(path);
+        dir.mkdir();
+        if (!dir.isDirectory()) {
+            throw new RuntimeException("Error creating dir: " + dir.getPath());
+        }
+        return dir;
+    }
+            
+    String getStationBaseName() {
+        return this.stationBase;
+    }
+        
+    StationConfiguration getStationConfig() {
+        return this.stationConfig;
+    }
+    
+    StationManager getStationManager() {
+        return this.stationManager;
+    }
+    
+    File getWorkDir() {
+        return this.workDir;
+    }
+    
     void parse(String args[]) throws ParseException {
         Options options = new Options();
         options.addOption(new Option("h", "help", false, "print help"));
         options.addOption(new Option("p", "port", true, "server port"));
-        options.addOption(new Option("s", "start", true, "starting station ID (default 0)"));
+        options.addOption(new Option("s", "start", true, "starting station ID (default 1)"));
         options.addOption(new Option("w", "workdir", true, "work dir (default is current dir where server is started)"));
         options.addOption(new Option("b", "basename", true, "station base name"));
         options.addOption(new Option("c", "config", true, "config properties file"));
+        options.addOption(new Option("a", "add-plots", true, "output file for adding plots"));
+        options.addOption(new Option("i", "interval", true, "update interval in seconds for adding plots (default is every 1 minute)"));
+        options.addOption(new Option("D", "dry-run", false, "run plot task in dry run mode (plots will not be added or deleted)"));
         
         final CommandLineParser parser = new DefaultParser();
         CommandLine cl = parser.parse(options, args);
@@ -237,7 +308,7 @@ public class Server {
         if (cl.hasOption("h")) {
             final HelpFormatter help = new HelpFormatter();
             help.printHelp("Server", 
-                    "Start the online reconstruction server",
+                    "start the online reconstruction server",
                     options, "");
             System.exit(0);
         }
@@ -248,15 +319,15 @@ public class Server {
         }
         if (this.port < MIN_PORT || this.port >= MAX_PORT) {
             LOGGER.severe("Port number <" + this.port + "> is not between " + MIN_PORT + " and " + MAX_PORT);
-            throw new RuntimeException("Port number <" + this.port + "> is not allowed.");
+            throw new RuntimeException("Port number is not allowed: " + this.port);
         }
-        LOGGER.config("Server set to use port <" + this.port + ">");
+        LOGGER.config("Server port: " + this.port);
         
         // Starting station ID.
         if (cl.hasOption("s")) {
             int processID = Integer.parseInt(cl.getOptionValue("s"));
-            this.processManager.setStationID(processID);
-            LOGGER.config("Starting station ID is <" + processID + ">");
+            this.stationManager.setStationID(processID);
+            LOGGER.config("Starting station ID: " + processID);
         }
         
         // Base work directory for creating station directories.
@@ -264,88 +335,60 @@ public class Server {
             this.workPath = cl.getOptionValue("w");
         }                
         this.workDir = new File(this.workPath);
-        LOGGER.config("Server work dir set to <" + this.workDir + ">");
+        LOGGER.config("Server work dir: " + this.workDir);
         if (!this.workDir.exists()) {
-            throw new RuntimeException("Work dir does not exist <" + this.workPath + ">");
+            throw new RuntimeException("Work dir does not exist: " + this.workPath);
         }
         if (!this.workDir.isDirectory()) {
-            throw new RuntimeException("Work path is not a directory <" + this.workPath + ">");
+            throw new RuntimeException("Work path is not a directory: " + this.workPath);
         }
         if (!this.workDir.canWrite()) {
-            throw new RuntimeException("Work dir <" + this.workPath + "> is not writable.");
+            throw new RuntimeException("Work dir is not writable: " + this.workPath);
         }
         
         // Base name for station to which will be appended the station ID.
         if (cl.hasOption("b")) {
             this.stationBase = cl.getOptionValue("b");
         }
-        LOGGER.config("Station base name set to <" + this.stationBase + ">");
+        LOGGER.config("Station base name: " + this.stationBase);
+
+        // Output file for combined plots.
+        if (cl.hasOption("a")) {
+            this.plotFile = new File(cl.getOptionValue("a"));
+            LOGGER.config("Plot file set to: " + this.plotFile.toPath());
+        }
+        
+        // Plot update interval in seconds.
+        if (cl.hasOption("i")) {
+            this.plotIntervalMillis = Long.valueOf(cl.getOptionValue("i")) * 1000L;
+            LOGGER.config("Plot interval set to " + this.plotIntervalMillis + " ms (" + this.plotIntervalMillis / MILLIS_TO_SECONDS + " seconds).");
+        }
         
         // Load station configuration properties.
         if (cl.hasOption("c")) {
             File configFile = new File(cl.getOptionValue("c"));
             if (!configFile.exists()) {
-                throw new RuntimeException("Config file <" + configFile.getPath() + " does not exist.");
+                throw new RuntimeException("Config file does not exist: " + configFile.getPath());
             }
             this.stationConfig.load(configFile);
         }
-    }
-    
-    public static void main(String args[]) { 
-        Server server = new Server();
-        try {
-            server.parse(args);
-        } catch (ParseException e) {
-            throw new RuntimeException("Error parsing command line", e);
-        }        
-        server.start();
-    }
-            
-    final ExecutorService clientProcessingPool = Executors.newFixedThreadPool(10);
-    
-    private static final int MIN_PORT = 1024;
-    private static final int MAX_PORT = 49152;
-    private static final int DEFAULT_PORT = 22222;
-    
-    private int port = DEFAULT_PORT;
         
-    private final StationManager processManager;
-        
-    private String stationBase = "HPS_RECON";
-        
-    private File workDir;
-    
-    private final String DEFAULT_WORK_PATH = System.getProperty("user.dir");
-    
-    private String workPath = DEFAULT_WORK_PATH;
-    
-    public Server() {        
-        this.processManager = new StationManager(this);
-    }
-            
-    File createProcessDir(String name) {
-        String path = this.workDir.getPath() + File.separator + name;
-        File dir = new File(path);
-        dir.mkdir();
-        if (!dir.isDirectory()) {
-            throw new RuntimeException("Error creating dir <" + dir.getPath() + ">");
+        if (cl.hasOption("D")) {
+            this.dryRun = true;
         }
-        return dir;
     }
         
-    String getStationBaseName() {
-        return this.stationBase;
-    }
-    
-    File getWorkDir() {
-        return this.workDir;
-    }
-    
-    StationConfiguration getStationConfig() {
-        return this.stationConfig;
-    }
+    void start() {
         
-    void start() {                
+        // Schedule the plot timer.
+        if (this.plotFile != null) {
+            LOGGER.info("Starting plot add task with output file " + this.plotFile + " and update interval of " + this.plotIntervalMillis + " ms.");
+            this.plotTimer.schedule(new PlotAddTask(this, this.plotFile, dryRun), this.plotIntervalMillis, this.plotIntervalMillis);
+        } else {
+            LOGGER.info("Plot task was not enabled (no plot file was provided -a arg)");
+        }
+        
+        // Start the server.
         LOGGER.info("Starting server on port <" + this.port + ">");
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
@@ -354,6 +397,6 @@ public class Server {
             }
         } catch (IOException e) {
             throw new RuntimeException("Server exception", e);
-        }                       
+        }
     }
 }
