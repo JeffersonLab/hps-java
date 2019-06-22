0983c62c8ffb284e3dc49e2c13264389ecab0cb1
Jeremy McCormick <jermccormick@gmail.com>
2019-06-22 21:24:45 +0700

    Use nextLine() with Scanner. Add commands for redirecting output to terminal or file. Add to help print out.

--- online-recon/src/main/java/org/hps/online/recon/Console.java
+++ online-recon/src/main/java/org/hps/online/recon/Console.java
@@ -28,45 +28,74 @@ public class Console {
         this.commands = client.getCommands();
         this.commandMap = client.getCommandMap();
     }
-    
+         
     public void run() {
         String userInput;    
         
         Scanner sn = new Scanner(System.in);
+
+        System.out.println("HPS Online Reconstruction");
+        System.out.println("Type 'help' or 'help [command]' for more information or 'exit' to quit.");
         
         while (true) {
-            System.out.println("Type 'help' or 'help [command]' for more information or 'exit' to quit.");
             System.out.print("online> ");
-            userInput = sn.next().trim();
+            userInput = sn.nextLine();
+            userInput = userInput.trim();
+            //System.out.println("userInput: " + userInput);
             if (userInput.length() > 0) {
                 String rawInputArr[] = userInput.split(" ");
                 String cmdStr = rawInputArr[0];
-                //System.out.println("cmd: " + cmdStr);
                 List<String> args = new ArrayList<String>(Arrays.asList(rawInputArr));
+                //System.out.println("args: " + args);
                 if (args.size() > 0) {
                     args.remove(0);
                 }
+                //System.out.println("cmd args: " + args);
                 if (cmdStr.equals("exit")) {
                     break;
                 } else if (cmdStr.equals("help")) {
                     if (args.size() == 0) {
                         printHelp();
-                    } else {
-                        printCommandHelp(cmdStr);
+                    } else {                        
+                        printCommandHelp(args.get(0));
                     }
                 } else if (cmdStr.equals("port")) {
-                    if (args.size() > 0) {
+                    if (args.size() == 0) {
                         System.out.println(client.getPort());
                     } else {
-                        Integer port = Integer.parseInt(args.get(0));
-                        client.setPort(port);
+                        try {
+                            Integer port = Integer.parseInt(args.get(0));
+                            client.setPort(port);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
                 } else if (cmdStr.equals("host")) {
-                    if (rawInputArr.length < 2) {
+                    if (args.size() == 0) {
                         System.out.println(client.getHostname());
                     } else {
-                        String hostname = rawInputArr[1];
-                        client.setHostname(hostname);
+                        try {
+                            String hostname = args.get(0);
+                            client.setHostname(hostname);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } else if (cmdStr.equals("file")) {
+                    if (args.size() == 0) {
+                        if (client.getOutputFile() != null) {
+                            System.out.println(client.getOutputFile().toPath());
+                        } else {
+                            System.out.println("Output is being written to the terminal.");
+                        }
+                    } else {
+                        String filename = args.get(0);                        
+                        client.setOutputFile(filename);                        
+                    }
+                } else if (cmdStr.equals("terminal")) {
+                    if (client.getOutputFile() != null) {
+                        client.setOutputFile(null);
+                        System.out.println("Redirected output back to the terminal.");
                     }
                 } else {
                     if (this.commands.contains(cmdStr)) {
@@ -74,15 +103,19 @@ public class Console {
                         DefaultParser parser = new DefaultParser();
                         try {
                             String cmdArr[] = args.toArray(new String[0]);
-                            //System.out.println("args: " + Arrays.asList(cmdArr));
                             CommandLine cl = parser.parse(cmd.getOptions(), cmdArr);
-                            cmd.process(cl);
-                            client.send(cmd);
+                            try {
+                                cmd.process(cl);
+                                client.send(cmd);
+                                cmd.reset();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         } catch (ParseException e) {
                             e.printStackTrace();
                         }
                     } else {
-                        System.err.println("Unknown commnd: " + cmdStr);
+                        System.err.println("Unknown client commnd: " + cmdStr);
                     }
                 }
             }
@@ -92,13 +125,19 @@ public class Console {
     }
     
     void printHelp() {
-        System.out.println('\n' + "SETTINGS" + '\n');
-        System.out.println("    port - set the server port");
-        System.out.println("    host - set the server hostname" + '\n');        
-        System.out.println("COMMANDS");
+        System.out.println('\n' + "GENERAL" + '\n');
+        System.out.println("    help - print general information");
+        System.out.println("    help [cmd] - print information for specific command");
+        System.out.println("    exit - quit the console");
+        System.out.println('\n' + "SETTINGS" + '\n');        
+        System.out.println("    port [port] - set the server port");
+        System.out.println("    host [host]- set the server hostname");
+        System.out.println("    file [filename] - write server output to a file");
+        System.out.println("    terminal - redirect server output back to the terminal");
+        System.out.println('\n' + "COMMANDS" + '\n');
         for (Entry<String, ClientCommand> entry : this.commandMap.entrySet()) {
             System.out.println("    " + entry.getKey() + " - " + entry.getValue().getDescription());
-        }        
+        }
     }
     
     void printCommandHelp(String command) {
