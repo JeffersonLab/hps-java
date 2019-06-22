538a70b91e4b3e8a702952fe696a6a4c2d85e50b
Jeremy McCormick <jermccormick@gmail.com>
2019-06-22 23:55:40 +0700

    Add new package with commands.

---
+++ online-recon/src/main/java/org/hps/online/recon/commands/CleanupCommand.java
@@ -0,0 +1,22 @@
+package org.hps.online.recon.commands;
+
+import org.apache.commons.cli.CommandLine;
+import org.hps.online.recon.ClientCommand;
+
+/**
+ * Cleanup a station by deleting its working directory.
+ * 
+ * This will only work on inactive stations.
+ */
+class CleanupCommand extends ClientCommand {
+    
+    CleanupCommand() {            
+        super("cleanup", "Delete a station's working directory and files", "[IDs]",
+                "Provide a list of IDs or none to cleanup all");
+    }
+            
+    protected void process(CommandLine cl) {
+        super.process(cl);
+        readStationIDs(cl);
+    }
+}
