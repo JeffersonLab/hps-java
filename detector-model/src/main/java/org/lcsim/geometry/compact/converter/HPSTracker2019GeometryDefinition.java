260fa064e1bd7a0de7962eb0c44cbf88d757788a
Matthew Solt <mrsolt@stanford.edu>
2019-07-07 21:22:55 -0700

    updating 2019 detector putting sensors in nominal position

--- detector-model/src/main/java/org/lcsim/geometry/compact/converter/HPSTracker2019GeometryDefinition.java
+++ detector-model/src/main/java/org/lcsim/geometry/compact/converter/HPSTracker2019GeometryDefinition.java
@@ -470,8 +470,8 @@ public class HPSTracker2019GeometryDefinition extends HPSTracker2014v1GeometryDe
     //public static class ModuleL2Bot extends ModuleL13Bot {
     public static class ModuleL2Bot extends ShortModuleBot {
         protected final static double shift_along_uchannel = 0; //positive is downstream
-        protected final static double shift_across_uchannel = 0; //positive is beam right
-        protected final static double shift_vertically_uchannel = 0; //positive is towards beam
+        protected final static double shift_across_uchannel = 0.0; //positive is beam right
+        protected final static double shift_vertically_uchannel = 0.350; //positive is towards beam
         // Note the L1 measures are used here
         protected final static double cone_to_hole_along_uchannel = HPSTracker2014GeometryDefinition.ModuleL1Bot.cone_to_hole_along_uchannel + shift_along_uchannel;
         protected final static double cone_to_hole_across_uchannel = HPSTracker2014v1GeometryDefinition.ModuleL1Bot.cone_to_hole_across_uchannel + shift_across_uchannel; // change x position layer 1 bot
@@ -494,7 +494,7 @@ public class HPSTracker2019GeometryDefinition extends HPSTracker2014v1GeometryDe
     public static class ModuleL2Top extends ShortModuleTop {
         protected final static double shift_along_uchannel = 0; //positive is downstream
         protected final static double shift_across_uchannel = 0; //positive is beam left
-        protected final static double shift_vertically_uchannel = 0; //positive is towards beam
+        protected final static double shift_vertically_uchannel = 0.350; //positive is towards beam
 
         // Note the L1 measures are used here
         protected final static double cone_to_hole_along_uchannel = HPSTracker2014GeometryDefinition.ModuleL1Top.cone_to_hole_along_uchannel + shift_along_uchannel;
@@ -812,7 +812,8 @@ public class HPSTracker2019GeometryDefinition extends HPSTracker2014v1GeometryDe
      */
     public abstract static class ShortModule extends BaseModule {
 
-        protected final static double distance_between_stereo_axial_norm_dir = 3.0 / 16.0 * inch;
+        //protected final static double distance_between_stereo_axial_norm_dir = 3.0 / 16.0 * inch;
+        protected final static double distance_between_stereo_axial_norm_dir = 5.0 / 16.0 * inch;
 
         // OLD STUFF MOSTLY
         protected final static double hole_to_center_of_plate_width_dir = 5.875 * inch;
@@ -917,9 +918,9 @@ public class HPSTracker2019GeometryDefinition extends HPSTracker2014v1GeometryDe
         // position this module w.r.t. previous L1 by shifting it along the
         // channel
         protected final static double shift_along_uchannel = -50.;
-        protected final static double shift_vertically_uchannel = 0;// 20.6658;
-        protected final static double shift_across_uchannel = 5.19;
-        protected final static double shift_again_along_uchannel = 4.66;
+        protected final static double shift_vertically_uchannel = 0.0;//0;// 20.6658;
+        protected final static double shift_across_uchannel = 0;//5.19;
+        protected final static double shift_again_along_uchannel = 0;//4.66;
 
         protected final static double cone_to_hole_along_uchannel = HPSTracker2014v1GeometryDefinition.ModuleL1Bot.cone_to_hole_along_uchannel
                 + shift_along_uchannel - shift_again_along_uchannel;
@@ -948,14 +949,15 @@ public class HPSTracker2019GeometryDefinition extends HPSTracker2014v1GeometryDe
         // channel
         // note flip
         protected final static double shift_along_uchannel = -50.;
-        protected final static double shift_across_uchannel = 4.81;
-        protected final static double shift_again_along_uchannel = 4.32;
+        protected final static double shift_vertically_uchannel = 0.0;
+        protected final static double shift_across_uchannel = 0;//4.81;
+        protected final static double shift_again_along_uchannel = 0;//4.32;
         // note flip wr.t. bottom
         protected final static double cone_to_hole_along_uchannel = HPSTracker2014v1GeometryDefinition.ModuleL1Top.cone_to_hole_along_uchannel
                 + shift_along_uchannel + shift_again_along_uchannel;
         protected final static double cone_to_hole_across_uchannel = HPSTracker2014v1GeometryDefinition.ModuleL1Top.cone_to_hole_across_uchannel
                 - shift_across_uchannel; // change x position layer 1 top
-        protected final static double cone_to_hole_vertical_from_uchannel = HPSTracker2014v1GeometryDefinition.ModuleL1Top.cone_to_hole_vertical_from_uchannel;
+        protected final static double cone_to_hole_vertical_from_uchannel = HPSTracker2014v1GeometryDefinition.ModuleL1Top.cone_to_hole_vertical_from_uchannel + shift_vertically_uchannel;
 
         public ModuleL1Top(String name, SurveyVolume mother, AlignmentCorrection alignmentCorrection, SurveyVolume ref) {
             super(name, mother, alignmentCorrection, ref);
