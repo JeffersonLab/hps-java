1e497cac22501a434010969bb44d9de75019496a
Matthew Solt <mrsolt@stanford.edu>
2019-09-26 12:38:43 -0700

    changings L0/L1 strip length to 15mm and updating lcdd

--- detector-model/src/main/java/org/lcsim/geometry/compact/converter/HPSTracker2019GeometryDefinition.java
+++ detector-model/src/main/java/org/lcsim/geometry/compact/converter/HPSTracker2019GeometryDefinition.java
@@ -687,7 +687,8 @@ public class HPSTracker2019GeometryDefinition extends HPSTracker2014v1GeometryDe
 
         // static final double length= 10.00 + 1.00;
         //static final double length = 10.00;
-        static final double length = 10.00*2;
+        //static final double length = 10.00*2;
+        static final double length = 15.00*2;
         //static final double width = 14.080 + 2 * 0.250;
         static final double width = 14.025 + 2 * 0.250;
         static final double thickness = 0.200; // 0.250;
