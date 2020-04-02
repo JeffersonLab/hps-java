9e1502b221a9de2f18e53824007bc8892e015bd9
Pierfrancesco Butti <pierfrancesco.butti@gmail.com>
2020-04-02 00:54:01 -0700

    Add run-time protection when making V0 candidates

--- recon/src/main/java/org/hps/recon/particle/HpsReconParticleDriver.java
+++ recon/src/main/java/org/hps/recon/particle/HpsReconParticleDriver.java
@@ -378,7 +378,13 @@ public class HpsReconParticleDriver extends ReconParticleDriver {
                 }
                 
                 // Make V0 candidates
-                this.makeV0Candidates(electron, positron);
+                try {
+                    this.makeV0Candidates(electron, positron);
+                }
+                catch (RuntimeException e) {
+                    System.out.println("HpsReconParticleDriver::makeV0Candidates fails:: skipping ele/pos pair.");
+                    continue;
+                }
             }
         }
     }
