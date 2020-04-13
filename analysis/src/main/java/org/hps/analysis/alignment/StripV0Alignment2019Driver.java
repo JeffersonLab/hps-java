d7c9b8d30439e9e6160bf8ed736016e317c4e74d
Graf <ngraf@slac.stanford.edu>
2020-04-13 13:55:59 -0700

    New V0 skimmer, additions to VertexAnalysis

---
+++ analysis/src/main/java/org/hps/analysis/alignment/StripV0Alignment2019Driver.java
@@ -0,0 +1,30 @@
+package org.hps.analysis.alignment;
+
+import java.util.List;
+import org.lcsim.event.EventHeader;
+import org.lcsim.event.ReconstructedParticle;
+import org.lcsim.util.Driver;
+
+/**
+ *
+ * @author Norman A Graf
+ */
+public class StripV0Alignment2019Driver extends Driver {
+
+    int _numberOfEventsSelected;
+
+    protected void process(EventHeader event) {
+        boolean skipEvent = true;
+
+        List<ReconstructedParticle> V0List = event.get(ReconstructedParticle.class, "UnconstrainedV0Candidates");
+
+        if (V0List.size() != 0) {
+            skipEvent = false;
+            _numberOfEventsSelected++;
+        }
+    }
+
+    protected void endOfData() {
+        System.out.println("Selected " + _numberOfEventsSelected + " events");
+    }
+}
