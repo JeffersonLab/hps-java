46c329707a5de7f7638dd3b9363fb9e457114c9e
Graf <ngraf@slac.stanford.edu>
2020-04-24 09:38:25 -0700

    Update PrintGeometryDriver.java
    Print sensor name, u, v and uXv to output.

--- analysis/src/main/java/org/hps/analysis/examples/PrintGeometryDriver.java
+++ analysis/src/main/java/org/hps/analysis/examples/PrintGeometryDriver.java
@@ -2,6 +2,7 @@ package org.hps.analysis.examples;
 
 import hep.physics.vec.BasicHep3Vector;
 import hep.physics.vec.Hep3Vector;
+import hep.physics.vec.VecOp;
 
 import java.util.List;
 
@@ -20,21 +21,25 @@ import org.lcsim.util.Driver;
 /**
  *
  * @author phansson
+ * @author Norman A Graf
  */
 public class PrintGeometryDriver extends Driver {
 
     @Override
     protected void detectorChanged(Detector detector) {
+
         System.out.printf("%s: ################# Print geometry ##########################\n", this.getClass()
                 .getSimpleName());
         IDetectorElement detectorElement = detector.getDetectorElement();
         List<SiSensor> sensors = detectorElement.findDescendants(SiSensor.class);
-        System.out.printf("%s: %5s %40s %40s\n", this.getClass().getSimpleName(), "ID", "Pos", "u");
+        System.out.printf("%32s: %40s %40s %40s %40s\n", this.getClass().getSimpleName(), "Pos", "u", "v", "uXv");
         for (SiSensor sensor : sensors) {
             Hep3Vector position = sensor.getGeometry().getPosition();
-            Hep3Vector u = this.getUnitVector(sensor, "measured");
-            System.out.printf("%s: %5d %40s %40s\n", this.getClass().getSimpleName(), sensor.getSensorID(),
-                    position.toString(), u.toString());
+//            Hep3Vector u = this.getUnitVector(sensor, "measured");
+//            System.out.printf("%48s: %40s %40s\n", sensor.getName(), position.toString(), u.toString());
+            Hep3Vector[] vecs = getUnitVectors(sensor);
+            System.out.printf("%48s: %40s %40s %40s %40s\n", sensor.getName(), position.toString(), vecs[0], vecs[1], vecs[2]);
+
         }
         System.out.printf("%s: ###########################################################\n", this.getClass()
                 .getSimpleName());
@@ -65,6 +70,32 @@ public class PrintGeometryDriver extends Driver {
             }
         }
         return unit_vec;
+    }
+
+    private Hep3Vector[] getUnitVectors(SiSensor sensor) {
+
+        Hep3Vector unit_vecU = new BasicHep3Vector(-99, -99, -99);
+        Hep3Vector unit_vecV = new BasicHep3Vector(-99, -99, -99);
+        Hep3Vector unit_vecW = new BasicHep3Vector(-99, -99, -99);
+
+        for (ChargeCarrier carrier : ChargeCarrier.values()) {
+            if (sensor.hasElectrodesOnSide(carrier)) {
+                int channel = 1;
+                long cell_id = sensor.makeStripId(channel, carrier.charge()).getValue();
+                IIdentifier id = new Identifier(cell_id);
+                SiTrackerIdentifierHelper _sid_helper = (SiTrackerIdentifierHelper) sensor.getIdentifierHelper();
+                SiSensorElectrodes electrodes = sensor.getReadoutElectrodes(carrier);
+                ITransform3D local_to_global = new Transform3D();// sensor.getGeometry().getLocalToGlobal();
+                ITransform3D electrodes_to_global = electrodes.getLocalToGlobal();
+                ITransform3D global_to_hit = local_to_global.inverse();
+                ITransform3D electrodes_to_hit = Transform3D.multiply(global_to_hit, electrodes_to_global);
+
+                unit_vecU = electrodes_to_hit.rotated(electrodes.getMeasuredCoordinate(0));
+                unit_vecV = electrodes_to_hit.rotated(electrodes.getUnmeasuredCoordinate(0));
+                unit_vecW = VecOp.cross(unit_vecU, unit_vecV);
+            }
+        }
+        return new Hep3Vector[]{unit_vecU, unit_vecV, unit_vecW};
     }
 
 }
