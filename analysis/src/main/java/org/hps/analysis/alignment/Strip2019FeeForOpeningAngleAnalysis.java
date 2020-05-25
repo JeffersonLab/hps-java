8c238a9dbdc98236c1215351e2d7530310840065
Graf <ngraf@slac.stanford.edu>
2020-05-25 13:08:31 -0700

    Added Skim Driver for Opening Angle analysis
    Added number of hits in front track stub histogram

---
+++ analysis/src/main/java/org/hps/analysis/alignment/Strip2019FeeForOpeningAngleAnalysis.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hps.analysis.alignment;
+
+import org.lcsim.event.EventHeader;
+import org.lcsim.event.RawTrackerHit;
+import org.lcsim.event.Track;
+import org.lcsim.util.Driver;
+import org.lcsim.util.aida.AIDA;
+
+/**
+ * Simple Driver to skim off events for the SVT Opening Angle analysis
+ *
+ * @author Norman A. Graf
+ */
+public class Strip2019FeeForOpeningAngleAnalysis extends Driver {
+
+    int _numberOfEventsSelected;
+    private int _maxSvtRawTrackerHits = 250;
+    boolean matchFullTracks = true;
+    String fullTrackCollectionName = "s234_c5_e167";
+    private String l0to3CollectionName = "L0to3Tracks";
+    private String l4to6CollectionName = "L4to6Tracks";
+    private AIDA aida = AIDA.defaultInstance();
+
+    protected void process(EventHeader event) {
+        boolean skipEvent = false;
+        //filter out monster events
+        if (event.get(RawTrackerHit.class, "SVTRawTrackerHits").size() < _maxSvtRawTrackerHits) {
+            if (!event.hasCollection(Track.class, l0to3CollectionName)) {
+                skipEvent = true;
+            }
+            if (!event.hasCollection(Track.class, l4to6CollectionName)) {
+                skipEvent = true;
+            }
+            if (matchFullTracks) {
+                if (!event.hasCollection(Track.class, fullTrackCollectionName)) {
+                    skipEvent = true;
+                }
+            }
+            if (event.get(Track.class, l0to3CollectionName).isEmpty()) {
+                skipEvent = true;
+            }
+            if (event.get(Track.class, l4to6CollectionName).isEmpty()) {
+                skipEvent = true;
+            }
+            if (matchFullTracks) {
+                if (event.get(Track.class, fullTrackCollectionName).isEmpty()) {
+                    skipEvent = true;
+                }
+            }
+        }
+        if (skipEvent) {
+            throw new Driver.NextEventException();
+        } else {
+            _numberOfEventsSelected++;
+        }
+    }
+
+    public void endOfData() {
+        System.out.println("selected " + _numberOfEventsSelected + " events for the opening angle alignment analysis");
+    }
+}
