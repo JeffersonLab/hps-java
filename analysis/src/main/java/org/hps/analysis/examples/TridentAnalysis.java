ff8ba3d7f1cd35af9093a1944d252a382351e736
Graf <ngraf@slac.stanford.edu>
2020-05-27 16:50:30 -0700

    Update TridentAnalysis.java
    fix silly skim mistake
    add run-by-run analysis

--- analysis/src/main/java/org/hps/analysis/examples/TridentAnalysis.java
+++ analysis/src/main/java/org/hps/analysis/examples/TridentAnalysis.java
@@ -44,87 +44,87 @@ public class TridentAnalysis extends Driver {
         List<ReconstructedParticle> V0List = event.get(ReconstructedParticle.class, "UnconstrainedV0Candidates");
         aida.histogram1D("Number of V0s in the event", 10, -0.5, 9.5).fill(V0List.size());
         List<ReconstructedParticle> rpList = event.get(ReconstructedParticle.class, reconstructedParticleCollectionName);
-        if (rpList.size() != 3) { // note that requring three or less FSPs means we can have at most two V0s in the event
-            return;
-        }
-        aida.tree().mkdirs(V0List.size() + " event V0s");
-        aida.tree().cd(V0List.size() + " event V0s");
+        if (rpList.size() == 3) { // note that requring three or less FSPs means we can have at most two V0s in the event
+
+            aida.tree().mkdirs("run " + event.getRunNumber() + " " + V0List.size() + " event V0s");
+            aida.tree().cd("run " + event.getRunNumber() + " " + V0List.size() + " event V0s");
 //        if (V0List.size() != 1) {
 //            return;
 //        }
-        for (ReconstructedParticle V0 : V0List) {
-            List<ReconstructedParticle> trks = V0.getParticles();
-            ReconstructedParticle ele = trks.get(0);
-            ReconstructedParticle pos = trks.get(1);
-            ReconstructedParticle third = null;
-            String type;
-            for (ReconstructedParticle rp : rpList) {
+            for (ReconstructedParticle V0 : V0List) {
+                List<ReconstructedParticle> trks = V0.getParticles();
+                ReconstructedParticle ele = trks.get(0);
+                ReconstructedParticle pos = trks.get(1);
+                ReconstructedParticle third = null;
+                String type;
+                for (ReconstructedParticle rp : rpList) {
 
-                if (rp == ele) {
-                    continue;
+                    if (rp == ele) {
+                        continue;
+                    }
+                    if (rp == pos) {
+                        continue;
+                    }
+                    third = rp;
                 }
-                if (rp == pos) {
-                    continue;
+                int pdgId = third.getParticleIDUsed().getPDG();
+
+                switch (pdgId) {
+                    case 11:
+                        type = "electron";
+                        break;
+                    case 22:
+                        type = "photon";
+                        break;
+                    case -11:
+                        type = "positron";
+                        break;
+                    default:
+                        type = "unknown";
+                        break;
                 }
-                third = rp;
-            }
-            int pdgId = third.getParticleIDUsed().getPDG();
 
-            switch (pdgId) {
-                case 11:
-                    type = "electron";
-                    break;
-                case 22:
-                    type = "photon";
-                    break;
-                case -11:
-                    type = "positron";
-                    break;
-                default:
-                    type = "unknown";
-                    break;
-            }
+                HepLorentzVector beam = VecOp.add(V0.asFourVector(), third.asFourVector());
+                Hep3Vector beamVec = VecOp.mult(beamAxisRotation, beam.v3());
+                double beamEnergy = beam.t();
 
-            HepLorentzVector beam = VecOp.add(V0.asFourVector(), third.asFourVector());
-            Hep3Vector beamVec = VecOp.mult(beamAxisRotation, beam.v3());
-            double beamEnergy = beam.t();
+                aida.histogram1D("V0 + " + type + " energy", 100, 0.5, 5.5).fill(beamEnergy);
+                aida.histogram1D("V0 + " + type + " pX", 100, -0.2, 0.2).fill(beamVec.x());
+                aida.histogram1D("V0 + " + type + " pY", 100, -0.2, 0.2).fill(beamVec.y());
+                aida.histogram1D("V0 + " + type + " pZ", 100, 0.5, 5.5).fill(beamVec.z());
 
-            aida.histogram1D("V0 + " + type + " energy", 100, 0.5, 5.5).fill(beamEnergy);
-            aida.histogram1D("V0 + " + type + " pX", 100, -0.2, 0.2).fill(beamVec.x());
-            aida.histogram1D("V0 + " + type + " pY", 100, -0.2, 0.2).fill(beamVec.y());
-            aida.histogram1D("V0 + " + type + " pZ", 100, 0.5, 5.5).fill(beamVec.z());
-
-            double deltaPz = 0.3;
-            double beamPz = 2.3;
-            if (abs(beamPz - beamVec.z()) < deltaPz) {
-                aida.histogram1D("V0 + " + type + " energy after pZ cut", 100, 0.5, 5.5).fill(beamEnergy);
-                aida.histogram1D("V0 + " + type + " pX after pZ cut", 100, -0.2, 0.2).fill(beamVec.x());
-                aida.histogram1D("V0 + " + type + " pY after pZ cut", 100, -0.2, 0.2).fill(beamVec.y());
-                aida.histogram1D("V0 + " + type + " pZ after pZ cut", 100, 0.5, 5.5).fill(beamVec.z());
-            }
-
-            // try fitting all three tracks...
-            if (type.equals("electron")) {
-                List<BilliorTrack> tracksToVertex = new ArrayList<>();
-                tracksToVertex.add(new BilliorTrack(ele.getTracks().get(0)));
-                tracksToVertex.add(new BilliorTrack(pos.getTracks().get(0)));
-                tracksToVertex.add(new BilliorTrack(third.getTracks().get(0)));
-                BilliorVertex vtx = vtxFitter.fitVertex(tracksToVertex);
-                Hep3Vector v0Pos = V0.getReferencePoint();
-                Hep3Vector tridentPos = vtx.getPosition();
-                aida.histogram1D("V0 x", 100, -2., 2.).fill(v0Pos.x());
-                aida.histogram1D("V0 y", 100, -2., 2.).fill(v0Pos.y());
-                aida.histogram1D("V0 z", 100, -20., 20.).fill(v0Pos.z());
-                aida.histogram1D("Trident x", 100, -2., 2.).fill(tridentPos.x());
-                aida.histogram1D("Trident y", 100, -2., 2.).fill(tridentPos.y());
-                aida.histogram1D("Trident z", 100, -20., 20.).fill(tridentPos.z());
-                // good event, let's strip it, but only in single V0 events
-                if (V0List.size() == 1) {
-                    skipEvent = false;
+                double deltaPz = 0.3;
+                double beamPz = 2.3;
+                if (abs(beamPz - beamVec.z()) < deltaPz) {
+                    aida.histogram1D("V0 + " + type + " energy after pZ cut", 100, 0.5, 5.5).fill(beamEnergy);
+                    aida.histogram1D("V0 + " + type + " pX after pZ cut", 100, -0.2, 0.2).fill(beamVec.x());
+                    aida.histogram1D("V0 + " + type + " pY after pZ cut", 100, -0.2, 0.2).fill(beamVec.y());
+                    aida.histogram1D("V0 + " + type + " pZ after pZ cut", 100, 0.5, 5.5).fill(beamVec.z());
                 }
-            }// end of electron trident
-        }//end of loop over V0s
-        aida.tree().cd("..");
+
+                // try fitting all three tracks...
+                if (type.equals("electron")) {
+                    List<BilliorTrack> tracksToVertex = new ArrayList<>();
+                    tracksToVertex.add(new BilliorTrack(ele.getTracks().get(0)));
+                    tracksToVertex.add(new BilliorTrack(pos.getTracks().get(0)));
+                    tracksToVertex.add(new BilliorTrack(third.getTracks().get(0)));
+                    BilliorVertex vtx = vtxFitter.fitVertex(tracksToVertex);
+                    Hep3Vector v0Pos = V0.getReferencePoint();
+                    Hep3Vector tridentPos = vtx.getPosition();
+                    aida.histogram1D("V0 x", 100, -2., 2.).fill(v0Pos.x());
+                    aida.histogram1D("V0 y", 100, -2., 2.).fill(v0Pos.y());
+                    aida.histogram1D("V0 z", 100, -20., 20.).fill(v0Pos.z());
+                    aida.histogram1D("Trident x", 100, -2., 2.).fill(tridentPos.x());
+                    aida.histogram1D("Trident y", 100, -2., 2.).fill(tridentPos.y());
+                    aida.histogram1D("Trident z", 100, -20., 20.).fill(tridentPos.z());
+                    // good event, let's strip it, but only in single V0 events
+                    if (V0List.size() == 1) {
+                        skipEvent = false;
+                    }
+                }// end of electron trident
+            }//end of loop over V0s
+            aida.tree().cd("..");
+        }//end of check on three FSPs
         if (skipEvent) {
             throw new Driver.NextEventException();
         } else {
