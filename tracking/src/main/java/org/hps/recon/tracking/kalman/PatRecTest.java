690055eaeef0af4f99fd6a7b05351b49fbc8ab2b
robertprestonjohnson <rjohnson@ucsc.edu>
2020-09-22 09:59:34 -0700

    Removed reference to non-existent class from the test program PatRecTest

--- tracking/src/main/java/org/hps/recon/tracking/kalman/PatRecTest.java
+++ tracking/src/main/java/org/hps/recon/tracking/kalman/PatRecTest.java
@@ -493,7 +493,6 @@ class PatRecTest {
             vtxCov.M[0][0] = vtxRes[0]*vtxRes[0];
             vtxCov.M[1][1] = vtxRes[1]*vtxRes[1];
             vtxCov.M[2][2] = vtxRes[2]*vtxRes[2];
-            KalScatteredElectronFinder scatElec = new KalScatteredElectronFinder(SiModules, vtx.v, vtxCov.M, eventNumber);
             KalmanPatRecHPS patRec = new KalmanPatRecHPS(SiModules, 0, eventNumber, kPar);
             if (nPlot < mxPlot && verbose) {  // Code to make a single event display using gnuplot
                 nPlot++;
