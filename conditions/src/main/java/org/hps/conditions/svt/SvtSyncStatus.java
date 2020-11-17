05ce83ffee38c3a5f36e4412c12c07e708478e74
Jeremy McCormick <jermccormick@gmail.com>
2020-11-16 18:43:34 -0600

    Add class for SVT sync status.

---
+++ conditions/src/main/java/org/hps/conditions/svt/SvtSyncStatus.java
@@ -0,0 +1,20 @@
+package org.hps.conditions.svt;
+
+import org.hps.conditions.api.BaseConditionsObject;
+import org.hps.conditions.api.BaseConditionsObjectCollection;
+import org.hps.conditions.database.Field;
+import org.hps.conditions.database.Table;
+
+@Table(names = {"svt_sync_statuses"})
+public final class SvtSyncStatus extends BaseConditionsObject {
+
+    public static class SvtSyncStatusCollection extends BaseConditionsObjectCollection<SvtSyncStatus> {
+    }
+
+    @Field(names = {"good"})
+    public Boolean isGood() {
+        return this.getFieldValue(Boolean.class, "good");
+    }
+}
+
+
