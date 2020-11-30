fcc81b64478d568c881ded3212cf9b4f64f8eb8f
Jeremy McCormick <jermccormick@gmail.com>
2020-11-30 00:09:08 -0600

    Add remote aida dep

---
+++ online-recon/src/test/java/org/hps/online/recon/RemoteAidaClientTest.java
@@ -0,0 +1,59 @@
+package org.hps.online.recon;
+
+import java.net.InetAddress;
+
+import hep.aida.IAnalysisFactory;
+import hep.aida.IHistogram1D;
+import hep.aida.ITree;
+import hep.aida.ITreeFactory;
+import hep.aida.ref.remote.rmi.client.RmiStoreFactory;
+import junit.framework.TestCase;
+
+public class RemoteAidaClientTest extends TestCase {
+
+    public void testRemoteAidaClient() {
+
+        ITree clientTree;
+        IAnalysisFactory af = IAnalysisFactory.create();
+        ITreeFactory tf = af.createTreeFactory();
+
+        String localHost = null;
+        try {
+            localHost = InetAddress.getLocalHost().getHostName();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        int port = 2001;
+        String serverName = "RmiAidaServer";
+        String treeBindName = "//"+localHost+":"+port+"/"+serverName;
+
+        String hist1DTitle = "Aida 1D Histogram";
+        String histPath = "/hists";
+
+        // Create Rmi Client Tree
+        boolean clientDuplex = true;
+        boolean hurry = false;
+        String options = "duplex=\""+clientDuplex+"\",RmiServerName=\"rmi:"+treeBindName+"\",hurry=\""+hurry+"\"";
+        System.out.println("options: " + options);
+        try {
+            clientTree = tf.create(localHost, RmiStoreFactory.storeType, true, false, options);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        // Retrieve AIDA instances using the RMI
+        IHistogram1D rh1 = null;
+        try {
+            System.out.println("Finding remote histogram: " + histPath + "/" + hist1DTitle);
+            rh1 = (IHistogram1D) clientTree.find(histPath + "/" + hist1DTitle);
+
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Remote H1D has n entries: " + rh1.entries());
+    }
+}
---
+++ online-recon/src/test/java/org/hps/online/recon/RemoteAidaServerTest.java
@@ -0,0 +1,84 @@
+package org.hps.online.recon;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Random;
+
+import hep.aida.IAnalysisFactory;
+import hep.aida.IHistogram1D;
+import hep.aida.IHistogramFactory;
+import hep.aida.ITreeFactory;
+import hep.aida.dev.IDevTree;
+import hep.aida.ref.remote.RemoteServer;
+import hep.aida.ref.remote.rmi.interfaces.RmiServer;
+import hep.aida.ref.remote.rmi.server.RmiServerImpl;
+import junit.framework.TestCase;
+
+public class RemoteAidaServerTest extends TestCase {
+
+    public void testRemoteAidaServer() {
+        String localHost = null;
+        try {
+            localHost = InetAddress.getLocalHost().getHostName();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assert(localHost != null);
+
+        int port = 2001;
+        String serverName = "RmiAidaServer";
+
+        String histPath = "/hists";
+        String hist1DTitle = "Aida 1D Histogram";
+        int xbins = 40;
+        int nEntries = 10000;
+        double xLowerEdge = -2.3;
+        double xUpperEdge = 4.2;
+
+        IAnalysisFactory af = IAnalysisFactory.create();
+        ITreeFactory tf = af.createTreeFactory();
+        IDevTree serverTree = (IDevTree) tf.create();
+        IHistogramFactory hf = af.createHistogramFactory(serverTree);
+
+        serverTree.mkdir(histPath);
+        serverTree.cd(histPath);
+        IHistogram1D h1 = hf.createHistogram1D(hist1DTitle, xbins, xLowerEdge, xUpperEdge);
+
+        RemoteServer treeServer;
+        RmiServer rmiTreeServer;
+        String treeBindName = "//"+localHost+":"+port+"/"+serverName;
+        System.out.println("server tree: " + treeBindName);
+        try {
+            boolean serverDuplex = true;
+            treeServer = new RemoteServer(serverTree, serverDuplex);
+            rmiTreeServer = new RmiServerImpl(treeServer, treeBindName);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            Random r = new Random();
+            for (int i = 0; i < 10 * nEntries; i++) {
+                if (i % 10 == 0) {
+                    System.out.println("Filling entry: " + i);
+                }
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                double xVal = r.nextGaussian();
+                double w = r.nextDouble();
+                h1.fill(xVal, w);
+            }
+        } finally {
+            ((RmiServerImpl) rmiTreeServer).disconnect();
+            treeServer.close();
+            try {
+                serverTree.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
