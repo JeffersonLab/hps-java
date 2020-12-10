217a4d262d6d428b40e77d19bb9560fe9fc6920c
Jeremy McCormick <jermccormick@gmail.com>
2020-12-10 01:55:36 -0600

    Change name of class to indicate it is an example only

--- online-recon/src/main/java/org/hps/online/recon/remoteaida/RemoteAidaServerExample.java
+++ online-recon/src/main/java/org/hps/online/recon/remoteaida/RemoteAidaServerExample.java
@@ -16,7 +16,7 @@ import hep.aida.ref.remote.rmi.client.RmiStoreFactory;
 import hep.aida.ref.remote.rmi.interfaces.RmiServer;
 import hep.aida.ref.remote.rmi.server.RmiServerImpl;
 
-public class RemoteAidaServer {
+public class RemoteAidaServerExample {
 
     IAnalysisFactory af = IAnalysisFactory.create();
     ITreeFactory tf = af.createTreeFactory();
@@ -24,7 +24,7 @@ public class RemoteAidaServer {
     IHistogramFactory hf = af.createHistogramFactory(serverTree);
 
     public static void main(String[] args) {
-        RemoteAidaServer server = new RemoteAidaServer();
+        RemoteAidaServerExample server = new RemoteAidaServerExample();
         server.runIt();
     }
 
