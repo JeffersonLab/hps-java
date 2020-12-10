a332c9a90bc1ded5a1040b98cb899374c38d5e03
Jeremy McCormick <jermccormick@gmail.com>
2020-12-09 23:31:31 -0600

    Update remote hist example driver

--- online-recon/src/main/java/org/hps/online/example/RemoteHistExampleDriver.java
+++ online-recon/src/main/java/org/hps/online/example/RemoteHistExampleDriver.java
@@ -1,28 +1,44 @@
 package org.hps.online.example;
 
+import java.util.List;
+
 import org.lcsim.event.EventHeader;
+import org.lcsim.event.RawTrackerHit;
 import org.lcsim.event.Track;
-import org.lcsim.util.aida.AIDA;
 
 import hep.aida.IHistogram1D;
+import hep.aida.IHistogram2D;
 
 /**
- * Create an example histogram in a remote AIDA tree
+ * Create example 1D and 2D histograms in a remote AIDA tree
  */
 public class RemoteHistExampleDriver extends RemoteAidaDriver {
 
+    private static final String ECAL_DIR = "/subdet/ecal";
+    private static final String TRACKER_DIR = "/subdet/tracker";
     private IHistogram1D tracks = null;
-
-    private static final String DIR ="/subdet/tracker";
+    private IHistogram2D ecalHits = null;
 
     public void startOfData() {
-        tree.mkdirs(DIR);
-        tree.cd(DIR);
-        tracks = AIDA.defaultInstance().histogram1D("Tracks", 20, 0., 20.);
+        tree.mkdirs(TRACKER_DIR);
+        tree.cd(TRACKER_DIR);
+        tracks = aida.histogram1D("Tracks", 20, 0., 20.);
+
+        tree.mkdirs(ECAL_DIR);
+        tree.cd(ECAL_DIR);
+        ecalHits = aida.histogram2D("Ecal Hits", 47, -23.5, 23.5, 11, -5.5, 5.5);
+
         super.startOfData();
     }
 
     public void process(EventHeader event) {
         tracks.fill(event.get(Track.class, "MatchedTracks").size());
+
+        List<RawTrackerHit> hits = event.get(RawTrackerHit.class, "EcalReadoutHits");
+        for (RawTrackerHit hit : hits) {
+            int column = hit.getIdentifierFieldValue("ix");
+            int row = hit.getIdentifierFieldValue("iy");
+            ecalHits.fill(column, row);
+        }
     }
 }
