6f376e1323afec78444ca2b404cbe5d76134f584
Jeremy McCormick <jermccormick@gmail.com>
2020-12-28 15:08:47 -0600

    Add javadoc

--- online-recon/src/main/java/org/hps/online/recon/commands/package-info.java
+++ online-recon/src/main/java/org/hps/online/recon/commands/package-info.java
@@ -1,5 +1,5 @@
 /**
  * Commands for the online reconstruction system which are used by the <code>Client</code>
- * to send commands with parameters to the server.
+ * to send commands with parameters to the <code>Server</code>.
  */
 package org.hps.online.recon.commands;
