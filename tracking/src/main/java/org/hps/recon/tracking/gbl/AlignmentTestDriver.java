72ce74e58da51771b2c78e9fd566c648f6d1f724
Pierfrancesco Butti <pierfrancesco.butti@gmail.com>
2021-09-08 11:40:00 -0700

    Added test Driver

---
+++ tracking/src/main/java/org/hps/recon/tracking/gbl/AlignmentTestDriver.java
@@ -0,0 +1,43 @@
+package org.hps.recon.tracking.gbl;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.lcsim.detector.tracker.silicon.SiSensor;
+import org.lcsim.geometry.Detector;
+import org.lcsim.detector.IDetectorElement;
+import org.lcsim.util.Driver;
+import org.lcsim.event.EventHeader;
+
+public class AlignmentTestDriver extends Driver {
+
+    private List<SiSensor> sensors = new ArrayList<SiSensor>();
+
+    
+    @Override
+    protected void startOfData() {
+    }
+    
+    @Override
+    protected void endOfData() {
+    }
+    
+    
+    @Override
+    protected void detectorChanged(Detector detector) {
+        
+        //Alignment Manager  - Get the composite structures.
+        IDetectorElement detectorElement = detector.getDetectorElement();
+        
+        // Get the sensors subcomponents // This should be only HpsSiSensors
+        sensors = detectorElement.findDescendants(SiSensor.class);
+
+        System.out.println(":::AlignmentTestDriver:::");
+        AlignmentStructuresBuilder asb = new AlignmentStructuresBuilder(sensors);
+        
+    }
+
+    protected void process(EventHeader event){}
+
+
+
+}
