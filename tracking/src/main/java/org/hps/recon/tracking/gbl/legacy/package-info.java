845330ed41a2d645a58062412e469e750f81592d
tomeichlersmith <eichl008@umn.edu>
2023-04-24 15:00:45 -0500

    start moving legacy stuff into legacy package

---
+++ tracking/src/main/java/org/hps/recon/tracking/gbl/legacy/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Legacy Generalized Broken Lines (GBL) track fitting algorithm manually translated to java
+ * from C++. The original GBL source repository is available on DESY's GitLab:
+ * <p>
+ * <a href="https://gitlab.desy.de/claus.kleinwort/general-broken-lines">GBL Source Repository</a>
+ * <p>
+ * <a href="https://gitlab.desy.de/claus.kleinwort/general-broken-lines/-/wikis/home">GBL Documentation</a>
+ */
+package org.hps.recon.tracking.gbl.legacy;
+
