32adc40dee6246a803cea00afade8e07c6fd180d
rodwyer@stanford.edu <rodwyer1@sdfiana005.sdf.slac.stanford.edu>
2024-08-04 16:54:41 -0700

    Still trying to fix stuff

--- tracking/src/main/java/org/hps/recon/tracking/NearestNeighborRMSClusterer.java
+++ tracking/src/main/java/org/hps/recon/tracking/NearestNeighborRMSClusterer.java
@@ -157,7 +157,9 @@ public class NearestNeighborRMSClusterer implements ClusteringAlgorithm {
             IIdentifier id = rawHit.getIdentifier();
             int channel_number = sid_helper.getElectrodeValue(id);
 
-            if(((HpsSiSensor) rawHit.getDetectorElement()).isBadChannel(channel_number)){continue;}
+            if(_doDeadFix){
+	    	if(((HpsSiSensor) rawHit.getDetectorElement()).isBadChannel(channel_number)){continue;}
+	    }
 
             // Check for duplicate RawTrackerHits or channel numbers
             if (channel_to_hit.containsKey(channel_number)) {
